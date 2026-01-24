f5e098651a8baeae576333e6a292fa5f2ad194f5
Leturgone <finekon221@gmail.com>
2026-01-24 15:08:34 +0300

    create RoleEntity

---
+++ src/main/kotlin/api/masterplan/app/authModule/infrastructure/database/entity/RoleEntity.kt
@@ -0,0 +1,22 @@
+package api.masterplan.app.authModule.infrastructure.database.entity
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "role")
+data class RoleEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    val id: Long,
+
+    @Column(name = "title", nullable = false, unique = true)
+    val  title: String,
+
+    @ManyToMany(cascade = [CascadeType.ALL])
+    @JoinTable(name = "app_user_has_role",
+        joinColumns = [JoinColumn(name = "role_id", referencedColumnName = "id")],
+        inverseJoinColumns = [JoinColumn(name = "app_user_id", referencedColumnName = "id")]
+    )
+    val usersWithRole: MutableSet<AppUserEntity> = hashSetOf()
+)
