23bb1a6b845ba7e9708f2581970d271368ee6f22
Leturgone <finekon221@gmail.com>
2026-01-24 23:08:51 +0300

    implemented user repository

--- src/main/kotlin/api/masterplan/app/authModule/infrastructure/database/repository/UserRepositoryImpl.kt
+++ src/main/kotlin/api/masterplan/app/authModule/infrastructure/database/repository/UserRepositoryImpl.kt
@@ -1,4 +1,41 @@
 package api.masterplan.app.authModule.infrastructure.database.repository
 
-class UserRepositoryImpl {
+import api.masterplan.app.authModule.domain.`interface`.UserRepository
+import api.masterplan.app.authModule.domain.model.entity.AppUser
+import api.masterplan.app.authModule.domain.model.value.UserId
+import api.masterplan.app.authModule.domain.model.value.UserLogin
+import api.masterplan.app.authModule.infrastructure.database.mapper.UserDatabaseEntityMapper
+import api.masterplan.app.authModule.infrastructure.exceptions.MasterPlanDatabaseException
+
+class UserRepositoryImpl(
+    private val  jpaUserRepository: JpaUserRepository
+): UserRepository {
+
+    override suspend fun findById(id: UserId): Result<AppUser> {
+        val userEntity = jpaUserRepository.findByUid(id.value)?: return Result.failure(
+            MasterPlanDatabaseException.UserNotExistsWithId(id)
+        )
+        return try {
+            val domainUser = UserDatabaseEntityMapper.toDomain(userEntity)
+            Result.success(domainUser)
+        }catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+
+    override suspend fun findByLogin(login: UserLogin): Result<AppUser> {
+        val userEntity = jpaUserRepository.findByLogin(login.value)?: return Result.failure(
+            MasterPlanDatabaseException.UserNotExistsWithLogin(login)
+        )
+        return try {
+            val domainUser = UserDatabaseEntityMapper.toDomain(userEntity)
+            Result.success(domainUser)
+        }catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+
+    override suspend fun isUserExist(login: UserLogin): Boolean {
+        return jpaUserRepository.existsByLogin(login.value)
+    }
 }
