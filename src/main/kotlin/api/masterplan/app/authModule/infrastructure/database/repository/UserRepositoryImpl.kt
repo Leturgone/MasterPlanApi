ce9c9aa3a0f5c45f06f9450d5795969476336a9c
Leturgone <finekon221@gmail.com>
2026-01-25 23:47:36 +0300

    delete result from UserRepositoryImpl

--- src/main/kotlin/api/masterplan/app/authModule/infrastructure/database/repository/UserRepositoryImpl.kt
+++ src/main/kotlin/api/masterplan/app/authModule/infrastructure/database/repository/UserRepositoryImpl.kt
@@ -6,33 +6,23 @@ import api.masterplan.app.authModule.domain.model.value.UserId
 import api.masterplan.app.authModule.domain.model.value.UserLogin
 import api.masterplan.app.authModule.infrastructure.database.mapper.UserDatabaseEntityMapper
 import api.masterplan.app.authModule.infrastructure.exceptions.MasterPlanDatabaseException
+import org.springframework.stereotype.Repository
 
+@Repository
 class UserRepositoryImpl(
     private val  jpaUserRepository: JpaUserRepository
 ): UserRepository {
 
-    override suspend fun findById(id: UserId): Result<AppUser> {
-        val userEntity = jpaUserRepository.findByUid(id.value)?: return Result.failure(
-            MasterPlanDatabaseException.UserNotExistsWithId(id)
-        )
-        return try {
-            val domainUser = UserDatabaseEntityMapper.toDomain(userEntity)
-            Result.success(domainUser)
-        }catch (e: Exception){
-            Result.failure(e)
-        }
+    override suspend fun findById(id: UserId): AppUser? {
+        val userEntity = jpaUserRepository.findByUid(id.value) ?: return null
+        val domainUser = UserDatabaseEntityMapper.toDomain(userEntity)
+        return domainUser
     }
 
-    override suspend fun findByLogin(login: UserLogin): Result<AppUser> {
-        val userEntity = jpaUserRepository.findByLogin(login.value)?: return Result.failure(
-            MasterPlanDatabaseException.UserNotExistsWithLogin(login)
-        )
-        return try {
-            val domainUser = UserDatabaseEntityMapper.toDomain(userEntity)
-            Result.success(domainUser)
-        }catch (e: Exception){
-            Result.failure(e)
-        }
+    override suspend fun findByLogin(login: UserLogin): AppUser? {
+        val userEntity = jpaUserRepository.findByLogin(login.value)?: return null
+        val domainUser = UserDatabaseEntityMapper.toDomain(userEntity)
+        return domainUser
     }
 
     override suspend fun isUserExist(login: UserLogin): Boolean {
