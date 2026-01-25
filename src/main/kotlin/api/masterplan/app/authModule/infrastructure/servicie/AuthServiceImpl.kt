f52cfb5a5e6f42c675f7f3c8b1d69ab63e129584
Leturgone <finekon221@gmail.com>
2026-01-25 23:47:49 +0300

    created AuthServiceImpl

--- src/main/kotlin/api/masterplan/app/authModule/infrastructure/servicie/AuthServiceImpl.kt
+++ src/main/kotlin/api/masterplan/app/authModule/infrastructure/servicie/AuthServiceImpl.kt
@@ -1,4 +1,46 @@
 package api.masterplan.app.authModule.infrastructure.servicie
 
-class AuthServiceImpl {
+import api.masterplan.app.authModule.application.dto.UserCredentials
+import api.masterplan.app.authModule.application.service.AuthService
+import api.masterplan.app.authModule.domain.`interface`.PasswordHasher
+import api.masterplan.app.authModule.domain.`interface`.UserRepository
+import api.masterplan.app.authModule.domain.model.value.UserLogin
+import api.masterplan.app.authModule.domain.model.value.UserPassword
+import api.masterplan.app.authModule.infrastructure.exceptions.MasterPlanAuthException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class AuthServiceImpl(
+    private val userRepository: UserRepository,
+    private val passwordHasher: PasswordHasher
+): AuthService {
+
+    private val  logger = LoggerFactory.getLogger(this::class.java)
+
+    override suspend fun authenticate(
+        login: UserLogin,
+        password: UserPassword
+    ): Result<UserCredentials> {
+        return try {
+
+            val user = userRepository.findByLogin(login) ?: throw MasterPlanAuthException.UserNotExistsWithLogin(login)
+
+            if (!withContext(Dispatchers.IO) { passwordHasher.verify(password, user.password) }) {
+                return Result.failure(MasterPlanAuthException.InvalidCredentials())
+            }
+
+            logger.info("Authentication success for login=${login.value}")
+
+            Result.success(UserCredentials(user.id, user.roles))
+
+        }catch (e: Exception){
+            logger.warn("Authentication failed for login=${login.value}", e)
+
+            Result.failure(e)
+        }
+    }
+
 }
