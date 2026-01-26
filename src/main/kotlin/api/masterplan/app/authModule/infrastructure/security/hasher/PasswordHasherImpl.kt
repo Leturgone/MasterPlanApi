90dbf8941d051ba33396a24a965af43b1b48b817
Leturgone <finekon221@gmail.com>
2026-01-26 20:33:26 +0300

    create PasswordHasherImpl

--- src/main/kotlin/api/masterplan/app/authModule/infrastructure/security/hasher/PasswordHasherImpl.kt
+++ src/main/kotlin/api/masterplan/app/authModule/infrastructure/security/hasher/PasswordHasherImpl.kt
@@ -1,4 +1,23 @@
 package api.masterplan.app.authModule.infrastructure.security.hasher
 
-class PasswordHasherImpl {
+import api.masterplan.app.authModule.domain.`interface`.PasswordHasher
+import api.masterplan.app.authModule.domain.model.value.UserPassword
+import api.masterplan.app.authModule.infrastructure.exceptions.MasterPlanPasswordHashException
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Component
+
+@Component
+class PasswordHasherImpl(
+    private val encoder: PasswordEncoder
+): PasswordHasher {
+
+    override fun hash(rawPassword: UserPassword): UserPassword {
+        val hashPassword = encoder.encode(rawPassword.value) ?: throw MasterPlanPasswordHashException.EmptyPassword()
+        return UserPassword.create(hashPassword)
+    }
+
+    override fun verify(rawPassword: UserPassword, hash: UserPassword): Boolean {
+        return encoder.matches(rawPassword.value,hash.value)
+    }
+
 }
