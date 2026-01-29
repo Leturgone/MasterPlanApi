8f44339041ceaf2ac0e4921e9402621e72c1b308
Leturgone <finekon221@gmail.com>
2026-01-29 20:32:20 +0300

    add docs to LoginResponse

--- src/main/kotlin/api/masterplan/app/authModule/presentation/dto/LoginResponse.kt
+++ src/main/kotlin/api/masterplan/app/authModule/presentation/dto/LoginResponse.kt
@@ -3,35 +3,35 @@ package api.masterplan.app.authModule.presentation.dto
 import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
 
-@Schema(description = "Auth response")
+@Schema(description = "Ответ на аутентификацию")
 sealed class LoginResponse private constructor() {
 
-    @Schema(description = "Auth Success response")
+    @Schema(description = "Удачный ответ")
     data class Success(
 
-        @Schema(description = "JWT‑token for access API",
+        @Schema(description = "JWT‑токен для доступа к  API",
             example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODk..."
         )
         val token: String,
 
         @Schema(
-            description = "Token type",
+            description = "Тип токена",
             example = "Bearer",
             defaultValue = "Bearer"
         )
         val type: String = "Bearer",
     ): LoginResponse()
 
-    @Schema(description = "Auth Error response")
+    @Schema(description = "Неудачный ответ")
     data class Error(
 
-        @Schema(description = "HTTP error code", example = "500")
+        @Schema(description = "HTTP код ошибки", example = "500")
         val status: Int,
 
-        @Schema(description = "HTTP error description", example = "Error while login")
+        @Schema(description = "Описание HTTP ошибки", example = "Error while login")
         val message: String? = "",
 
-        @Schema(description = "Error date", example = "2026-01-27T18:30:00Z")
+        @Schema(description = "Дата ошибки ", example = "2026-01-27T18:30:00Z")
         val timestamp: LocalDateTime,
     ): LoginResponse()
 
