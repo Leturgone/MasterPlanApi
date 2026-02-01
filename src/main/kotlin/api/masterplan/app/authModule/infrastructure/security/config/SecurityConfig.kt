496337bd7b0f071da565291d59846f9f7aca6295
Leturgone <finekon221@gmail.com>
2026-02-01 21:06:12 +0300

    add SecurityFilter to SecurityConfig

--- src/main/kotlin/api/masterplan/app/authModule/infrastructure/security/config/SecurityConfig.kt
+++ src/main/kotlin/api/masterplan/app/authModule/infrastructure/security/config/SecurityConfig.kt
@@ -1,14 +1,17 @@
 package api.masterplan.app.authModule.infrastructure.security.config
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 
 @Configuration
-@EnableWebSecurity
+@EnableConfigurationProperties(JwtProperties::class)
 class SecurityConfig {
 
     @Bean
@@ -29,4 +32,18 @@ class SecurityConfig {
         return encoder
     }
 
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain{
+        return http
+            .csrf { it.disable() }
+            .authorizeHttpRequests { auth ->
+                auth
+                    .requestMatchers(
+                        "/swagger-ui/**", "/api/v1/api-docs/**","/api/v1/api-docs.yaml"
+                    ).permitAll()
+                    .requestMatchers("/api/v1/auth/login").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .build()
+    }
 }
