0804f7b1f3dfac35e65709dfc66228da661e5e03
Leturgone <finekon221@gmail.com>
2026-02-01 21:06:30 +0300

    add JwtProperties to TokenGeneratorServiceImpl

--- src/main/kotlin/api/masterplan/app/authModule/infrastructure/servicie/TokenGeneratorServiceImpl.kt
+++ src/main/kotlin/api/masterplan/app/authModule/infrastructure/servicie/TokenGeneratorServiceImpl.kt
@@ -5,23 +5,31 @@ import api.masterplan.app.authModule.application.service.TokenGeneratorService
 import api.masterplan.app.authModule.domain.model.value.UserId
 import api.masterplan.app.authModule.domain.model.value.UserRole
 import api.masterplan.app.authModule.infrastructure.exceptions.MasterPlanTokenException
+import api.masterplan.app.authModule.infrastructure.security.config.JwtProperties
 import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.io.Decoders
 import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.util.*
 import javax.crypto.SecretKey
 
 @Service
 class TokenGeneratorServiceImpl(
-    private val jwtSecret: String,
-    private val expirationHours: Long = 12
+    private val jwtProperties: JwtProperties
 ): TokenGeneratorService {
 
     override fun generateToken(userId: UserId, userRoles: Set<UserRole>): Result<JwtToken> {
 
         return try {
             val key: SecretKey by lazy {
-                Keys.hmacShaKeyFor(jwtSecret.toByteArray())
+                val decodedBytes = Decoders.BASE64.decode(jwtProperties.secret)
+
+                require(decodedBytes.size >= 32) {
+                    "JWT secret must be at least 32 bytes for HS256"
+                }
+
+                Keys.hmacShaKeyFor(decodedBytes)
             }
 
             val expirationTime = calculateExpiration()
@@ -47,7 +55,7 @@ class TokenGeneratorServiceImpl(
     }
 
     private fun calculateExpiration(): Date {
-        return Date(System.currentTimeMillis() + expirationHours * 60 * 60 * 1000)
+        return Date(System.currentTimeMillis() + jwtProperties.expiration * 60 * 60 * 1000)
     }
 
 }
