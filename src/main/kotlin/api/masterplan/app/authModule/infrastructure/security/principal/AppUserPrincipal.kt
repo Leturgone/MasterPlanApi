42d929a22d5290151c1a6092de532eff872cf3d7
Leturgone <finekon221@gmail.com>
2026-02-02 20:49:33 +0300

    add AppUserDetailsPrincipal

---
+++ src/main/kotlin/api/masterplan/app/authModule/infrastructure/security/principal/AppUserPrincipal.kt
@@ -0,0 +1,23 @@
+package api.masterplan.app.authModule.infrastructure.security.principal
+
+import api.masterplan.app.authModule.domain.model.entity.AppUser
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class AppUserPrincipal(private val userEntity: AppUser) : UserDetails {
+
+    override fun getAuthorities(): Collection<GrantedAuthority?> {
+        return userEntity.roles.map {
+            SimpleGrantedAuthority(it.name)
+        }
+    }
+
+    override fun getPassword(): String? {
+        return userEntity.password.value
+    }
+
+    override fun getUsername(): String? {
+        return userEntity.login.value
+    }
+}
