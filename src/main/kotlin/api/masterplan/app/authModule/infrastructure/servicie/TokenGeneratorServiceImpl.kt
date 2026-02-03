364cb8cbf073839104afd643c2dc33ded681e88e
Leturgone <finekon221@gmail.com>
2026-02-03 17:38:19 +0300

    add JwtService to TokenGeneratorServiceImpl

--- src/main/kotlin/api/masterplan/app/authModule/infrastructure/servicie/TokenGeneratorServiceImpl.kt
+++ src/main/kotlin/api/masterplan/app/authModule/infrastructure/servicie/TokenGeneratorServiceImpl.kt
@@ -5,42 +5,19 @@ import api.masterplan.app.authModule.application.service.TokenGeneratorService
 import api.masterplan.app.authModule.domain.model.value.UserId
 import api.masterplan.app.authModule.domain.model.value.UserRole
 import api.masterplan.app.authModule.infrastructure.exceptions.MasterPlanTokenException
-import api.masterplan.app.authModule.infrastructure.security.config.JwtProperties
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.io.Decoders
-import io.jsonwebtoken.security.Keys
-import org.springframework.beans.factory.annotation.Value
+import api.masterplan.app.authModule.infrastructure.security.service.JwtService
 import org.springframework.stereotype.Service
-import java.util.*
-import javax.crypto.SecretKey
 
 @Service
-class TokenGeneratorServiceImpl(
-    private val jwtProperties: JwtProperties
-): TokenGeneratorService {
+class TokenGeneratorServiceImpl(private val jwtService: JwtService): TokenGeneratorService {
 
     override fun generateToken(userId: UserId, userRoles: Set<UserRole>): Result<JwtToken> {
-
         return try {
-            val key: SecretKey by lazy {
-                val decodedBytes = Decoders.BASE64.decode(jwtProperties.secret)
 
-                require(decodedBytes.size >= 32) {
-                    "JWT secret must be at least 32 bytes for HS256"
-                }
-
-                Keys.hmacShaKeyFor(decodedBytes)
-            }
-
-            val expirationTime = calculateExpiration()
-            val token = Jwts.builder()
-                .subject(userId.value.toString())
-                .issuer("MasterPlanApi")
-                .expiration(expirationTime)
-                .id(UUID.randomUUID().toString())
-                .claim("roles",userRoles.map { it.name })
-                .signWith(key)
-                .compact()
+            val token = jwtService.generateToken(
+                userId = userId.value.toString(),
+                roles = userRoles.map { it.name }
+            )
 
             Result.success(
                 JwtToken(
@@ -52,10 +29,6 @@ class TokenGeneratorServiceImpl(
             Result.failure(MasterPlanTokenException.TokenGenerationException(userId,e.message))
         }
 
-    }
-
-    private fun calculateExpiration(): Date {
-        return Date(System.currentTimeMillis() + jwtProperties.expiration * 60 * 60 * 1000)
     }
 
 }
