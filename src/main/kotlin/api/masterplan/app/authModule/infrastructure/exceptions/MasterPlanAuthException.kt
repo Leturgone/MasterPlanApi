a1c2b489b0efdaec1d9fd26f4d71a7275666fd84
Leturgone <finekon221@gmail.com>
2026-02-03 17:36:30 +0300

    add exception

--- src/main/kotlin/api/masterplan/app/authModule/infrastructure/exceptions/MasterPlanAuthException.kt
+++ src/main/kotlin/api/masterplan/app/authModule/infrastructure/exceptions/MasterPlanAuthException.kt
@@ -7,13 +7,12 @@ sealed class MasterPlanAuthException(message: String): Exception(message) {
     class InvalidCredentials: MasterPlanAuthException(
         "Passwords don't match"
     )
+    class InvalidToken: MasterPlanAuthException(
+        "Invalid token"
+    )
 
     data class UserNotExistsWithLogin(val login: UserLogin): MasterPlanAuthException(
         "User with login ${login.value} is not exists"
-    )
-
-    class EmptyLogin: MasterPlanAuthException(
-        "Login must not be empty"
     )
 
 }
