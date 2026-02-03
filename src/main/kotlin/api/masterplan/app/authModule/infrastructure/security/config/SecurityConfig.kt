28b3b582c0bd7412600a767e64be018aa8c93f0e
Leturgone <finekon221@gmail.com>
2026-02-03 17:37:35 +0300

    add JwtFilter to SecurityConfig

--- src/main/kotlin/api/masterplan/app/authModule/infrastructure/security/config/SecurityConfig.kt
+++ src/main/kotlin/api/masterplan/app/authModule/infrastructure/security/config/SecurityConfig.kt
@@ -1,18 +1,24 @@
 package api.masterplan.app.authModule.infrastructure.security.config
 
+import api.masterplan.app.authModule.infrastructure.security.filter.JwtFilter
+import api.masterplan.app.authModule.infrastructure.security.service.AppUserDetailsService
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 
 @Configuration
 @EnableConfigurationProperties(JwtProperties::class)
-class SecurityConfig {
+class SecurityConfig(
+    private val appUserDetailsService: AppUserDetailsService,
+    private val jwtFilter: JwtFilter,
+) {
 
     @Bean
     fun  passwordEncoder(): PasswordEncoder {
@@ -34,16 +40,24 @@ class SecurityConfig {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain{
-        return http
-            .csrf { it.disable() }
-            .authorizeHttpRequests { auth ->
-                auth
-                    .requestMatchers(
-                        "/swagger-ui/**", "/api/v1/api-docs/**","/api/v1/api-docs.yaml"
-                    ).permitAll()
-                    .requestMatchers("/api/v1/auth/login").permitAll()
-                    .anyRequest().authenticated()
-            }
-            .build()
+
+        http.csrf { it.disable() }
+
+        http.authorizeHttpRequests { auth ->
+            auth.requestMatchers("/swagger-ui/**", "/api/v1/api-docs/**","/api/v1/api-docs.yaml").permitAll()
+            auth.requestMatchers("/api/v1/auth/login").permitAll()
+            auth.requestMatchers("/api/v1/admin/**").hasAuthority("ADMIN")
+            auth.anyRequest().authenticated()
+        }
+        // Загрузка пользователя при аутентификации
+        http.userDetailsService(appUserDetailsService)
+
+        http.sessionManagement{it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)}
+
+        // Вставка фильтра перед первым фильтром
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
+
+
+        return http.build()
     }
 }
