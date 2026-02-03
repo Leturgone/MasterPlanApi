dace545fb958143aff4e6a56d329edf178f779c0
Leturgone <finekon221@gmail.com>
2026-02-03 17:37:16 +0300

    add JwtService

---
+++ src/main/kotlin/api/masterplan/app/authModule/infrastructure/security/service/JwtService.kt
@@ -0,0 +1,90 @@
+package api.masterplan.app.authModule.infrastructure.security.service
+
+import api.masterplan.app.authModule.infrastructure.exceptions.MasterPlanAuthException
+import api.masterplan.app.authModule.infrastructure.security.config.JwtProperties
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.io.Decoders
+import io.jsonwebtoken.security.Keys
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Service
+import java.util.Date
+import java.util.UUID
+import javax.crypto.SecretKey
+
+@Service
+class JwtService(
+    private val jwtProperties: JwtProperties
+) {
+
+    private val key: SecretKey by lazy {
+        val decodedBytes = Decoders.BASE64.decode(jwtProperties.secret)
+
+        require(decodedBytes.size >= 32) {
+            "JWT secret must be at least 32 bytes for HS256"
+        }
+
+        Keys.hmacShaKeyFor(decodedBytes)
+    }
+
+    fun generateToken(userId: String, roles:  List<String>): String  {
+
+        val expirationTime = calculateExpiration()
+        val token = Jwts.builder()
+            .subject(userId)
+            .issuer("MasterPlanApi")
+            .expiration(expirationTime)
+            .id(UUID.randomUUID().toString())
+            .claim("roles",roles)
+            .signWith(key).compact()
+
+        return token
+
+    }
+
+    fun isValidToken(token: String, user: UserDetails): Boolean {
+        val username = extractUsername(token)
+        return username == user.username && !isTokenExpired(token)
+    }
+
+    private fun calculateExpiration(): Date {
+        return Date(System.currentTimeMillis() + jwtProperties.expiration * 60 * 60 * 1000)
+    }
+
+    private fun isTokenExpired(token: String): Boolean{
+        val result = extractExpiration(token)?.before(Date())
+            ?: throw MasterPlanAuthException.InvalidToken()
+        return !result
+    }
+
+    private fun extractExpiration(token: String): Date? {
+        return extractClaim(token){claims -> claims.expiration}
+    }
+
+    fun extractUsername(token: String): String? {
+        return extractClaim(token){claims -> claims.subject}
+    }
+
+
+    fun extractRoles(token: String): List<String>? {
+        return extractClaim(token){claims -> claims["roles"] as List<String>}
+    }
+
+
+    private fun <T> extractClaim(token: String, resolver: (Claims) -> T): T? {
+        val claims = extractAllClaims(token)
+        return resolver(claims)
+    }
+
+    // Получение данных из токена
+    private fun extractAllClaims(token: String): Claims {
+        val parser = Jwts.parser()
+
+        parser.verifyWith(key)
+
+        return parser.build().parseSignedClaims(token).getPayload()
+    }
+
+
+
+}
