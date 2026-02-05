26d59508a1bdb3fb46f2c40f6fc2625fd5372549
Leturgone <finekon221@gmail.com>
2026-02-05 17:42:46 +0300

    add UserLogin to userManagementModule domain layer

---
+++ src/main/kotlin/api/masterplan/app/userManagementModule/domain/models/value/UserLogin.kt
@@ -0,0 +1,12 @@
+package api.masterplan.app.userManagementModule.domain.models.value
+
+@JvmInline
+value class UserLogin(val value: String){
+    companion object{
+        fun create(login: String): UserLogin {
+            require(login.isNotBlank()) {"Login cant be blank"}
+            require(login.length <= 255) { "Login too long" }
+            return UserLogin(login.lowercase())
+        }
+    }
+}
