20f31217439d29da246fbca8807f6616b536abbb
Leturgone <finekon221@gmail.com>
2026-02-06 23:12:15 +0300

    rename value class

--- src/main/kotlin/api/masterplan/app/authModule/domain/model/value/AuthUserLogin.kt
+++ src/main/kotlin/api/masterplan/app/authModule/domain/model/value/AuthUserLogin.kt
@@ -1,12 +1,12 @@
 package api.masterplan.app.authModule.domain.model.value
 
 @JvmInline
-value class UserLogin(val value: String){
+value class AuthUserLogin(val value: String){
     companion object{
-        fun create(login: String): UserLogin {
+        fun validate(login: String): AuthUserLogin {
             require(login.isNotBlank()) {"Login cant be blank"}
             require(login.length <= 255) { "Login too long" }
-            return UserLogin(login.lowercase())
+            return AuthUserLogin(login.lowercase())
         }
     }
 }
