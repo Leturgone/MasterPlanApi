9100db3be80c8710b0e859dd88e849f9392dfef6
Leturgone <finekon221@gmail.com>
2026-02-06 23:12:31 +0300

    rename value class for password

--- src/main/kotlin/api/masterplan/app/authModule/domain/model/value/AuthUserPassword.kt
+++ src/main/kotlin/api/masterplan/app/authModule/domain/model/value/AuthUserPassword.kt
@@ -1,17 +1,14 @@
 package api.masterplan.app.authModule.domain.model.value
 
 @JvmInline
-value class UserPassword(val value: String){
+value class AuthUserPassword(val value: String){
     companion object {
-        fun create(password: String): UserPassword{
-            validateStrength(password)
-            return UserPassword(password)
-        }
-
-        private fun validateStrength(password: String){
+        fun validate(password: String): AuthUserPassword{
             require(password.isNotBlank()) {"Password cant be blank"}
             require(password.length >= 8) { "Password too short" }
             require(password.length <= 255) { "Password too long" }
+            return AuthUserPassword(password)
         }
+
     }
 }
