f1fe5c2e1e107525e487c9fe8aa777970974a6e5
Leturgone <finekon221@gmail.com>
2026-02-06 23:37:00 +0300

    add UserAuthCredentials dto in auth module application layer

---
+++ src/main/kotlin/api/masterplan/app/authModule/application/dto/UserAuthCredentials.kt
@@ -0,0 +1,9 @@
+package api.masterplan.app.authModule.application.dto
+
+import api.masterplan.app.authModule.domain.model.value.AuthUserId
+import api.masterplan.app.authModule.domain.model.value.AuthUserRole
+
+data class UserAuthCredentials(
+    val authUserId: AuthUserId,
+    val roles: Set<AuthUserRole>
+)
