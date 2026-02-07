d747cdbdc8c8616638a5e58689ccafd6f33d5566
Leturgone <finekon221@gmail.com>
2026-02-07 23:34:37 +0300

    add mappers for UserCredentialsProvider

---
+++ src/main/kotlin/api/masterplan/app/authModule/infrastructure/adapters/AuthInnerModuleSuccessMapper.kt
@@ -0,0 +1,28 @@
+package api.masterplan.app.authModule.infrastructure.adapters
+
+import api.masterplan.app.authModule.application.dto.AuthUserModel
+import api.masterplan.app.authModule.application.dto.UserAuthCredentials
+import api.masterplan.app.authModule.domain.model.value.AuthUserId
+import api.masterplan.app.authModule.domain.model.value.AuthUserLogin
+import api.masterplan.app.authModule.domain.model.value.AuthUserPassword
+import api.masterplan.app.authModule.domain.model.value.AuthUserRole
+import api.masterplan.app.userManagementModule.UserCredentialsDto
+
+internal object AuthInnerModuleSuccessMapper {
+
+    fun toAuthUserModel(user: UserCredentialsDto ): AuthUserModel {
+        return AuthUserModel(
+            login = AuthUserLogin.validate(user.login),
+            password = AuthUserPassword.validate(user.password),
+            roles = user.roles.map {AuthUserRole.valueOf(it)}.toSet()
+        )
+    }
+
+    fun toUserAuthCredentials(user: UserCredentialsDto): UserAuthCredentials {
+        return UserAuthCredentials(
+            authUserId = AuthUserId(user.userId),
+            roles = user.roles.map {AuthUserRole.valueOf(it)}.toSet()
+        )
+    }
+
+}
