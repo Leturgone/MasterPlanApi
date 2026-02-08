5374af0062cf8ec989dcabaa8ea0a58ab26aba35
Leturgone <finekon221@gmail.com>
2026-02-08 15:15:31 +0300

    removed unused files from auth module

--- src/main/kotlin/api/masterplan/app/authModule/infrastructure/exceptions/MasterPlanPasswordHashException.kt
+++
@@ -1,8 +0,0 @@
-package api.masterplan.app.authModule.infrastructure.exceptions
-
-sealed class MasterPlanPasswordHashException(message: String): Exception(message)  {
-
-    class EmptyPassword: MasterPlanPasswordHashException(
-        "Password is empty"
-    )
-}
