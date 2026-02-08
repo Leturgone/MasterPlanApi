d83363bbce0161af898fc1e5991bc1053a2515c6
Leturgone <finekon221@gmail.com>
2026-02-08 13:28:46 +0300

    rename exceptions in ExceptionToHttpCodeMapper

--- src/main/kotlin/api/masterplan/app/authModule/presentation/mapper/ExceptionToHttpCodeMapper.kt
+++ src/main/kotlin/api/masterplan/app/authModule/presentation/mapper/ExceptionToHttpCodeMapper.kt
@@ -1,17 +1,14 @@
 package api.masterplan.app.authModule.presentation.mapper
 
-import api.masterplan.app.authModule.infrastructure.exceptions.MasterPlanAuthException
-import api.masterplan.app.authModule.infrastructure.exceptions.MasterPlanPasswordHashException
-import api.masterplan.app.authModule.infrastructure.exceptions.MasterPlanTokenException
+import api.masterplan.app.authModule.domain.exception.AuthException
 import org.springframework.http.HttpStatus
 
 object ExceptionToHttpCodeMapper {
     fun exceptionToHttpCode(ex: Throwable): HttpStatus {
         return when (ex) {
-            is MasterPlanPasswordHashException.EmptyPassword  -> HttpStatus.BAD_REQUEST
-            is MasterPlanTokenException.TokenGenerationException -> HttpStatus.BAD_REQUEST
-            is MasterPlanAuthException.UserNotExistsWithLogin -> HttpStatus.UNAUTHORIZED
-            is MasterPlanAuthException.InvalidCredentials -> HttpStatus.UNAUTHORIZED
+            is AuthException.TokenGenerationException -> HttpStatus.BAD_REQUEST
+            is AuthException.UserNotExistsWithLogin -> HttpStatus.UNAUTHORIZED
+            is AuthException.InvalidCredentials -> HttpStatus.UNAUTHORIZED
             else -> HttpStatus.INTERNAL_SERVER_ERROR
         }
     }
