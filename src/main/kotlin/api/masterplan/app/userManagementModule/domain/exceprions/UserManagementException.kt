2b3f829ce7b41647ec2f4c014713eb97786fcfd9
Leturgone <finekon221@gmail.com>
2026-02-09 22:22:46 +0300

    add new Exception in domain

--- src/main/kotlin/api/masterplan/app/userManagementModule/domain/exceprions/UserManagementException.kt
+++ src/main/kotlin/api/masterplan/app/userManagementModule/domain/exceprions/UserManagementException.kt
@@ -1,14 +1,19 @@
 package api.masterplan.app.userManagementModule.domain.exceprions
 
+import api.masterplan.app.userManagementModule.domain.models.value.UserId
 import api.masterplan.app.userManagementModule.domain.models.value.UserLogin
 
 sealed class UserManagementException(message: String): Exception(message) {
 
     class UserNotFoundException(val login: UserLogin) : UserManagementException(
-        "User with ${login.value} not found"
+        "User with login = ${login.value} not found"
     )
 
     class InvalidUserCredentialsException : UserManagementException(
         "Invalid credentials"
+    )
+
+    class UserNotExistsException(val uid: UserId) : UserManagementException(
+        "User with uid = ${uid.value} not exists"
     )
 }
