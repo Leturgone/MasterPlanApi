f91dd65263b9a85612bca1a204ecf07d9684d198
Leturgone <finekon221@gmail.com>
2026-02-10 21:52:39 +0300

    implemented UserService

--- src/main/kotlin/api/masterplan/app/userManagementModule/application/service/UserServiceImpl.kt
+++ src/main/kotlin/api/masterplan/app/userManagementModule/application/service/UserServiceImpl.kt
@@ -4,6 +4,7 @@ import api.masterplan.app.userManagementModule.domain.dtos.AppUserDetails
 import api.masterplan.app.userManagementModule.domain.exceprions.UserManagementException
 import api.masterplan.app.userManagementModule.domain.interfaces.UserRepository
 import api.masterplan.app.userManagementModule.domain.interfaces.UserService
+import api.masterplan.app.userManagementModule.domain.models.entity.AppUser
 import api.masterplan.app.userManagementModule.domain.models.value.UserId
 import api.masterplan.app.userManagementModule.domain.models.value.UserLogin
 import api.masterplan.app.userManagementModule.domain.models.value.UserPassword
@@ -35,20 +36,57 @@ class UserServiceImpl(
         }
     }
 
-    override fun editUser(userId: UserId, newUserData: AppUserDetails): Result<UserId> {
-        TODO("Not yet implemented")
+    override fun resetPasswordForUser(userId: UserId, newPassword: UserPassword): Result<UserId> {
+        return try {
+
+            val editedUserId = userRepository.setPassword(userId,newPassword) ?: throw UserManagementException.FailedToResetPasswordForUser(userId)
+            logger.info("User with id=${userId.value} updated")
+            Result.success(editedUserId)
+        }catch (e: Exception){
+            logger.warn("Failed to update user with id =${userId.value}", e)
+            Result.failure(e)
+        }
     }
 
     override fun createUser(login: UserLogin, password: UserPassword, roles: Set<UserRole>): Result<UserId> {
-        TODO("Not yet implemented")
+        return try {
+            val newUser = AppUser.create(login = login, rawPassword = password, roles = roles)
+            val userId = userRepository.saveUser(newUser) ?: throw UserManagementException.FailedToCreateUserException(login)
+            logger.info("User with id=${userId.value} saved")
+            Result.success(userId)
+        }catch (e: Exception){
+            logger.warn("Failed to save user with login =${login.value}", e)
+            Result.failure(e)
+        }
     }
 
     override fun getUser(userId: UserId): Result<AppUserDetails> {
-        TODO("Not yet implemented")
+        return try {
+            val user = userRepository.getUser(userId) ?: throw UserManagementException.UserNotExistsException(userId)
+            val userDetails = AppUserDetails(
+                id = user.id,
+                login = user.login,
+                password = user.password,
+                roles = user.roles
+            )
+            logger.info("Get user with id=${userId.value} ")
+            Result.success(userDetails)
+        }catch (e: Exception){
+            logger.warn("Failed to get user with id =${userId.value}", e)
+            Result.failure(e)
+        }
     }
 
     override fun deleteUser(userId: UserId): Result<UserId> {
-        TODO("Not yet implemented")
+        return try {
+            val deletedUserId = userRepository.deleteUser(userId) ?: throw UserManagementException.FailedToDeleteUserException(userId)
+            logger.info("Delete user with id=${userId.value} ")
+            Result.success(deletedUserId)
+        }catch (e: Exception){
+            logger.warn("Failed to delete user with id =${userId.value}", e)
+            Result.failure(e)
+        }
+
     }
 
 
