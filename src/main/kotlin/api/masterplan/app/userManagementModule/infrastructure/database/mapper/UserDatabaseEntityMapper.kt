389c4a71588b48741804ef4c921a08b4e6afde55
Leturgone <finekon221@gmail.com>
2026-02-11 22:33:28 +0300

    add toEntity mapper in UserDatabaseEntityMapper

--- src/main/kotlin/api/masterplan/app/userManagementModule/infrastructure/database/mapper/UserDatabaseEntityMapper.kt
+++ src/main/kotlin/api/masterplan/app/userManagementModule/infrastructure/database/mapper/UserDatabaseEntityMapper.kt
@@ -5,8 +5,10 @@ import api.masterplan.app.userManagementModule.domain.models.value.UserId
 import api.masterplan.app.userManagementModule.domain.models.value.UserLogin
 import api.masterplan.app.userManagementModule.domain.models.value.UserPassword
 import api.masterplan.app.userManagementModule.infrastructure.database.entity.AppUserEntity
+import api.masterplan.app.userManagementModule.infrastructure.database.entity.RoleEntity
 
 object UserDatabaseEntityMapper {
+
     fun toDomain(databaseEntity: AppUserEntity): AppUser {
         val domainRoles = RoleDatabaseMapper.toDomain(databaseEntity.roles)
         return AppUser.create(
@@ -14,6 +16,16 @@ object UserDatabaseEntityMapper {
             login = UserLogin(databaseEntity.login),
             rawPassword = UserPassword(databaseEntity.passwordHash),
             roles = domainRoles
+        )
+    }
+
+    fun toEntity(domainUser: AppUser,databaseRoles: HashSet<RoleEntity>): AppUserEntity{
+        val entityRoles = RoleDatabaseMapper.toEntity(domainUser.roles, databaseRoles)
+        return AppUserEntity(
+            id = domainUser.id.value,
+            login = domainUser.login.value,
+            passwordHash = domainUser.password.value,
+            roles = entityRoles
         )
     }
 }
