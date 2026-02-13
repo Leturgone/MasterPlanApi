9236a51d3a9401633a9cce51bb2d250550eb4cfb
Leturgone <finekon221@gmail.com>
2026-02-13 17:35:41 +0300

    remove try catch from UserServiceImpl

--- src/main/kotlin/api/masterplan/app/userManagementModule/application/service/UserServiceImpl.kt
+++ src/main/kotlin/api/masterplan/app/userManagementModule/application/service/UserServiceImpl.kt
@@ -19,74 +19,91 @@ class UserServiceImpl(
 
     private val  logger = LoggerFactory.getLogger(this::class.java)
 
-    override fun getUserByLogin(login: UserLogin): Result<AppUserDetails> {
-        return try {
-            val user = userRepository.findByLogin(login) ?: throw UserManagementException.UserNotFoundException(login)
-            val appUserDetails = AppUserDetails(
-                id = user.id,
-                login = login,
-                password = user.password,
-                roles = user.roles
-            )
-            logger.info("User with login=${login.value} found")
-            Result.success(appUserDetails)
-        }catch (e: Exception){
-            logger.warn("User with login =${login.value} not found", e)
-            Result.failure(e)
-        }
+    override fun getUserByLogin(login: UserLogin): AppUserDetails {
+//        return try {
+//            val user = userRepository.findByLogin(login) ?: throw UserManagementException.UserNotFoundException(login)
+//            val appUserDetails = AppUserDetails(
+//                id = user.id,
+//                login = login,
+//                password = user.password,
+//                roles = user.roles
+//            )
+//            logger.info("User with login=${login.value} found")
+//            Result.success(appUserDetails)
+//        }catch (e: Exception){
+//            logger.warn("User with login =${login.value} not found", e)
+//            Result.failure(e)
+//        }
+        val user = userRepository.findByLogin(login) ?: throw UserManagementException.UserNotFoundException(login)
+        val appUserDetails = AppUserDetails(
+            id = user.id,
+            login = login,
+            password = user.password,
+            roles = user.roles
+        )
+        return appUserDetails
     }
 
-    override fun resetPasswordForUser(userId: UserId, newPassword: UserPassword): Result<UserId> {
-        return try {
-
-            val editedUserId = userRepository.setPassword(userId,newPassword) ?: throw UserManagementException.FailedToResetPasswordForUser(userId)
-            logger.info("User with id=${userId.value} updated")
-            Result.success(editedUserId)
-        }catch (e: Exception){
-            logger.warn("Failed to update user with id =${userId.value}", e)
-            Result.failure(e)
-        }
+    override fun resetPasswordForUser(userId: UserId, newPassword: UserPassword): UserId {
+//        return try {
+//            val editedUserId = userRepository.setPassword(userId,newPassword) ?: throw UserManagementException.FailedToResetPasswordForUser(userId)
+//            logger.info("User with id=${userId.value} updated")
+//            Result.success(editedUserId)
+//        }catch (e: Exception){
+//            logger.warn("Failed to update user with id =${userId.value}", e)
+//            Result.failure(e)
+//        }
+        val editedUserId = userRepository.setPassword(userId,newPassword) ?: throw UserManagementException.FailedToResetPasswordForUser(userId)
+        return editedUserId
     }
 
-    override fun createUser(login: UserLogin, password: UserPassword, roles: Set<UserRole>): Result<UserId> {
-        return try {
-            val newUser = AppUser.create(login = login, rawPassword = password, roles = roles)
-            val userId = userRepository.saveUser(newUser) ?: throw UserManagementException.FailedToCreateUserException(login)
-            logger.info("User with id=${userId.value} saved")
-            Result.success(userId)
-        }catch (e: Exception){
-            logger.warn("Failed to save user with login =${login.value}", e)
-            Result.failure(e)
-        }
+    override fun createUser(login: UserLogin, password: UserPassword, roles: Set<UserRole>): UserId {
+//        return try {
+//            if (userRepository.isUserExist(login)) return Result.failure(UserManagementException.UserAlreadyExistsException(login))
+//            val newUser = AppUser.create(login = login, rawPassword = password, roles = roles)
+//            val userId = userRepository.saveUser(newUser) ?: throw UserManagementException.FailedToCreateUserException(login)
+//            logger.info("User with id=${userId.value} saved")
+//            Result.success(userId)
+//        }catch (e: Exception){
+//            logger.warn("Failed to save user with login =${login.value}", e)
+//            Result.failure(e)
+//        }
+        if (userRepository.isUserExist(login)) throw UserManagementException.UserAlreadyExistsException(login)
+        val newUser = AppUser.create(login = login, rawPassword = password, roles = roles)
+        val userId = userRepository.saveUser(newUser) ?: throw UserManagementException.FailedToCreateUserException(login)
+        return userId
     }
 
-    override fun getUser(userId: UserId): Result<AppUserDetails> {
-        return try {
-            val user = userRepository.getUser(userId) ?: throw UserManagementException.UserNotExistsException(userId)
-            val userDetails = AppUserDetails(
-                id = user.id,
-                login = user.login,
-                password = user.password,
-                roles = user.roles
-            )
-            logger.info("Get user with id=${userId.value} ")
-            Result.success(userDetails)
-        }catch (e: Exception){
-            logger.warn("Failed to get user with id =${userId.value}", e)
-            Result.failure(e)
-        }
+    override fun getUser(userId: UserId): AppUserDetails {
+//        return try {
+//
+//            logger.info("Get user with id=${userId.value} ")
+//            Result.success(userDetails)
+//        }catch (e: Exception){
+//            logger.warn("Failed to get user with id =${userId.value}", e)
+//            Result.failure(e)
+//        }
+        val user = userRepository.getUser(userId) ?: throw UserManagementException.UserNotExistsException(userId)
+        val userDetails = AppUserDetails(
+            id = user.id,
+            login = user.login,
+            password = user.password,
+            roles = user.roles
+        )
+        return userDetails
     }
 
-    override fun deleteUser(userId: UserId): Result<UserId> {
-        return try {
-            val deletedUserId = userRepository.deleteUser(userId) ?: throw UserManagementException.FailedToDeleteUserException(userId)
-            logger.info("Delete user with id=${userId.value} ")
-            Result.success(deletedUserId)
-        }catch (e: Exception){
-            logger.warn("Failed to delete user with id =${userId.value}", e)
-            Result.failure(e)
-        }
-
+    override fun deleteUser(userId: UserId): UserId {
+//        return try {
+//
+//            logger.info("Delete user with id=${userId.value} ")
+//            Result.success(deletedUserId)
+//        }catch (e: Exception){
+//            logger.warn("Failed to delete user with id =${userId.value}", e)
+//            Result.failure(e)
+//        }
+        val deletedUserId = userRepository.deleteUser(userId) ?: throw UserManagementException.FailedToDeleteUserException(userId)
+        return deletedUserId
     }
 
 
