a6e2b0e414bdb0b54bde6f9922ab59f973e07f7d
Leturgone <finekon221@gmail.com>
2026-02-13 23:14:54 +0300

    add LoggingConfig

---
+++ src/main/kotlin/api/masterplan/app/logging/config/LoggingConfig.kt
@@ -0,0 +1,9 @@
+package api.masterplan.app.logging.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.EnableAspectJAutoProxy
+
+@Configuration
+@EnableAspectJAutoProxy
+class LoggingConfig {
+}
