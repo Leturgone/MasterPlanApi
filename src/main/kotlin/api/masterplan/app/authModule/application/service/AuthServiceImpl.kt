721cb67f3c55ca2647568258f1fed5cea459a528
Leturgone <finekon221@gmail.com>
2026-02-13 23:16:46 +0300

    add Logging annotation to service

--- src/main/kotlin/api/masterplan/app/authModule/application/service/AuthServiceImpl.kt
+++ src/main/kotlin/api/masterplan/app/authModule/application/service/AuthServiceImpl.kt
@@ -1,11 +1,11 @@
 package api.masterplan.app.authModule.application.service
 
-import api.masterplan.app.authModule.domain.dto.UserAuthCredentials
 import api.masterplan.app.authModule.application.ports.UserCredentialsProvider
+import api.masterplan.app.authModule.domain.dto.UserAuthCredentials
 import api.masterplan.app.authModule.domain.interfaces.AuthService
 import api.masterplan.app.authModule.domain.model.value.AuthUserLogin
 import api.masterplan.app.authModule.domain.model.value.AuthUserPassword
-import org.slf4j.LoggerFactory
+import api.masterplan.app.logging.LoggingMethod
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,22 +13,12 @@ class AuthServiceImpl(
     private val userCredentialsProvider: UserCredentialsProvider,
 ): AuthService {
 
-    private val  logger = LoggerFactory.getLogger(this::class.java)
 
+    @LoggingMethod
     override fun authenticate(
         login: AuthUserLogin,
         password: AuthUserPassword
     ): UserAuthCredentials {
-//        return try {
-//            val userAuthCredentials = userCredentialsProvider.validateCredentials(login,password)
-//            logger.info("Authentication success for login=${login.value}")
-//
-//            Result.success(userAuthCredentials)
-//
-//        }catch (e: Exception){
-//            logger.warn("Authentication failed for login=${login.value}", e)
-//            Result.failure(e)
-//        }
         val userAuthCredentials = userCredentialsProvider.validateCredentials(login,password)
         return userAuthCredentials
     }
