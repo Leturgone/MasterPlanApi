3129bfb622f428cb35d9ea8c9659c4e1199c76d8
Leturgone <finekon221@gmail.com>
2026-02-14 21:16:26 +0300

    add module name to annotation

--- src/main/kotlin/api/masterplan/app/logging/LoggingMethod.kt
+++ src/main/kotlin/api/masterplan/app/logging/LoggingMethod.kt
@@ -3,5 +3,5 @@ package api.masterplan.app.logging
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class LoggingMethod(
-    val value: String = "",
+    val moduleName: String = "",
 )
