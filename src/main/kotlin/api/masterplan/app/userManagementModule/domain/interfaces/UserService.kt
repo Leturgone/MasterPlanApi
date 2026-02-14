62cb984f2bd1fa066befefc18ca5fafc4d903eba
Leturgone <finekon221@gmail.com>
2026-02-14 20:52:31 +0300

    add employee info to service

--- src/main/kotlin/api/masterplan/app/userManagementModule/domain/interfaces/UserService.kt
+++ src/main/kotlin/api/masterplan/app/userManagementModule/domain/interfaces/UserService.kt
@@ -1,5 +1,6 @@
 package api.masterplan.app.userManagementModule.domain.interfaces
 
+import api.masterplan.app.userManagementModule.application.dto.EmployeeInfo
 import api.masterplan.app.userManagementModule.domain.dtos.AppUserDetails
 import api.masterplan.app.userManagementModule.domain.models.value.UserId
 import api.masterplan.app.userManagementModule.domain.models.value.UserLogin
@@ -12,7 +13,7 @@ interface UserService {
 
     fun resetPasswordForUser(userId: UserId, newPassword: UserPassword): UserId
 
-    fun createUser(login: UserLogin,password: UserPassword, roles: Set<UserRole>): UserId
+    fun createUser(login: UserLogin,password: UserPassword, roles: Set<UserRole>, employeeInfo: EmployeeInfo): UserId
 
     fun getUser(userId: UserId): AppUserDetails
 
