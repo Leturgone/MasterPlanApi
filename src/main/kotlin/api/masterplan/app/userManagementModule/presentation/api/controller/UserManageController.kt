7bcff17d12ca75f63fe614137d5d3a803ef490f3
Leturgone <finekon221@gmail.com>
2026-02-14 21:21:51 +0300

    add UserManageController

---
+++ src/main/kotlin/api/masterplan/app/userManagementModule/presentation/api/controller/UserManageController.kt
@@ -0,0 +1,177 @@
+package api.masterplan.app.userManagementModule.presentation.api.controller
+
+import api.masterplan.app.userManagementModule.application.command.CreateUserCommand
+import api.masterplan.app.userManagementModule.application.command.DeleteUserCommand
+import api.masterplan.app.userManagementModule.application.command.GetUserByLoginCommand
+import api.masterplan.app.userManagementModule.application.command.ResetPasswordCommand
+import api.masterplan.app.userManagementModule.application.command.ValidateCredentialsCommand
+import api.masterplan.app.userManagementModule.application.dto.EmployeeInfo
+import api.masterplan.app.userManagementModule.application.usecase.*
+import api.masterplan.app.userManagementModule.domain.dtos.AppUserDetails
+import api.masterplan.app.userManagementModule.domain.exceprions.UserManagementException
+import api.masterplan.app.userManagementModule.domain.models.value.UserId
+import api.masterplan.app.userManagementModule.presentation.dto.request.CreateProfileRequest
+import api.masterplan.app.userManagementModule.presentation.dto.request.ResetPasswordRequest
+import api.masterplan.app.userManagementModule.presentation.dto.request.ValidateCredentialsRequest
+import api.masterplan.app.userManagementModule.presentation.dto.responce.UserDataResponse
+import api.masterplan.app.userManagementModule.presentation.dto.responce.UserUidResponse
+import api.masterplan.app.userManagementModule.presentation.mapper.UserDomainToResponseMapper
+import api.masterplan.app.userManagementModule.presentation.mapper.UserExceptionToHttpCodeMapper
+import api.masterplan.app.userManagementModule.presentation.mapper.UserRequestToDomainMapper
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+import java.util.*
+
+@RestController
+@RequestMapping("/api/v1/admin/users")
+@Tag(name = "Users", description = "Управление пользователями")
+class UserManageController(
+    private val createUserUseCase: CreateUserUseCase,
+    private val deleteUserUseCase: DeleteUserUseCase,
+    private val getUserByLoginUseCase: GetUserByLoginUseCase,
+    private val resetPasswordUseCase: ResetPasswordUseCase,
+    private val validateCredentialsUseCase: ValidateCredentialsUseCase
+) {
+
+    @PostMapping("/createProfile")
+    fun createUser(@RequestBody request: CreateProfileRequest): ResponseEntity<UserUidResponse>{
+        val employeeInfo = EmployeeInfo(
+            name = request.name,
+            surname = request.surname,
+            patronymic = request.patronymic,
+            directorId = request.directorId,
+        )
+
+        val command = try {
+            val loginValidated = UserRequestToDomainMapper.loginToDomain(request.login)
+            val passwordValidated = UserRequestToDomainMapper.passwordToDomain(request.password)
+            val rolesValidated = UserRequestToDomainMapper.rolesToDomain(request.roles)
+            CreateUserCommand(
+                login = loginValidated,
+                password = passwordValidated,
+                roles = rolesValidated,
+                employeeInfo = employeeInfo,
+            )
+        }catch (e: UserManagementException){
+            return handleExceptionUserId(e)
+        }
+
+        return createUserUseCase(command).handleUserIdResult()
+    }
+
+    @DeleteMapping("/delete/")
+    fun deleteUser(@RequestParam(value = "id")id: UUID): ResponseEntity<UserUidResponse> {
+
+        val command = try {
+            DeleteUserCommand(UserRequestToDomainMapper.idToDomain(id))
+        }catch (e: UserManagementException.InvalidUserCredentialsException){
+            return handleExceptionUserId(e)
+        }
+
+        return deleteUserUseCase(command).handleUserIdResult()
+
+    }
+
+
+    @GetMapping("/getByLogin/")
+    fun getUserByLogin(@RequestParam(value = "login") login: String): ResponseEntity<UserDataResponse> {
+        val command = try {
+            val loginValidated = UserRequestToDomainMapper.loginToDomain(login)
+            GetUserByLoginCommand(loginValidated)
+        }catch (e: UserManagementException.InvalidUserCredentialsException){
+            return handleExceptionUserData(e)
+        }
+
+        return getUserByLoginUseCase(command).handleUserResult()
+    }
+
+
+    @PatchMapping("/resetPassword/")
+    fun resetPassword(@RequestBody request: ResetPasswordRequest): ResponseEntity<UserUidResponse>{
+        val id = UserId(request.userId)
+        val command = try {
+            val passwordValidated = UserRequestToDomainMapper.passwordToDomain(request.password)
+            ResetPasswordCommand(id,passwordValidated)
+        }catch (e: UserManagementException.InvalidUserCredentialsException){
+            return handleExceptionUserId(e)
+        }
+
+        return resetPasswordUseCase(command).handleUserIdResult()
+    }
+
+
+    @PostMapping("/validateCredentials/")
+    fun validateCredentials(@RequestBody credentials: ValidateCredentialsRequest):ResponseEntity<UserDataResponse>{
+        val command = try {
+            val loginValidated = UserRequestToDomainMapper.loginToDomain(credentials.login)
+            val passwordValidated = UserRequestToDomainMapper.passwordToDomain(credentials.password)
+            ValidateCredentialsCommand(loginValidated,passwordValidated)
+        }catch (e: UserManagementException.InvalidUserCredentialsException){
+            return handleExceptionUserData(e)
+        }
+
+        return validateCredentialsUseCase(command).handleUserResult()
+    }
+
+
+
+    private fun Result<AppUserDetails>.handleUserResult(): ResponseEntity<UserDataResponse> = this.fold(
+            onSuccess = { user ->
+                val resp = UserDomainToResponseMapper.userToResponse(user)
+                ResponseEntity.ok(resp)
+            },
+            onFailure = { error ->
+                val status = UserExceptionToHttpCodeMapper.exceptionToHttpCode(error)
+                val body = UserDataResponse.Error(
+                    status = status.value(),
+                    message = error.message,
+                    timestamp = LocalDateTime.now()
+                )
+                ResponseEntity.status(status).body(body)
+            }
+        )
+
+
+    private fun Result<UserId>.handleUserIdResult(): ResponseEntity<UserUidResponse> = this.fold(
+        onSuccess = {uid ->
+            ResponseEntity.ok(UserUidResponse.Success(
+                uid.value
+            ))
+        },
+        onFailure = {error ->
+            val status = UserExceptionToHttpCodeMapper.exceptionToHttpCode(error)
+            val body = UserUidResponse.Error(
+                status = status.value(),error.message, LocalDateTime.now()
+            )
+            ResponseEntity.status(status).body(body)
+        }
+    )
+
+
+    private fun handleExceptionUserId(e: UserManagementException): ResponseEntity<UserUidResponse> {
+        val status = UserExceptionToHttpCodeMapper.exceptionToHttpCode(e)
+        val body = UserUidResponse.Error(
+            status = status.value(),
+            message = e.message,
+            timestamp = LocalDateTime.now()
+        )
+        return ResponseEntity.status(status).body(body)
+    }
+
+
+    private fun handleExceptionUserData(e: UserManagementException): ResponseEntity<UserDataResponse> {
+        val status = UserExceptionToHttpCodeMapper.exceptionToHttpCode(e)
+        val body = UserDataResponse.Error(
+            status = status.value(),
+            message = e.message,
+            timestamp = LocalDateTime.now()
+        )
+        return ResponseEntity.status(status).body(body)
+    }
+
+
+
+
+}
