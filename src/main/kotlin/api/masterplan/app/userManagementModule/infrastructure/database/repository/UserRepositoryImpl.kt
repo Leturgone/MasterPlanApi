d61b83e3543cf04321f4d235dbba8de6201261e5
Leturgone <finekon221@gmail.com>
2026-02-14 21:19:49 +0300

    add database logging to UserRepositoryImpl and also transfer checks to service

--- src/main/kotlin/api/masterplan/app/userManagementModule/infrastructure/database/repository/UserRepositoryImpl.kt
+++ src/main/kotlin/api/masterplan/app/userManagementModule/infrastructure/database/repository/UserRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package api.masterplan.app.userManagementModule.infrastructure.database.repository
 
+import api.masterplan.app.logging.LoggingDatabaseMethod
 import api.masterplan.app.userManagementModule.domain.interfaces.UserRepository
 import api.masterplan.app.userManagementModule.domain.models.entity.AppUser
 import api.masterplan.app.userManagementModule.domain.models.value.UserId
@@ -7,7 +8,6 @@ import api.masterplan.app.userManagementModule.domain.models.value.UserLogin
 import api.masterplan.app.userManagementModule.domain.models.value.UserPassword
 import api.masterplan.app.userManagementModule.infrastructure.database.mapper.UserDatabaseEntityMapper
 import org.springframework.stereotype.Repository
-import org.springframework.transaction.annotation.Transactional
 import kotlin.jvm.optionals.getOrNull
 
 @Repository
@@ -17,17 +17,20 @@ class UserRepositoryImpl(
 ): UserRepository {
 
 
+    @LoggingDatabaseMethod(moduleName = "userManagementModule")
     override fun findByLogin(login: UserLogin): AppUser? {
         val userEntity = jpaUserRepository.findByLogin(login.value)?: return null
         val domainUser = UserDatabaseEntityMapper.toDomain(userEntity)
         return domainUser
     }
 
+    @LoggingDatabaseMethod(moduleName = "userManagementModule")
     override fun isUserExist(login: UserLogin): Boolean {
         return jpaUserRepository.existsByLogin(login.value)
     }
 
 
+    @LoggingDatabaseMethod(moduleName = "userManagementModule")
     override fun setPassword(userId: UserId, newPassword: UserPassword): UserId? {
         val userEntity = jpaUserRepository.findById(userId.value).getOrNull() ?: return null
         val userWithNewPasswordEntity = userEntity.copy(passwordHash = newPassword.value)
@@ -35,6 +38,7 @@ class UserRepositoryImpl(
     }
 
 
+    @LoggingDatabaseMethod(moduleName = "userManagementModule")
     override fun saveUser(user: AppUser): UserId? {
         val roles = jpaRoleRepository.findAll().toSet()
         val databaseEntity = UserDatabaseEntityMapper.toEntity(user,roles)
@@ -42,6 +46,7 @@ class UserRepositoryImpl(
         return userid
     }
 
+    @LoggingDatabaseMethod(moduleName = "userManagementModule")
     override fun getUser(userId: UserId): AppUser? {
         val userEntity = jpaUserRepository.findById(userId.value).getOrNull() ?: return null
         val domainUser = UserDatabaseEntityMapper.toDomain(userEntity)
@@ -49,9 +54,9 @@ class UserRepositoryImpl(
     }
 
 
-    override fun deleteUser(userId: UserId): UserId? {
-        val userEntity = jpaUserRepository.findById(userId.value).getOrNull() ?: return null
-        jpaUserRepository.delete(userEntity)
+    @LoggingDatabaseMethod(moduleName = "userManagementModule")
+    override fun deleteUser(userId: UserId): UserId {
+        jpaUserRepository.deleteById(userId.value)
         return userId
     }
 }
