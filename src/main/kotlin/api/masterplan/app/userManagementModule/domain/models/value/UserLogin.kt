a0dcb183ef5e081b834f833461b748e754db4fdd
Leturgone <finekon221@gmail.com>
2026-02-14 20:45:50 +0300

    add domain error throw to validate method in UserLogin and UserPassword

--- src/main/kotlin/api/masterplan/app/userManagementModule/domain/models/value/UserLogin.kt
+++ src/main/kotlin/api/masterplan/app/userManagementModule/domain/models/value/UserLogin.kt
@@ -1,11 +1,17 @@
 package api.masterplan.app.userManagementModule.domain.models.value
 
+import api.masterplan.app.userManagementModule.domain.exceprions.UserManagementException
+
 @JvmInline
 value class UserLogin(val value: String){
     companion object{
         fun validate(login: String): UserLogin {
-            require(login.isNotBlank()) {"Login cant be blank"}
-            require(login.length <= 255) { "Login too long" }
+            try {
+                require(login.isNotBlank()) {"Login cant be blank"}
+                require(login.length <= 255) { "Login too long" }
+            }catch (e:IllegalArgumentException){
+                throw UserManagementException.InvalidUserCredentialsException(e.message)
+            }
             return UserLogin(login.lowercase())
         }
     }
--- src/main/kotlin/api/masterplan/app/userManagementModule/domain/models/value/UserPassword.kt
+++ src/main/kotlin/api/masterplan/app/userManagementModule/domain/models/value/UserPassword.kt
@@ -1,14 +1,18 @@
 package api.masterplan.app.userManagementModule.domain.models.value
 
+import api.masterplan.app.userManagementModule.domain.exceprions.UserManagementException
+
 @JvmInline
 value class UserPassword(val value: String){
     companion object {
         fun validate(password: String): UserPassword{
-
-            require(password.isNotBlank()) {"Password cant be blank"}
-            require(password.length >= 8) { "Password too short" }
-            require(password.length <= 255) { "Password too long" }
-
+            try {
+                require(password.isNotBlank()) {"Password cant be blank"}
+                require(password.length >= 8) { "Password too short" }
+                require(password.length <= 255) { "Password too long" }
+            }catch (e: IllegalArgumentException){
+                throw UserManagementException.InvalidUserCredentialsException(e.message)
+            }
             return UserPassword(password)
         }
     }
