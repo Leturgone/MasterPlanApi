9bd9a82662328f633430c5a22b56e0e580c91c85
Leturgone <finekon221@gmail.com>
2026-02-14 17:35:21 +0300

    add UserExceptionToHttpCodeMapper

---
+++ src/main/kotlin/api/masterplan/app/userManagementModule/presentation/mapper/UserExceptionToHttpCodeMapper.kt
@@ -0,0 +1,20 @@
+package api.masterplan.app.userManagementModule.presentation.mapper
+
+import api.masterplan.app.userManagementModule.domain.exceprions.UserManagementException
+import org.springframework.http.HttpStatus
+
+object UserExceptionToHttpCodeMapper {
+    fun exceptionToHttpCode(ex: Throwable): HttpStatus {
+        return when (ex) {
+            is UserManagementException.UserNotFoundException -> HttpStatus.NOT_FOUND
+            is UserManagementException.UserNotExistsException -> HttpStatus.BAD_REQUEST
+            is UserManagementException.UserAlreadyExistsException -> HttpStatus.CONFLICT
+            is UserManagementException.FailedToCreateUserException -> HttpStatus.INTERNAL_SERVER_ERROR
+            is UserManagementException.InvalidUserCredentialsException -> HttpStatus.BAD_REQUEST
+            is UserManagementException.InvalidRoleTitle -> HttpStatus.BAD_REQUEST
+            is UserManagementException.FailedToResetPasswordForUser -> HttpStatus.INTERNAL_SERVER_ERROR
+            is UserManagementException.FailedToDeleteUserException -> HttpStatus.INTERNAL_SERVER_ERROR
+            else -> HttpStatus.INTERNAL_SERVER_ERROR
+        }
+    }
+}
