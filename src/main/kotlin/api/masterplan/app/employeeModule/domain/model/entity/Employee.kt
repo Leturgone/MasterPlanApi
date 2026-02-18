0dcfc2d8be40d14c9bdfaa0a23c30854deba7f4e
Leturgone <finekon221@gmail.com>
2026-02-18 21:21:18 +0300

    add EmployeeUserId to Employee and EmployeeDetails

--- src/main/kotlin/api/masterplan/app/employeeModule/domain/model/entity/Employee.kt
+++ src/main/kotlin/api/masterplan/app/employeeModule/domain/model/entity/Employee.kt
@@ -4,6 +4,7 @@ import api.masterplan.app.employeeModule.domain.model.value.EmployeeId
 import api.masterplan.app.employeeModule.domain.model.value.EmployeeName
 import api.masterplan.app.employeeModule.domain.model.value.EmployeePatronymic
 import api.masterplan.app.employeeModule.domain.model.value.EmployeeSurname
+import api.masterplan.app.employeeModule.domain.model.value.EmployeeUserId
 
 @ConsistentCopyVisibility
 data class Employee private constructor(
@@ -11,18 +12,20 @@ data class Employee private constructor(
     val name: EmployeeName,
     val surname: EmployeeSurname,
     val patronymic: EmployeePatronymic? = null,
-    val directorId: EmployeeId? = null
+    val directorId: EmployeeId? = null,
+    val userId: EmployeeUserId
 ){
     companion object{
         fun create(id: EmployeeId? = null, name: EmployeeName,
                    surname: EmployeeSurname, patronymic: EmployeePatronymic? = null,
-                   directorId: EmployeeId? = null): Employee {
+                   directorId: EmployeeId? = null, userId: EmployeeUserId): Employee {
             return Employee(
                 id = id ?: EmployeeId.generate(),
                 name = name,
                 surname = surname,
                 patronymic = patronymic,
-                directorId = directorId
+                directorId = directorId,
+                userId = userId
             )
         }
     }
