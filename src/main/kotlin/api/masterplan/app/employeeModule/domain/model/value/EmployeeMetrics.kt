32aed204111c9811bb0ef580f34a7539f0302cf3
Leturgone <finekon221@gmail.com>
2026-02-20 21:56:05 +0300

    add new field to EmployeeMetrics

--- src/main/kotlin/api/masterplan/app/employeeModule/domain/model/value/EmployeeMetrics.kt
+++ src/main/kotlin/api/masterplan/app/employeeModule/domain/model/value/EmployeeMetrics.kt
@@ -3,5 +3,5 @@ package api.masterplan.app.employeeModule.domain.model.value
 data class EmployeeMetrics(
     val rating: Double,
     val workload: Double,
-    val completedTasksCount: Int
+    val assignedTasksCount: Int
 )
