361dc83a562e0d2527674e3fdf24919fcc5cd4aa
Leturgone <finekon221@gmail.com>
2026-02-22 01:25:31 +0300

    add JpaEmployeeRepository

---
+++ src/main/kotlin/api/masterplan/app/employeeModule/infrastructure/database/repository/JpaEmployeeRepository.kt
@@ -0,0 +1,32 @@
+package api.masterplan.app.employeeModule.infrastructure.database.repository
+
+import api.masterplan.app.employeeModule.infrastructure.database.entity.EmployeeEntity
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface JpaEmployeeRepository: CrudRepository<EmployeeEntity, UUID> {
+    @Query("SELECT e FROM EmployeeEntity e WHERE " +
+            "LOWER(e.name) LIKE LOWER(CONCAT('%', :name, '%')) OR " +
+            "LOWER(e.surname) LIKE LOWER(CONCAT('%', :surname, '%'))")
+    fun searchByNameOrSurname(@Param("name") name: String,
+                              @Param("surname") surname: String): List<EmployeeEntity>
+
+    @Query("SELECT e FROM EmployeeEntity e WHERE " +
+            "e.directorId = :directorId AND " +
+            "(LOWER(e.name) LIKE LOWER(CONCAT('%', :name, '%')) OR " +
+            "LOWER(e.surname) LIKE LOWER(CONCAT('%', :surname, '%')))")
+    fun searchByNameOrSurnameAndDirId(@Param("name") name: String,
+                                      @Param("surname") surname: String,
+                                      @Param("directorId") directorId: UUID): List<EmployeeEntity>
+
+    @Query("SELECT e FROM EmployeeEntity e WHERE e.directorId = :directorId")
+    fun getByDirectorId(@Param("directorId") directorId: UUID): List<EmployeeEntity>
+
+    @Query("SELECT COUNT(e) > 0 FROM EmployeeEntity e WHERE e.appUserId = :userId")
+    fun existsByUserId(userId: UUID): Boolean
+
+}
