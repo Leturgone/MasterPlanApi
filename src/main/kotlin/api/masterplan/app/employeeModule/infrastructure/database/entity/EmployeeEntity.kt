f1f3cb9887fa4009f5c84d2a7319e99ac4e427db
Leturgone <finekon221@gmail.com>
2026-02-21 18:36:38 +0300

    add EmployeeEntity

---
+++ src/main/kotlin/api/masterplan/app/employeeModule/infrastructure/database/entity/EmployeeEntity.kt
@@ -0,0 +1,53 @@
+package api.masterplan.app.employeeModule.infrastructure.database.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.ForeignKey
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import java.util.UUID
+
+
+@Entity
+@Table(name = "app_user")
+data class EmployeeEntity(
+    @Id
+    @Column(name = "id")
+    val id: UUID,
+
+    @Column(name = "name", length = 45, nullable = false)
+    val name: String,
+
+    @Column(name = "surname", length = 100, nullable = false)
+    val surname: String,
+
+    @Column(name = "patronymic", length = 45)
+    val patronymic: String? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+        name = "director_id",
+        referencedColumnName = "id",
+        foreignKey = ForeignKey(name = "fk_employee_director")
+    )
+    var director: EmployeeEntity? = null,
+
+    @Column(name = "app_user_id",nullable = false, unique = true)
+    val appUserId: UUID,
+
+){
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as EmployeeEntity
+        return  id == other.id
+    }
+    override fun hashCode(): Int = id.hashCode()
+
+    override fun toString(): String{
+        return "EmployeeEntity(id=$id, name='$name', surname='$surname',patronymic=${patronymic?:""})"
+    }
+}
