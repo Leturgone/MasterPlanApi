76e5f4858e9115deea85f1eed2e24ae8838a11ae
Leturgone <finekon221@gmail.com>
2026-02-22 21:12:19 +0300

    add DirectorDetailsDto

---
+++ src/main/kotlin/api/masterplan/app/employeeModule/presentation/dto/responce/DirectorDetailsDto.kt
@@ -0,0 +1,7 @@
+package api.masterplan.app.employeeModule.presentation.dto.responce
+
+data class DirectorDetailsDto(
+    val name: String,
+    val surname: String,
+    val patronymic: String? = null,
+)
