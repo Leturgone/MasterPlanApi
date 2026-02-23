7330ce91194f273313c72505a0593ac1ece24844
Leturgone <finekon221@gmail.com>
2026-02-24 00:26:00 +0300

    add idToResponse in UserDomainToResponseMapper

--- src/main/kotlin/api/masterplan/app/userManagementModule/presentation/mapper/UserDomainToResponseMapper.kt
+++ src/main/kotlin/api/masterplan/app/userManagementModule/presentation/mapper/UserDomainToResponseMapper.kt
@@ -1,16 +1,20 @@
 package api.masterplan.app.userManagementModule.presentation.mapper
 
 import api.masterplan.app.userManagementModule.domain.dtos.AppUserDetails
+import api.masterplan.app.userManagementModule.domain.models.value.UserId
 import api.masterplan.app.userManagementModule.presentation.dto.responce.UserDataResponse
+import api.masterplan.app.userManagementModule.presentation.dto.responce.UserUidResponse
 
 object UserDomainToResponseMapper {
 
-    fun userToResponse(user: AppUserDetails): UserDataResponse.Success {
-        return UserDataResponse.Success(
+    fun userToResponse(user: AppUserDetails): UserDataResponse{
+        return UserDataResponse(
             id = user.id.value,
             login = user.login.value,
             password = user.password.value,
             roles = user.roles.map { it.name }.toSet(),
         )
     }
+
+    fun idToResponse(id: UserId): UserUidResponse = UserUidResponse(id.value)
 }
