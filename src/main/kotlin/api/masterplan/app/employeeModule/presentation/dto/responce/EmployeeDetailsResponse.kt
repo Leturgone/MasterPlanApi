9fdc167ed8f076bb2ed3ebd9d8d7aa00a2c6629f
Leturgone <finekon221@gmail.com>
2026-02-23 21:27:26 +0300

    remove sealed

--- src/main/kotlin/api/masterplan/app/employeeModule/presentation/dto/responce/EmployeeDetailsResponse.kt
+++ src/main/kotlin/api/masterplan/app/employeeModule/presentation/dto/responce/EmployeeDetailsResponse.kt
@@ -1,39 +1,20 @@
 package api.masterplan.app.employeeModule.presentation.dto.responce
 
 import io.swagger.v3.oas.annotations.media.Schema
-import java.time.LocalDateTime
 import java.util.*
 
 @Schema(description = "Ответ Данные Сотрудника")
-sealed class EmployeeDetailsResponse private constructor(){
-
-    @Schema(description = "Удачный ответ")
-    data class Success(
-        @Schema(description = "ID сотрудника UUIDv7", example = "06115aa098-9277-0087-49a8-cb901fc2f7")
-        val id: UUID,
-        @Schema(description = "Имя сотрудника", example = "Иван")
-        val name: String,
-        @Schema(description = "Фамилия сотрудника", example = "Иванов")
-        val surname: String,
-        @Schema(description = "Отчество сотрудника", example = "Иванович")
-        val patronymic: String? = null,
-        @Schema(description = "ID руководителя сотрудника UUIDv7", example = "06115aa098-9277-0087-49a8-cb901fc2f7")
-        val directorId: UUID? = null,
-        @Schema(description = "ID пользователя сотрудника UUIDv7", example = "06115aa098-9277-0087-49a8-cb901fc2f7")
-        val userId: UUID,
-    ):EmployeeDetailsResponse()
-
-
-    @Schema(description = "Неудачный ответ")
-    data class Error(
-
-        @Schema(description = "HTTP код ошибки", example = "500")
-        val status: Int,
-
-        @Schema(description = "Описание HTTP ошибки", example = "Error while login")
-        val message: String? = "",
-
-        @Schema(description = "Дата ошибки ", example = "2026-01-27T18:30:00Z")
-        val timestamp: LocalDateTime,
-    ):EmployeeDetailsResponse()
-}
+data class EmployeeDetailsResponse (
+    @Schema(description = "ID сотрудника UUIDv7", example = "06115aa098-9277-0087-49a8-cb901fc2f7")
+    val id: UUID,
+    @Schema(description = "Имя сотрудника", example = "Иван")
+    val name: String,
+    @Schema(description = "Фамилия сотрудника", example = "Иванов")
+    val surname: String,
+    @Schema(description = "Отчество сотрудника", example = "Иванович")
+    val patronymic: String? = null,
+    @Schema(description = "ID руководителя сотрудника UUIDv7", example = "06115aa098-9277-0087-49a8-cb901fc2f7")
+    val directorId: UUID? = null,
+    @Schema(description = "ID пользователя сотрудника UUIDv7", example = "06115aa098-9277-0087-49a8-cb901fc2f7")
+    val userId: UUID,
+)
--- src/main/kotlin/api/masterplan/app/employeeModule/presentation/dto/responce/EmployeeFileResponse.kt
+++ src/main/kotlin/api/masterplan/app/employeeModule/presentation/dto/responce/EmployeeFileResponse.kt
@@ -1,47 +1,29 @@
 package api.masterplan.app.employeeModule.presentation.dto.responce
 
 import io.swagger.v3.oas.annotations.media.Schema
-import java.time.LocalDateTime
 
 @Schema(description = "Ответ Файл с экспортрованными пользователями")
-sealed class EmployeeFileResponse private constructor(){
+data class EmployeeFileResponse(
+    @Schema(description = "Файл")
+    val fileData: ByteArray,
+    @Schema(description = "Название файла", example = "EmployeesExport23022026172732")
+    val fileName: String,
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
 
-    @Schema(description = "Удачный ответ")
-    data class Success(
-        @Schema(description = "Файл")
-        val fileData: ByteArray,
-        @Schema(description = "Название файла", example = "EmployeesExport23022026172732")
-        val fileName: String,
-    ):EmployeeFileResponse() {
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (javaClass != other?.javaClass) return false
+        other as EmployeeFileResponse
 
-            other as Success
+        if (!fileData.contentEquals(other.fileData)) return false
+        if (fileName != other.fileName) return false
 
-            if (!fileData.contentEquals(other.fileData)) return false
-            if (fileName != other.fileName) return false
-
-            return true
-        }
-
-        override fun hashCode(): Int {
-            var result = fileData.contentHashCode()
-            result = 31 * result + fileName.hashCode()
-            return result
-        }
+        return true
     }
 
-    @Schema(description = "Неудачный ответ")
-    data class Error(
-
-        @Schema(description = "HTTP код ошибки", example = "500")
-        val status: Int,
-
-        @Schema(description = "Описание HTTP ошибки", example = "Error while login")
-        val message: String? = "",
-
-        @Schema(description = "Дата ошибки ", example = "2026-01-27T18:30:00Z")
-        val timestamp: LocalDateTime,
-    ): EmployeeFileResponse()
+    override fun hashCode(): Int {
+        var result = fileData.contentHashCode()
+        result = 31 * result + fileName.hashCode()
+        return result
+    }
 }
