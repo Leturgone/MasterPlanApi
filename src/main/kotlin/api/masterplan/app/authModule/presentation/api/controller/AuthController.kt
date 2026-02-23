19625082c72a87179e317d299bed2de7cd6817aa
Leturgone <finekon221@gmail.com>
2026-02-23 19:57:33 +0300

    add exception handler to AuthController

--- src/main/kotlin/api/masterplan/app/authModule/presentation/api/controller/AuthController.kt
+++ src/main/kotlin/api/masterplan/app/authModule/presentation/api/controller/AuthController.kt
@@ -2,9 +2,10 @@ package api.masterplan.app.authModule.presentation.api.controller
 
 import api.masterplan.app.authModule.application.command.LoginCommand
 import api.masterplan.app.authModule.application.usecase.LoginUseCase
+import api.masterplan.app.authModule.presentation.api.exceptionHandler.AuthControllerExceptionHandler
+import api.masterplan.app.authModule.presentation.dto.AuthErrorResponse
 import api.masterplan.app.authModule.presentation.dto.LoginRequest
 import api.masterplan.app.authModule.presentation.dto.LoginResponse
-import api.masterplan.app.authModule.presentation.mapper.AuthExceptionToHttpCodeMapper
 import api.masterplan.app.authModule.presentation.mapper.RequestToDomainMapper
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
@@ -16,9 +17,9 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDateTime
 
 @RestController
+@AuthControllerExceptionHandler
 @RequestMapping("/api/v1/auth")
 @Tag(name = "Auth", description = "Аутентификация")
 class AuthController(
@@ -32,52 +33,33 @@ class AuthController(
             ApiResponse(
                 responseCode = "200",
                 description = "",
-                content = [Content(schema = Schema(implementation = LoginResponse.Success::class))]
+                content = [Content(schema = Schema(implementation = LoginResponse::class))]
             ),
             ApiResponse(
                 responseCode = "400",
                 description = "Некорректные данные (пустой логин/пароль)",
-                content = [Content(schema = Schema(implementation = LoginResponse.Error::class))]
+                content = [Content(schema = Schema(implementation = AuthErrorResponse::class))]
             ),
             ApiResponse(
                 responseCode = "401",
                 description = "Неверный логин или пароль",
-                content = [Content(schema = Schema(implementation = LoginResponse.Error::class))]
+                content = [Content(schema = Schema(implementation = AuthErrorResponse::class))]
             )
         ]
     )
     @PostMapping("/login")
     fun login(@RequestBody loginRequest: LoginRequest): ResponseEntity<LoginResponse>{
 
-        val loginCommand = try {
-            val userPassword = RequestToDomainMapper.toDomainPassword(loginRequest.password)
-            val userLogin = RequestToDomainMapper.toDomainLogin(loginRequest.login)
-            LoginCommand(
-                login = userLogin,
-                password = userPassword
-            )
-        }catch (e: Exception){
-            val status = AuthExceptionToHttpCodeMapper.exceptionToHttpCode(e)
-            val body = LoginResponse.Error(
-                status = status.value(),e.message, LocalDateTime.now()
-            )
-            return ResponseEntity.status(status).body(body)
-        }
-
-
-        return  loginUseCase(loginCommand).fold(
-            onSuccess = { token ->
-                ResponseEntity.ok(LoginResponse.Success(
-                    token = token.token
-                ))
-            },
-            onFailure = {error ->
-                val status = AuthExceptionToHttpCodeMapper.exceptionToHttpCode(error)
-                val body = LoginResponse.Error(
-                    status = status.value(),error.message, LocalDateTime.now()
-                )
-                ResponseEntity.status(status).body(body)
-            }
+        val userPassword = RequestToDomainMapper.toDomainPassword(loginRequest.password)
+        val userLogin = RequestToDomainMapper.toDomainLogin(loginRequest.login)
+        val loginCommand = LoginCommand(
+            login = userLogin,
+            password = userPassword
         )
+
+        val result = loginUseCase(loginCommand).getOrThrow()
+        return ResponseEntity.ok(LoginResponse(
+            token = result.token
+        ))
     }
 }
