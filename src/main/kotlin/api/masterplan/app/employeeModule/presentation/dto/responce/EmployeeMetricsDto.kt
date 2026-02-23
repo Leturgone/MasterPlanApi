f519f649cda2805772c4578d5a6a454372d08912
Leturgone <finekon221@gmail.com>
2026-02-23 17:50:27 +0300

    add docs to EmployeeMetricsDto

--- src/main/kotlin/api/masterplan/app/employeeModule/presentation/dto/responce/EmployeeMetricsDto.kt
+++ src/main/kotlin/api/masterplan/app/employeeModule/presentation/dto/responce/EmployeeMetricsDto.kt
@@ -1,7 +1,14 @@
 package api.masterplan.app.employeeModule.presentation.dto.responce
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+
+@Schema(description = "Данные сотрудника с метриками, полные данные о сотруднике")
 data class EmployeeMetricsDto(
+    @Schema(description = "Рейтинг сотрудника", example = "5.5")
     val rating: Double,
+    @Schema(description = "Загруженность сотрудника", example = "3.0")
     val workload: Double,
+    @Schema(description = "Число порученных сотруднику задач", example = "10")
     val assignedTasksCount: Int
 )
