5b85c4946a73e4767598e6eaecc5d5274f141499
Leturgone <finekon221@gmail.com>
2026-02-23 17:52:23 +0300

    UpdateEmployeeUseCase returns id

--- src/main/kotlin/api/masterplan/app/employeeModule/application/usecase/UpdateEmployeeUseCase.kt
+++ src/main/kotlin/api/masterplan/app/employeeModule/application/usecase/UpdateEmployeeUseCase.kt
@@ -1,15 +1,15 @@
 package api.masterplan.app.employeeModule.application.usecase
 
 import api.masterplan.app.employeeModule.application.command.UpdateEmployeeCommand
-import api.masterplan.app.employeeModule.domain.dtos.EmployeeDetails
 import api.masterplan.app.employeeModule.domain.interfaces.EmployeeService
+import api.masterplan.app.employeeModule.domain.model.value.EmployeeId
 import org.springframework.stereotype.Service
 
 @Service
 class UpdateEmployeeUseCase(
     private val employeeService: EmployeeService
 ) {
-    operator fun invoke(command: UpdateEmployeeCommand): Result<EmployeeDetails> {
+    operator fun invoke(command: UpdateEmployeeCommand): Result<EmployeeId> {
         return try {
             val updatedEmployee = employeeService.updateEmployee(
                 id = command.id,
