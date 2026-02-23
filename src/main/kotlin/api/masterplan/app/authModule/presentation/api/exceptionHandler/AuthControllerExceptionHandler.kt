5379c4b6bc35a73c84185f2a3607212813639d84
Leturgone <finekon221@gmail.com>
2026-02-23 19:56:40 +0300

    add AuthControllerExceptionHandler annotation

---
+++ src/main/kotlin/api/masterplan/app/authModule/presentation/api/exceptionHandler/AuthControllerExceptionHandler.kt
@@ -0,0 +1,5 @@
+package api.masterplan.app.authModule.presentation.api.exceptionHandler
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class AuthControllerExceptionHandler()
