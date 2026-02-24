1295b427f521c747aec921d06488af95e534c826
Leturgone <finekon221@gmail.com>
2026-02-24 22:17:08 +0300

    add TaskRepository

---
+++ src/main/kotlin/api/masterplan/app/plansModule/domain/interfaces/TaskRepository.kt
@@ -0,0 +1,23 @@
+package api.masterplan.app.plansModule.domain.interfaces
+
+import api.masterplan.app.plansModule.domain.model.entity.Task
+import api.masterplan.app.plansModule.domain.model.value.ExecutorId
+import api.masterplan.app.plansModule.domain.model.value.PlanId
+import api.masterplan.app.plansModule.domain.model.value.TaskId
+
+interface TaskRepository {
+
+    fun getTask(taskId: TaskId): Task?
+
+    fun deleteTask(taskId: TaskId): TaskId?
+
+    fun saveTask(task: Task): TaskId?
+
+    fun getTasksByPlanId(planId: PlanId): List<Task>
+
+    fun getTasksByExecutorId(executorId: ExecutorId): List<Task>
+
+    fun isTaskExist(taskId: TaskId): Boolean
+
+    fun updateTask(taskId: TaskId, task: Task): TaskId?
+}
