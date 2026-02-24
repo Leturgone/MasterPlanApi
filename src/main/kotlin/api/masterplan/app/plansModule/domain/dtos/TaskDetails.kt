90b7519dd6f468c1129c44eb66c3c6e0c8f3d278
Leturgone <finekon221@gmail.com>
2026-02-24 21:47:10 +0300

    add TaskDetails

---
+++ src/main/kotlin/api/masterplan/app/plansModule/domain/dtos/TaskDetails.kt
@@ -0,0 +1,21 @@
+package api.masterplan.app.plansModule.domain.dtos
+
+import api.masterplan.app.plansModule.domain.model.value.ExecutorId
+import api.masterplan.app.plansModule.domain.model.value.PlanId
+import api.masterplan.app.plansModule.domain.model.value.TaskDate
+import api.masterplan.app.plansModule.domain.model.value.TaskDescription
+import api.masterplan.app.plansModule.domain.model.value.TaskDocumentId
+import api.masterplan.app.plansModule.domain.model.value.TaskId
+import api.masterplan.app.plansModule.domain.model.value.TaskStatus
+import api.masterplan.app.plansModule.domain.model.value.TaskTitle
+
+data class TaskDetails(
+    val id: TaskId,
+    val title: TaskTitle,
+    val description: TaskDescription,
+    val endDate: TaskDate,
+    val status: TaskStatus,
+    val planId: PlanId,
+    val documentId: TaskDocumentId? = null,
+    val executorsIds: MutableList<ExecutorId>
+)
