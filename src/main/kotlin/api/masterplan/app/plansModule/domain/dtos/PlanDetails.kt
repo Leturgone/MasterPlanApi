66ac4538d4cb715eec9357bba7709deb918f7c8c
Leturgone <finekon221@gmail.com>
2026-02-25 19:52:30 +0300

    add MutableList to PlanDetails

--- src/main/kotlin/api/masterplan/app/plansModule/domain/dtos/PlanDetails.kt
+++ src/main/kotlin/api/masterplan/app/plansModule/domain/dtos/PlanDetails.kt
@@ -1,19 +1,12 @@
 package api.masterplan.app.plansModule.domain.dtos
 
-import api.masterplan.app.plansModule.domain.model.entity.Task
-import api.masterplan.app.plansModule.domain.model.value.PlanDate
-import api.masterplan.app.plansModule.domain.model.value.PlanDescription
-import api.masterplan.app.plansModule.domain.model.value.PlanDirectorId
-import api.masterplan.app.plansModule.domain.model.value.PlanDocumentId
-import api.masterplan.app.plansModule.domain.model.value.PlanId
-import api.masterplan.app.plansModule.domain.model.value.PlanStatus
-import api.masterplan.app.plansModule.domain.model.value.PlanTitle
+import api.masterplan.app.plansModule.domain.model.value.*
 
 data class PlanDetails(
     val id: PlanId,
     val title: PlanTitle,
     val description: PlanDescription,
-    val tasks: MutableList<Task>,
+    val tasks: MutableList<TaskDetails>,
     val startDate: PlanDate,
     val endDate: PlanDate,
     val status: PlanStatus,
