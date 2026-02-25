81ddf30ff11d75b4b47035cb019540c1b3d82a0f
Leturgone <finekon221@gmail.com>
2026-02-25 19:52:48 +0300

    add exceptions

--- src/main/kotlin/api/masterplan/app/plansModule/domain/exceptions/PlanException.kt
+++ src/main/kotlin/api/masterplan/app/plansModule/domain/exceptions/PlanException.kt
@@ -1,5 +1,10 @@
 package api.masterplan.app.plansModule.domain.exceptions
 
+import api.masterplan.app.plansModule.domain.model.value.PlanDirectorId
+import api.masterplan.app.plansModule.domain.model.value.PlanId
+import api.masterplan.app.plansModule.domain.model.value.PlanTitle
+import api.masterplan.app.plansModule.domain.model.value.TaskId
+
 sealed class PlanException(message: String): Exception(message){
 
     class InvalidTaskTitle(message: String?) : PlanException(
@@ -17,5 +22,35 @@ sealed class PlanException(message: String): Exception(message){
     class InvalidPlanDesc(message: String?) : PlanException(
         "Invalid plan description: ${message?.let {": $it"  }}"
     )
+
+    class PlanNotExist(planId: PlanId) : PlanException(
+        "Plan with id = ${planId.value} not found"
+    )
+
+    class PlanAlreadyExists(planTitle: PlanTitle) : PlanException(
+        "Plan with title = ${planTitle.value} already exists"
+    )
+
+    class FailedToCreatePlan(planTitle: PlanTitle, directorId: PlanDirectorId) : PlanException(
+        "Failed to create plan with id =  ${planTitle.value} with director id = ${directorId.value}"
+    )
+
+    class FailedToUpdatePlan(planId: PlanId) : PlanException(
+        "Failed to update plan with id = ${planId.value} "
+    )
+
+    class FailedToDeletePlan(planId: PlanId) : PlanException(
+        "Failed to delete plan with id = ${planId.value} "
+    )
+
+    class FailedToAddTask(planId: PlanId,taskId: TaskId) : PlanException(
+        "Failed to add task with id = ${taskId.value} to plan with id = ${planId.value}"
+    )
+
+    class FailedToRemoveTask(planId: PlanId,taskId: TaskId) : PlanException(
+        "Failed to remove task with id = ${taskId.value} from plan with id = ${taskId.value}"
+    )
+
+
 
 }
