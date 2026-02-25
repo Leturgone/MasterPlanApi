dd2ac249017add9919bcd77afe7775748c030895
Leturgone <finekon221@gmail.com>
2026-02-25 23:19:45 +0300

    add domain types to port

--- src/main/kotlin/api/masterplan/app/plansModule/application/ports/PlanFilesPort.kt
+++ src/main/kotlin/api/masterplan/app/plansModule/application/ports/PlanFilesPort.kt
@@ -1,22 +1,19 @@
 package api.masterplan.app.plansModule.application.ports
 
-import api.masterplan.app.plansModule.application.dto.PlanExportFile
-import api.masterplan.app.plansModule.application.dto.PlanFile
-import api.masterplan.app.plansModule.application.dto.PlanFileId
-import api.masterplan.app.plansModule.application.dto.TaskFile
-import api.masterplan.app.plansModule.application.dto.TaskFileId
+import api.masterplan.app.plansModule.application.dto.*
+import api.masterplan.app.plansModule.domain.model.value.PlanDocumentId
 import api.masterplan.app.plansModule.domain.model.value.PlanId
-import api.masterplan.app.plansModule.domain.model.value.TaskId
+import api.masterplan.app.plansModule.domain.model.value.TaskDocumentId
 
 interface PlanFilesPort {
 
-    fun uploadPlanFile(planId: PlanId, planFile: PlanFile): PlanFileId
+    fun uploadPlanFile(planFile: PlanFile): PlanDocumentId
 
-    fun downloadPlanFile(planId: PlanId): PlanFile
+    fun downloadPlanFile(planFileId: PlanDocumentId): PlanFile
 
-    fun uploadTaskFile(taskId: TaskId, taskFile: TaskFile): TaskFileId
+    fun uploadTaskFile(taskFile: TaskFile): TaskDocumentId
 
-    fun downloadTaskFile(taskId: TaskId): TaskFile
+    fun downloadTaskFile(taskFileId: TaskDocumentId): TaskFile
 
     fun exportPlan(planId: PlanId): PlanExportFile
 
