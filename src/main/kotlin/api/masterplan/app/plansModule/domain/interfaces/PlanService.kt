bc594227349027ff8871ae7007202e446b7c9c9f
Leturgone <finekon221@gmail.com>
2026-02-25 19:55:34 +0300

    add removeTaskFromPlan method

--- src/main/kotlin/api/masterplan/app/plansModule/domain/interfaces/PlanService.kt
+++ src/main/kotlin/api/masterplan/app/plansModule/domain/interfaces/PlanService.kt
@@ -9,11 +9,13 @@ interface PlanService {
 
     fun getPlanById(planId: PlanId): PlanDetails
 
-    fun createPlan(id: PlanId? = null, title: PlanTitle, description: PlanDescription, startDate: PlanDate,
+    fun createPlan(id: PlanId? = null, title: PlanTitle, description: PlanDescription, startDate: PlanDate? = null,
                    endDate: PlanDate, directorId: PlanDirectorId,
                    documentId: PlanDocumentId? = null): PlanId
 
-    fun addTaskToPlan(planId: PlanId, taskId: Task): PlanId
+    fun addTaskToPlan(planId: PlanId, task: Task): PlanId
+
+    fun removeTaskFromPlan(planId: PlanId, task: Task): PlanId
 
     fun getAllDirPlans(directorId: PlanDirectorId): List<PlanDetails>
 
@@ -21,7 +23,7 @@ interface PlanService {
 
     fun deletePlan(planId: PlanId): PlanId
 
-    fun filterDirPlansByDate(directorId: PlanDirectorId): List<PlanDetails>
+    fun sortDirPlansByDate(directorId: PlanDirectorId): List<PlanDetails>
 
     fun filterDirPlansByStatus(directorId: PlanDirectorId, status: PlanStatus): List<PlanDetails>
 }
