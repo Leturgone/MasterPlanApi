c3594f95666c86e4015c9ef6ada2b9cc39b6f122
Leturgone <finekon221@gmail.com>
2026-02-28 01:11:00 +0300

    add work with files to UpdateTaskUseCase

--- src/main/kotlin/api/masterplan/app/plansModule/application/usecase/UpdateTaskUseCase.kt
+++ src/main/kotlin/api/masterplan/app/plansModule/application/usecase/UpdateTaskUseCase.kt
@@ -1,6 +1,7 @@
 package api.masterplan.app.plansModule.application.usecase
 
 import api.masterplan.app.plansModule.application.command.UpdateTaskCommand
+import api.masterplan.app.plansModule.application.ports.PlanFilesPort
 import api.masterplan.app.plansModule.domain.interfaces.TaskService
 import api.masterplan.app.plansModule.domain.model.value.TaskId
 import org.springframework.stereotype.Service
@@ -8,11 +9,19 @@ import org.springframework.stereotype.Service
 
 @Service
 class UpdateTaskUseCase(
-    private val taskService: TaskService
+    private val taskService: TaskService,
+    private val planFilesPort: PlanFilesPort
 ) {
     operator fun invoke(command: UpdateTaskCommand):Result<TaskId> {
         return try {
-            val updatedTaskId = taskService.updateTask(command.taskId,command.updatedTask)
+            val taskDocId = taskService.getTaskById(command.taskId).documentId
+
+            val docId = command.document?.let {planFilesPort.uploadOrUpdateTaskFile(taskDocId,it)}
+
+            val updatedTask  = docId?.let { command.updatedTask.addDocument(it) }?: command.updatedTask
+
+            val updatedTaskId = taskService.updateTask(command.taskId,updatedTask)
+
             Result.success(updatedTaskId)
         }catch (e: Exception){
             Result.failure(e)
