733cff0a86eaec3f39926a0056d2152caae4685b
Leturgone <finekon221@gmail.com>
2026-02-28 01:10:39 +0300

    add file to UpdateTaskCommand

--- src/main/kotlin/api/masterplan/app/plansModule/application/command/UpdateTaskCommand.kt
+++ src/main/kotlin/api/masterplan/app/plansModule/application/command/UpdateTaskCommand.kt
@@ -1,9 +1,11 @@
 package api.masterplan.app.plansModule.application.command
 
+import api.masterplan.app.plansModule.application.dto.TaskFile
 import api.masterplan.app.plansModule.domain.model.entity.Task
 import api.masterplan.app.plansModule.domain.model.value.TaskId
 
 data class UpdateTaskCommand(
     val taskId: TaskId,
-    val updatedTask: Task
+    val updatedTask: Task,
+    val document: TaskFile? = null
 )
