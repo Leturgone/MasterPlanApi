015efb0ae044ed58a39a98d63b879cb3377e0caa
Leturgone <finekon221@gmail.com>
2026-02-28 00:55:14 +0300

    change with getting tasks

--- src/main/kotlin/api/masterplan/app/plansModule/application/usecase/DeletePlanUseCase.kt
+++ src/main/kotlin/api/masterplan/app/plansModule/application/usecase/DeletePlanUseCase.kt
@@ -3,12 +3,14 @@ package api.masterplan.app.plansModule.application.usecase
 import api.masterplan.app.plansModule.application.command.DeletePlanCommand
 import api.masterplan.app.plansModule.application.ports.PlanFilesPort
 import api.masterplan.app.plansModule.domain.interfaces.PlanService
+import api.masterplan.app.plansModule.domain.interfaces.TaskService
 import api.masterplan.app.plansModule.domain.model.value.PlanId
 import org.springframework.stereotype.Service
 
 @Service
 class DeletePlanUseCase(
     private val planService: PlanService,
+    private val taskService: TaskService,
     private val planFilesPort: PlanFilesPort
 ) {
     operator fun invoke(command: DeletePlanCommand): Result<PlanId>{
@@ -16,7 +18,8 @@ class DeletePlanUseCase(
 
             val deletedPlan = planService.getPlanById(command.planId)
             val planFileId = deletedPlan.documentId
-            val tasks = deletedPlan.tasks
+            val tasks = taskService.getTasksByPlanId(deletedPlan.id)
+
             val deletedPlanId = planService.deletePlan(command.planId)
 
             // Удаление файлов
