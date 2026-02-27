aeb0f19bc46d6e3c6d7d75ef2566f73753311754
Leturgone <finekon221@gmail.com>
2026-02-28 01:11:07 +0300

    add work with files to UpdatePlanUseCase

--- src/main/kotlin/api/masterplan/app/plansModule/application/usecase/UpdatePlanUseCase.kt
+++ src/main/kotlin/api/masterplan/app/plansModule/application/usecase/UpdatePlanUseCase.kt
@@ -1,17 +1,26 @@
 package api.masterplan.app.plansModule.application.usecase
 
 import api.masterplan.app.plansModule.application.command.UpdatePlanCommand
+import api.masterplan.app.plansModule.application.ports.PlanFilesPort
 import api.masterplan.app.plansModule.domain.interfaces.PlanService
 import api.masterplan.app.plansModule.domain.model.value.PlanId
 import org.springframework.stereotype.Service
 
 @Service
 class UpdatePlanUseCase(
-    private val planService: PlanService
+    private val planService: PlanService,
+    private val planFilesPort: PlanFilesPort
 ) {
     operator fun invoke(command: UpdatePlanCommand): Result<PlanId> {
         return try {
-            val updatedPlanId = planService.updatePlan(command.planId, command.updatedPlan)
+            val planDocId = planService.getPlanById(command.planId).documentId
+
+            val docId = command.document?.let { planFilesPort.uploadOrUpdatePlanFile(planDocId,it) }
+
+            val updatedPlan = docId?.let { command.updatedPlan.addDocument(it)}?: command.updatedPlan
+
+            val updatedPlanId = planService.updatePlan(command.planId, updatedPlan)
+
             Result.success(updatedPlanId)
         }catch (e: Exception){
             Result.failure(e)
