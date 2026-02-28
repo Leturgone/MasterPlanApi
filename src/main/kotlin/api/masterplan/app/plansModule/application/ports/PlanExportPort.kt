de3d6715f02cdb794af1eb4a551df787eed8239c
Leturgone <finekon221@gmail.com>
2026-02-28 16:21:59 +0300

    remove PlanExportPort

--- src/main/kotlin/api/masterplan/app/plansModule/application/ports/PlanExportPort.kt
+++
@@ -1,9 +0,0 @@
-package api.masterplan.app.plansModule.application.ports
-
-import api.masterplan.app.plansModule.application.dto.PlanExportFile
-import api.masterplan.app.plansModule.domain.dtos.PlanDetails
-
-interface PlanExportPort {
-
-    fun exportPlan(plan: PlanDetails): PlanExportFile
-}
