5a2ca4060ca58f32e33535688a7d32941fdb0db1
Leturgone <finekon221@gmail.com>
2026-03-01 14:21:48 +0300

    add logging PlanRepositoryImpl

--- src/main/kotlin/api/masterplan/app/plansModule/infrastructure/database/repository/PlanRepositoryImpl.kt
+++ src/main/kotlin/api/masterplan/app/plansModule/infrastructure/database/repository/PlanRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package api.masterplan.app.plansModule.infrastructure.database.repository
 
+import api.masterplan.app.logging.LoggingDatabaseMethod
 import api.masterplan.app.plansModule.domain.interfaces.PlanRepository
 import api.masterplan.app.plansModule.domain.model.entity.Plan
 import api.masterplan.app.plansModule.domain.model.value.PlanDirectorId
@@ -14,16 +15,22 @@ class PlanRepositoryImpl(
     private val jpaPlanRepository: JpaPlanRepository,
     private val jpaPlanStatusRepository: JpaPlanStatusRepository
 ): PlanRepository {
+
+    @LoggingDatabaseMethod(moduleName = "planModule")
     override fun getPlan(planId: PlanId): Plan? {
         val plan = jpaPlanRepository.findById(planId.value).getOrElse { return null }
         return PlanDatabaseMapper.toDomain(plan)
     }
 
+
+    @LoggingDatabaseMethod(moduleName = "planModule")
     override fun deletePlan(planId: PlanId): PlanId? {
         jpaPlanRepository.deleteById(planId.value)
         return planId
     }
 
+
+    @LoggingDatabaseMethod(moduleName = "planModule")
     override fun savePlan(plan: Plan): PlanId? {
         val status = jpaPlanStatusRepository.findAll().toSet()
         val planEntity = PlanDatabaseMapper.toEntity(plan,status)
@@ -31,15 +38,21 @@ class PlanRepositoryImpl(
         return PlanId(planId)
     }
 
+
+    @LoggingDatabaseMethod(moduleName = "planModule")
     override fun getDirPlans(directorId: PlanDirectorId): List<Plan> {
         val plans = jpaPlanRepository.findByDirectorId(directorId.value)
         return PlanDatabaseMapper.toDomain(plans)
     }
 
+
+    @LoggingDatabaseMethod(moduleName = "planModule")
     override fun isPlanExist(planTitle: PlanTitle, directorId: PlanDirectorId): Boolean {
         return jpaPlanRepository.existsByTitleAndDirectorId(planTitle.value,directorId.value)
     }
 
+
+    @LoggingDatabaseMethod(moduleName = "planModule")
     override fun updatePlan(planId: PlanId, plan: Plan): PlanId? {
         jpaPlanRepository.findById(planId.value).getOrElse { return null }
         val status = jpaPlanStatusRepository.findAll().toSet()
