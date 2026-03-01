fecf3d2782bfb2e90171e589f4828a1fbd2aa13a
Leturgone <finekon221@gmail.com>
2026-03-01 23:51:51 +0300

    change EmpTaskModel with list of emp ids

--- src/main/kotlin/api/masterplan/app/employeeModule/application/dto/EmpTaskModel.kt
+++ src/main/kotlin/api/masterplan/app/employeeModule/application/dto/EmpTaskModel.kt
@@ -3,7 +3,7 @@ package api.masterplan.app.employeeModule.application.dto
 import api.masterplan.app.employeeModule.domain.model.value.EmployeeId
 
 data class EmpTaskModel(
-    val employeeId: EmployeeId,
+    val employeeIds: List<EmployeeId>,
     val status: EmpTaskStatus,
     val weight: Double,
 )
