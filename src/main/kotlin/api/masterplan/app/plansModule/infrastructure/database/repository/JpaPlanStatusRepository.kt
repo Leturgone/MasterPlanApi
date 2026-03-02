6e237382785160dcf740d51ce5e714a520c056e7
Leturgone <finekon221@gmail.com>
2026-03-02 22:46:51 +0300

    add repository annotation

--- src/main/kotlin/api/masterplan/app/plansModule/infrastructure/database/repository/JpaPlanStatusRepository.kt
+++ src/main/kotlin/api/masterplan/app/plansModule/infrastructure/database/repository/JpaPlanStatusRepository.kt
@@ -2,6 +2,8 @@ package api.masterplan.app.plansModule.infrastructure.database.repository
 
 import api.masterplan.app.plansModule.infrastructure.database.entity.PlanStatusEntity
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface JpaPlanStatusRepository: CrudRepository<PlanStatusEntity, Int> {
 }
--- src/main/kotlin/api/masterplan/app/plansModule/infrastructure/database/repository/JpaTaskStatusRepository.kt
+++ src/main/kotlin/api/masterplan/app/plansModule/infrastructure/database/repository/JpaTaskStatusRepository.kt
@@ -2,6 +2,8 @@ package api.masterplan.app.plansModule.infrastructure.database.repository
 
 import api.masterplan.app.plansModule.infrastructure.database.entity.TaskStatusEntity
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface JpaTaskStatusRepository: CrudRepository<TaskStatusEntity, Int> {
 }
