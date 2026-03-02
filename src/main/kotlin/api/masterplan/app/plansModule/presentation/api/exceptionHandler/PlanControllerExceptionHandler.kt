a4e5715924676e4c3dc3d8783f773bf925e40b27
Leturgone <finekon221@gmail.com>
2026-03-02 22:59:19 +0300

    add PlanControllerExceptionHandler annotation

---
+++ src/main/kotlin/api/masterplan/app/plansModule/presentation/api/exceptionHandler/PlanControllerExceptionHandler.kt
@@ -0,0 +1,5 @@
+package api.masterplan.app.plansModule.presentation.api.exceptionHandler
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PlanControllerExceptionHandler()
--- src/main/kotlin/api/masterplan/app/plansModule/presentation/dto/responce/PlanControllerExceptionHandler.kt
+++
@@ -1,5 +0,0 @@
-package api.masterplan.app.plansModule.presentation.dto.responce
-
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class PlanControllerExceptionHandler()
