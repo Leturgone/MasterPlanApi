a236e3ce02b5c680b889bc0f89b8838831add2ff
Leturgone <finekon221@gmail.com>
2026-03-02 20:39:05 +0300

    add GetAssignedTasksForMultipleEmpUseCase

---
+++ src/main/kotlin/api/masterplan/app/plansModule/application/usecase/GetAssignedTasksForMultipleEmpUseCase.kt
@@ -0,0 +1,20 @@
+package api.masterplan.app.plansModule.application.usecase
+
+import api.masterplan.app.plansModule.application.command.GetAssignedTasksForMultipleEmpCommand
+import api.masterplan.app.plansModule.domain.dtos.TaskDetails
+import api.masterplan.app.plansModule.domain.interfaces.TaskService
+import org.springframework.stereotype.Service
+
+@Service
+class GetAssignedTasksForMultipleEmpUseCase(
+    private val taskService: TaskService
+) {
+    operator fun invoke(command: GetAssignedTasksForMultipleEmpCommand): Result<List<TaskDetails>>{
+        return try {
+            val tasks = taskService.getAssignedTasksForMultipleExecutors(command.executorIds)
+            Result.success(tasks)
+        }catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+}
