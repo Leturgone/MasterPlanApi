ffb242d4c23113bc40ada0b929263751c6e2c097
Leturgone <finekon221@gmail.com>
2026-03-02 20:57:21 +0300

    add PlanModuleServiceImpl

---
+++ src/main/kotlin/api/masterplan/app/plansModule/infrastructure/intermodule/PlanModuleServiceImpl.kt
@@ -0,0 +1,46 @@
+package api.masterplan.app.plansModule.infrastructure.intermodule
+
+import api.masterplan.app.plansModule.PlanModuleService
+import api.masterplan.app.plansModule.TaskModelDto
+import api.masterplan.app.plansModule.application.command.GetAssignedTasksCommand
+import api.masterplan.app.plansModule.application.command.GetAssignedTasksForMultipleEmpCommand
+import api.masterplan.app.plansModule.application.usecase.GetAssignedTasksForMultipleEmpUseCase
+import api.masterplan.app.plansModule.application.usecase.GetAssignedTasksUseCase
+import api.masterplan.app.plansModule.domain.model.value.ExecutorId
+import org.springframework.stereotype.Service
+import java.util.UUID
+
+@Service
+class PlanModuleServiceImpl(
+    private val getAssignedTasksUseCase: GetAssignedTasksUseCase,
+    private val getAllDirEmpTasksUseCase: GetAssignedTasksForMultipleEmpUseCase
+
+): PlanModuleService {
+    override fun getTasksByEmployeeId(employeeId: UUID): Result<List<TaskModelDto>> {
+        return try {
+            val command = GetAssignedTasksCommand(
+                executorId = ExecutorId(employeeId)
+            )
+            val result = getAssignedTasksUseCase(command).getOrThrow()
+            val mappedResult = InterModuleTaskToDtoSuccessMapper.toDto(result)
+            Result.success(mappedResult)
+        }catch (e: Exception){
+            val exception = InterModuleTaskToDtoErrorMapper.toDto(e)
+            Result.failure(exception)
+        }
+    }
+
+    override fun getTasksByEmployeeIds(employeeIds: Set<UUID>): Result<List<TaskModelDto>> {
+        return try {
+            val command = GetAssignedTasksForMultipleEmpCommand(
+            executorIds = employeeIds.map { ExecutorId(it) }.toSet()
+            )
+            val result = getAllDirEmpTasksUseCase(command).getOrThrow()
+            val mappedResult = InterModuleTaskToDtoSuccessMapper.toDto(result)
+            Result.success(mappedResult)
+        }catch (e: Exception){
+            val exception = InterModuleTaskToDtoErrorMapper.toDto(e)
+            Result.failure(exception)
+        }
+    }
+}
