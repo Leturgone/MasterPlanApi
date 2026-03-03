1738831380a6edd25cd082ddbc04769d1892703f
Leturgone <finekon221@gmail.com>
2026-03-03 21:37:17 +0300

    change desc

--- src/main/kotlin/api/masterplan/app/plansModule/presentation/dto/request/UpdateTaskStatusRequest.kt
+++ src/main/kotlin/api/masterplan/app/plansModule/presentation/dto/request/UpdateTaskStatusRequest.kt
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import java.util.UUID
 
-@Schema(description = "Данные для статуса задачи")
+@Schema(description = "Данные для обновления статуса задачи")
 data class UpdateTaskStatusRequest(
     @NotBlank(message = "task Id cant be blank")
     @Schema(description = "ID задачи UUIDv7", example = "06115aa098-9277-0087-49a8-cb901fc2f7")
