d3c0138ca585d33eacacc51d8444e66718ccebcc
Leturgone <finekon221@gmail.com>
2026-03-03 22:12:46 +0300

    add funs

--- src/main/kotlin/api/masterplan/app/plansModule/presentation/mapper/RequestToDomainMapper.kt
+++ src/main/kotlin/api/masterplan/app/plansModule/presentation/mapper/RequestToDomainMapper.kt
@@ -1,11 +1,22 @@
 package api.masterplan.app.plansModule.presentation.mapper
 
+import api.masterplan.app.plansModule.application.dto.PlanFile
+import api.masterplan.app.plansModule.application.dto.TaskFile
 import api.masterplan.app.plansModule.domain.exceptions.PlanException
 import api.masterplan.app.plansModule.domain.model.value.ExecutorId
+import api.masterplan.app.plansModule.domain.model.value.PlanDate
+import api.masterplan.app.plansModule.domain.model.value.PlanDescription
+import api.masterplan.app.plansModule.domain.model.value.PlanDirectorId
+import api.masterplan.app.plansModule.domain.model.value.PlanDocumentId
 import api.masterplan.app.plansModule.domain.model.value.PlanId
 import api.masterplan.app.plansModule.domain.model.value.PlanStatus
+import api.masterplan.app.plansModule.domain.model.value.PlanTitle
+import api.masterplan.app.plansModule.domain.model.value.TaskDate
+import api.masterplan.app.plansModule.domain.model.value.TaskDescription
 import api.masterplan.app.plansModule.domain.model.value.TaskId
 import api.masterplan.app.plansModule.domain.model.value.TaskStatus
+import api.masterplan.app.plansModule.domain.model.value.TaskTitle
+import java.time.LocalDate
 import java.util.UUID
 
 object RequestToDomainMapper {
@@ -30,4 +41,36 @@ object RequestToDomainMapper {
     fun toTaskId(taskId: UUID) = TaskId(taskId)
 
     fun toExecutorId(executorId: UUID) = ExecutorId(executorId)
+
+    fun toTaskTitle(taskTitle: String) = TaskTitle.validate(taskTitle)
+
+    fun toTaskDescription(taskDescription: String) = TaskDescription.validate(taskDescription)
+
+    fun toTaskDate(date: LocalDate) = TaskDate(date)
+
+    fun toDirectorId(directorId: UUID) = PlanDirectorId(directorId)
+
+    fun toPlanDocumentId(documentId: UUID) = PlanDocumentId(documentId)
+
+    fun toPlanTitle(planTitle: String) = PlanTitle.validate(planTitle)
+
+    fun toPlanDescription(planDescription: String) = PlanDescription.validate(planDescription)
+
+    fun toPlanFile(fileName: String?,fileData: ByteArray?): PlanFile?{
+        return if (fileName.isNullOrEmpty() || fileData == null) {
+            null
+        }else{
+            PlanFile(fileData, fileName)
+        }
+    }
+
+    fun toTaskFile(fileName: String?,fileData: ByteArray?): TaskFile?{
+        return if (fileName.isNullOrEmpty() || fileData == null) {
+            null
+        }else{
+            TaskFile(fileData, fileName)
+        }
+    }
+
+    fun toPlanDate(date: LocalDate) = PlanDate(date)
 }
