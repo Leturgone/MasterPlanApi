075051168e1addb7771cfab58ddd224e0ce5ecdb
Leturgone <finekon221@gmail.com>
2026-03-05 18:43:24 +0300

    remove taskId field

--- src/main/kotlin/api/masterplan/app/plansModule/presentation/dto/request/UpdateTaskStatusRequest.kt
+++ src/main/kotlin/api/masterplan/app/plansModule/presentation/dto/request/UpdateTaskStatusRequest.kt
@@ -6,9 +6,6 @@ import java.util.UUID
 
 @Schema(description = "Данные для обновления статуса задачи")
 data class UpdateTaskStatusRequest(
-    @NotBlank(message = "task Id cant be blank")
-    @Schema(description = "ID задачи UUIDv7", example = "06115aa098-9277-0087-49a8-cb901fc2f7")
-    val taskId: UUID,
     @NotBlank(message = "task status cant be blank")
     @Schema(description = "Статус задачи", example = "COMPLETED")
     val status: String
