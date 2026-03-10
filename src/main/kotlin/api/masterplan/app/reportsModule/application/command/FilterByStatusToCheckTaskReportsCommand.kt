048be83b762532517976608e6612b6b59f1df3a0
Leturgone <finekon221@gmail.com>
2026-03-11 00:21:15 +0300

    renamed

--- src/main/kotlin/api/masterplan/app/reportsModule/application/command/FilterByStatusToCheckTaskReportsCommand.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/application/command/FilterByStatusToCheckTaskReportsCommand.kt
@@ -3,7 +3,7 @@ package api.masterplan.app.reportsModule.application.command
 import api.masterplan.app.reportsModule.domain.models.value.ReportEmployeeId
 import api.masterplan.app.reportsModule.domain.models.value.TaskReportStatus
 
-data class FilterByStatusToCheckPlanReportsCommand(
+data class FilterByStatusToCheckTaskReportsCommand(
     val directorId: ReportEmployeeId,
     val status: TaskReportStatus
 )
--- src/main/kotlin/api/masterplan/app/reportsModule/application/usecase/FilterByStatusToCheckTaskReportsUseCase.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/application/usecase/FilterByStatusToCheckTaskReportsUseCase.kt
@@ -1,17 +1,17 @@
 package api.masterplan.app.reportsModule.application.usecase
 
-import api.masterplan.app.reportsModule.application.command.FilterByStatusToCheckPlanReportsCommand
+import api.masterplan.app.reportsModule.application.command.FilterByStatusToCheckTaskReportsCommand
 import api.masterplan.app.reportsModule.application.ports.EmployeesPort
 import api.masterplan.app.reportsModule.domain.dtos.TaskReportDetails
 import api.masterplan.app.reportsModule.domain.interfaces.TaskReportService
 import org.springframework.stereotype.Service
 
 @Service
-class FilterByStatusToCheckPlanReportsUseCase(
+class FilterByStatusToCheckTaskReportsUseCase(
     private val taskReportService: TaskReportService,
     private val employeesPort: EmployeesPort
 ) {
-    operator fun invoke(command: FilterByStatusToCheckPlanReportsCommand): Result<List<TaskReportDetails>>{
+    operator fun invoke(command: FilterByStatusToCheckTaskReportsCommand): Result<List<TaskReportDetails>>{
         return try {
             val subordinatesList = employeesPort.getSubordinates(command.directorId)
             val filterList = taskReportService.filterSubordinatesTaskReportsByStatus(
