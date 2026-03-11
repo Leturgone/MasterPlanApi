1bc6acc707ccd0ae4e3f02986252e6cd54dfe656
Leturgone <finekon221@gmail.com>
2026-03-12 00:01:09 +0300

    rename methods

--- src/main/kotlin/api/masterplan/app/reportsModule/domain/interfaces/TaskReportRepository.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/domain/interfaces/TaskReportRepository.kt
@@ -3,19 +3,20 @@ package api.masterplan.app.reportsModule.domain.interfaces
 import api.masterplan.app.reportsModule.domain.models.entity.TaskReport
 import api.masterplan.app.reportsModule.domain.models.value.ReportEmployeeId
 import api.masterplan.app.reportsModule.domain.models.value.TaskReportId
+import api.masterplan.app.reportsModule.domain.models.value.TaskReportTitle
 
 interface TaskReportRepository {
     fun getTaskReport(taskReportId: TaskReportId): TaskReport?
 
     fun deleteTaskReport(taskReportId: TaskReportId): TaskReportId?
 
-    fun savePlanReport(report: TaskReport): TaskReportId?
+    fun saveTaskReport(report: TaskReport): TaskReportId?
 
-    fun getPlanReportsByEmployeeId(employeeId: ReportEmployeeId): List<TaskReport>
+    fun getTaskReportsByEmployeeId(employeeId: ReportEmployeeId): List<TaskReport>
 
-    fun getTaskReportByEmployeeIds(employeeIds: List<ReportEmployeeId>): List<TaskReport>
+    fun getTaskReportByEmployeeIds(employeeIds: Set<ReportEmployeeId>): List<TaskReport>
 
-    fun isPlanReportExist(taskReportId: TaskReportId): Boolean
+    fun isPlanReportExist(employeeId: ReportEmployeeId,taskReportTitle: TaskReportTitle): Boolean
 
-    fun updatePlanReport(taskReportId: TaskReportId, updatedReport: TaskReport): TaskReportId
+    fun updatePlanReport(taskReportId: TaskReportId, updatedReport: TaskReport): TaskReportId?
 }
