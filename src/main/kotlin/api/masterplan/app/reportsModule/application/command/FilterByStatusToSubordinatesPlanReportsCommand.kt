84cf5b999d198eb175dd1cc3181e9dac4d17c9b0
Leturgone <finekon221@gmail.com>
2026-03-11 19:07:35 +0300

    remove

--- src/main/kotlin/api/masterplan/app/reportsModule/application/command/FilterByStatusToSubordinatesPlanReportsCommand.kt
+++
@@ -1,9 +0,0 @@
-package api.masterplan.app.reportsModule.application.command
-
-import api.masterplan.app.reportsModule.domain.models.value.PlanReportStatus
-import api.masterplan.app.reportsModule.domain.models.value.ReportEmployeeId
-
-data class FilterByStatusToSubordinatesPlanReportsCommand(
-    val directorId: ReportEmployeeId,
-    val status: PlanReportStatus
-)
