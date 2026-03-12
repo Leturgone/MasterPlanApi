79a716618e331c8978e4b3000109591e17c9810d
Leturgone <finekon221@gmail.com>
2026-03-12 21:57:22 +0300

    add ReportEmployeesAdapter

---
+++ src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/adapters/ReportEmployeesAdapter.kt
@@ -0,0 +1,20 @@
+package api.masterplan.app.reportsModule.infrastructure.adapters
+
+import api.masterplan.app.employeeModule.EmployeeModuleService
+import api.masterplan.app.reportsModule.application.ports.ReportEmployeesPort
+import api.masterplan.app.reportsModule.domain.models.value.ReportEmployeeId
+import org.springframework.stereotype.Component
+
+@Component
+class ReportEmployeesAdapter(
+    private val employeeModuleService: EmployeeModuleService
+): ReportEmployeesPort {
+
+    override fun getSubordinates(directorId: ReportEmployeeId): Set<ReportEmployeeId> {
+        val subordinatesList = employeeModuleService.getSubordinateEmployees(directorId.value).getOrElse {
+            throw ReportInnerModuleErrorMapper.exceptionToModuleException(it)
+        }
+        val subordinatesModelList = ReportInnerModuleSuccessMapper.toReportEmpIdSet(subordinatesList)
+        return subordinatesModelList
+    }
+}
