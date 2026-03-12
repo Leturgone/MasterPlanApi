1070fc6bfc99b9eaf1d0dbfacb5fa08afe72fa6d
Leturgone <finekon221@gmail.com>
2026-03-13 00:33:27 +0300

    rename ReportDescription

--- src/main/kotlin/api/masterplan/app/reportsModule/domain/models/value/ReportDescription.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/domain/models/value/ReportDescription.kt
@@ -3,14 +3,14 @@ package api.masterplan.app.reportsModule.domain.models.value
 import api.masterplan.app.reportsModule.domain.exceptions.ReportException
 
 @JvmInline
-value class PlanReportDescription(val value: String) {
-    fun validate(desc: String): PlanReportDescription {
+value class ReportDescription(val value: String) {
+    fun validate(desc: String): ReportDescription {
         try {
             require(desc.isNotEmpty()) { "Description cant be blank" }
             require(desc.length <=255){ "Description too long" }
         }catch(e:IllegalStateException){
             throw ReportException.InvalidReportDescription(e.message)
         }
-        return PlanReportDescription(desc)
+        return ReportDescription(desc)
     }
 }
