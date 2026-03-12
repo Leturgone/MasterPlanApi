07f46289d75a954fe17e46027f79ebba16b4d34a
Leturgone <finekon221@gmail.com>
2026-03-12 21:56:51 +0300

    remove unused imports

--- src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/adapters/ReportFilesAdapter.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/adapters/ReportFilesAdapter.kt
@@ -4,7 +4,6 @@ import api.masterplan.app.reportsModule.application.dto.ReportFile
 import api.masterplan.app.reportsModule.application.ports.ReportFilesPort
 import api.masterplan.app.reportsModule.domain.models.value.ReportDocumentId
 import org.springframework.stereotype.Component
-import org.springframework.stereotype.Service
 
 @Component
 class ReportFilesAdapter(): ReportFilesPort {
