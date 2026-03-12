f95e349f7fc89901785dcdb5bdda95476b59403e
Leturgone <finekon221@gmail.com>
2026-03-13 00:33:44 +0300

    rename ReportId

--- src/main/kotlin/api/masterplan/app/reportsModule/domain/models/value/ReportId.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/domain/models/value/ReportId.kt
@@ -4,8 +4,8 @@ import com.github.f4b6a3.uuid.UuidCreator
 import java.util.*
 
 @JvmInline
-value class PlanReportId(val id: UUID){
+value class ReportId(val id: UUID){
     companion object {
-        fun generate() = PlanReportId(UuidCreator.getTimeOrderedEpoch())
+        fun generate() = ReportId(UuidCreator.getTimeOrderedEpoch())
     }
 }
