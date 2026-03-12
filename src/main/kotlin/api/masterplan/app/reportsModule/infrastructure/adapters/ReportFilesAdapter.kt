9583b2f402ef025189f1355cbd306f424061eced
Leturgone <finekon221@gmail.com>
2026-03-12 21:21:54 +0300

    add ReportFilesAdapter

--- src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/adapters/ReportFilesAdapter.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/adapters/ReportFilesAdapter.kt
@@ -3,15 +3,18 @@ package api.masterplan.app.reportsModule.infrastructure.adapters
 import api.masterplan.app.reportsModule.application.dto.ReportFile
 import api.masterplan.app.reportsModule.application.ports.ReportFilesPort
 import api.masterplan.app.reportsModule.domain.models.value.ReportDocumentId
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 
-@Service
+@Component
 class ReportFilesAdapter(): ReportFilesPort {
     override fun uploadReportFile(reportFile: ReportFile): ReportDocumentId {
+        println("Uploading report")
         TODO("Not yet implemented")
     }
 
     override fun removeReportFile(reportFileId: ReportDocumentId): ReportDocumentId {
+        println("Removing report")
         TODO("Not yet implemented")
     }
 
@@ -19,6 +22,7 @@ class ReportFilesAdapter(): ReportFilesPort {
         reportFileId: ReportDocumentId,
         reportFile: ReportFile
     ): ReportDocumentId {
+        println("Updating report")
         TODO("Not yet implemented")
     }
 }
