eaa7113ccb24f1187b821c00603524efb001ddd6
Leturgone <finekon221@gmail.com>
2026-03-13 00:34:08 +0300

    rename ReportTitle

--- src/main/kotlin/api/masterplan/app/reportsModule/domain/models/value/ReportTitle.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/domain/models/value/ReportTitle.kt
@@ -3,16 +3,16 @@ package api.masterplan.app.reportsModule.domain.models.value
 import api.masterplan.app.reportsModule.domain.exceptions.ReportException
 
 @JvmInline
-value class PlanReportTitle private constructor(val value: String) {
+value class ReportTitle private constructor(val value: String) {
     companion object {
-        fun validate(title: String): PlanReportTitle {
+        fun validate(title: String): ReportTitle {
             try {
                 require(title.isNotBlank()) { "Title cant be blank" }
                 require(title.length <= 100) { "Title too long" }
             } catch (e: IllegalArgumentException) {
                 throw ReportException.InvalidReportTitle(e.message)
             }
-            return PlanReportTitle(title)
+            return ReportTitle(title)
         }
     }
 }
