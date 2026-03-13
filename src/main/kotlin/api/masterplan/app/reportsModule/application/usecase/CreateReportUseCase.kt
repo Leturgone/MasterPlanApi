68d8906d45a774a9b5b1ff3c68c29594f09170ef
Leturgone <finekon221@gmail.com>
2026-03-13 22:20:52 +0300

    refactor CreateReportUseCase

--- src/main/kotlin/api/masterplan/app/reportsModule/application/usecase/CreateReportUseCase.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/application/usecase/CreateReportUseCase.kt
@@ -1,25 +1,25 @@
 package api.masterplan.app.reportsModule.application.usecase
 
-import api.masterplan.app.reportsModule.application.command.CreatePlanReportCommand
+import api.masterplan.app.reportsModule.application.command.CreateReportCommand
 import api.masterplan.app.reportsModule.application.ports.ReportFilesPort
-import api.masterplan.app.reportsModule.domain.interfaces.PlanReportService
-import api.masterplan.app.reportsModule.domain.models.value.PlanReportId
+import api.masterplan.app.reportsModule.domain.interfaces.ReportService
+import api.masterplan.app.reportsModule.domain.models.value.ReportId
 import org.springframework.stereotype.Service
 
 @Service
-class CreatePlanReportUseCase(
-    private val planReportService: PlanReportService,
+class CreateReportUseCase(
+    private val planReportService: ReportService,
     private val reportFilesPort: ReportFilesPort
 ) {
-    operator fun invoke(command: CreatePlanReportCommand): Result<PlanReportId> {
+    operator fun invoke(command: CreateReportCommand): Result<ReportId> {
         return try {
             val planReportFileId = reportFilesPort.uploadReportFile(command.document)
-            val planReportId = planReportService.createPlanReport(
+            val planReportId = planReportService.createReport(
                 id = command.id,
                 title = command.title,
                 description = command.description,
                 employeeId = command.employeeId,
-                taskId = command.taskId,
+                referenceId = command.referenceId,
                 documentId = planReportFileId,
             )
             Result.success(planReportId)
