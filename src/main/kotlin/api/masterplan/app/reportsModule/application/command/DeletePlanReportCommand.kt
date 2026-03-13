3c8a89b85c08c525d328aac807fe1c7317ad6d30
Leturgone <finekon221@gmail.com>
2026-03-13 21:41:15 +0300

    remove DeletePlanReportCommand and DeleteTaskReportCommand

--- src/main/kotlin/api/masterplan/app/reportsModule/application/command/DeletePlanReportCommand.kt
+++
@@ -1,7 +0,0 @@
-package api.masterplan.app.reportsModule.application.command
-
-import api.masterplan.app.reportsModule.domain.models.value.PlanReportId
-
-data class DeletePlanReportCommand(
-    val reportId: PlanReportId
-)
--- src/main/kotlin/api/masterplan/app/reportsModule/application/command/DeleteTaskReportCommand.kt
+++
@@ -1,7 +0,0 @@
-package api.masterplan.app.reportsModule.application.command
-
-import api.masterplan.app.reportsModule.domain.models.value.TaskReportId
-
-data class DeleteTaskReportCommand(
-    val reportId: TaskReportId
-)
