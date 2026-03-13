b3b42966c77c906174cc76efbd7690f2ba230233
Leturgone <finekon221@gmail.com>
2026-03-13 21:38:41 +0300

    add CreateReportCommand

--- src/main/kotlin/api/masterplan/app/reportsModule/application/command/CreateReportCommand.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/application/command/CreateReportCommand.kt
@@ -3,11 +3,12 @@ package api.masterplan.app.reportsModule.application.command
 import api.masterplan.app.reportsModule.application.dto.ReportFile
 import api.masterplan.app.reportsModule.domain.models.value.*
 
-data class CreatePlanReportCommand(
-    val id: PlanReportId? = null,
-    val title: PlanReportTitle,
-    val description: PlanReportDescription? = null,
+data class CreateReportCommand(
+    val id: ReportId? = null,
+    val title: ReportTitle,
+    val description: ReportDescription? = null,
     val employeeId: ReportEmployeeId,
     val taskId: ReportTaskId,
     val document: ReportFile,
+    val reportType: ReportType
 )
