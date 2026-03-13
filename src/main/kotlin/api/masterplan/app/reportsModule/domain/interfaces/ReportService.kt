7acdb03e0d30d731acce13c8ab521d51f3a7f134
Leturgone <finekon221@gmail.com>
2026-03-13 20:19:12 +0300

    add ReportService

--- src/main/kotlin/api/masterplan/app/reportsModule/domain/interfaces/ReportService.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/domain/interfaces/ReportService.kt
@@ -1,13 +1,12 @@
 package api.masterplan.app.reportsModule.domain.interfaces
 
-import api.masterplan.app.reportsModule.domain.dtos.PlanReportDetails
-import api.masterplan.app.reportsModule.domain.dtos.TaskReportDetails
+import api.masterplan.app.reportsModule.domain.dtos.ReportDetails
 import api.masterplan.app.reportsModule.domain.models.entity.Report
 import api.masterplan.app.reportsModule.domain.models.value.*
 
 interface ReportService {
 
-    fun getReport(reportId: ReportId, reportType: ReportType): PlanReportDetails
+    fun getReport(reportId: ReportId, reportType: ReportType): ReportDetails
 
     fun updateReport(reportId: ReportId, reportType: ReportType, updatedPlanReport: Report): ReportId
 
@@ -17,16 +16,16 @@ interface ReportService {
                          employeeId: ReportEmployeeId, referenceId: ReportReferenceId,
                      documentId: ReportDocumentId): ReportId
 
-    fun getCreatedReports(employeeId: ReportEmployeeId,reportType: ReportType): List<PlanReportDetails>
+    fun getCreatedReports(employeeId: ReportEmployeeId,reportType: ReportType): List<ReportDetails>
 
     fun filterCreatedReportsByStatus(employeeId: ReportEmployeeId ,reportType: ReportType,
-                                     status: ReportStatus): List<PlanReportDetails>
+                                     status: ReportStatus): List<ReportDetails>
 
     fun changeReportStatus(reportId: ReportId, reportType: ReportType, status: ReportStatus): ReportId
 
-    fun getSubordinatesTaskReports(subordinatesIds:Set<ReportEmployeeId>): List<TaskReportDetails>
+    fun getSubordinatesTaskReports(subordinatesIds:Set<ReportEmployeeId>): List<ReportDetails>
 
     fun filterSubordinatesTaskReportsByStatus(subordinatesIds:Set<ReportEmployeeId>,
-                                              status: ReportStatus): List<TaskReportDetails>
+                                              status: ReportStatus): List<ReportDetails>
 
 }
