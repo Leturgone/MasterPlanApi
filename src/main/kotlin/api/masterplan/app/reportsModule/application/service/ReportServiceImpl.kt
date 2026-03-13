dd7a432efa1f51f5a5412dd24a6d6885588023ea
Leturgone <finekon221@gmail.com>
2026-03-13 20:19:17 +0300

    add ReportServiceImpl

--- src/main/kotlin/api/masterplan/app/reportsModule/application/service/ReportServiceImpl.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/application/service/ReportServiceImpl.kt
@@ -1,20 +1,14 @@
 package api.masterplan.app.reportsModule.application.service
 
 import api.masterplan.app.logging.LoggingMethod
-import api.masterplan.app.reportsModule.domain.dtos.PlanReportDetails
-import api.masterplan.app.reportsModule.domain.dtos.TaskReportDetails
+import api.masterplan.app.reportsModule.application.mapper.ReportToEntityMapper
+import api.masterplan.app.reportsModule.domain.dtos.ReportDetails
+import api.masterplan.app.reportsModule.domain.exceptions.ReportException
 import api.masterplan.app.reportsModule.domain.interfaces.PlanReportRepository
 import api.masterplan.app.reportsModule.domain.interfaces.ReportService
 import api.masterplan.app.reportsModule.domain.interfaces.TaskReportRepository
 import api.masterplan.app.reportsModule.domain.models.entity.Report
-import api.masterplan.app.reportsModule.domain.models.value.ReportDescription
-import api.masterplan.app.reportsModule.domain.models.value.ReportDocumentId
-import api.masterplan.app.reportsModule.domain.models.value.ReportEmployeeId
-import api.masterplan.app.reportsModule.domain.models.value.ReportId
-import api.masterplan.app.reportsModule.domain.models.value.ReportReferenceId
-import api.masterplan.app.reportsModule.domain.models.value.ReportStatus
-import api.masterplan.app.reportsModule.domain.models.value.ReportTitle
-import api.masterplan.app.reportsModule.domain.models.value.ReportType
+import api.masterplan.app.reportsModule.domain.models.value.*
 import org.springframework.stereotype.Service
 
 @Service
@@ -24,57 +18,142 @@ class ReportServiceImpl(
 ): ReportService {
 
     @LoggingMethod("reportModule")
-    override fun getReport(reportId: ReportId, reportType: ReportType): PlanReportDetails {
-        TODO("Not yet implemented")
+    override fun getReport(reportId: ReportId, reportType: ReportType): ReportDetails {
+        val report =  when(reportType) {
+            ReportType.TASK -> planReportRepository.getPlanReport(reportId)
+            ReportType.PLAN -> taskReportRepository.getTaskReport(reportId)
+        }?: throw ReportException.ReportNotExist(reportId, reportType)
+        return ReportToEntityMapper.toReportDetails(report)
     }
 
 
     @LoggingMethod("reportModule")
     override fun updateReport(reportId: ReportId, reportType: ReportType, updatedPlanReport: Report): ReportId {
-        TODO("Not yet implemented")
+        val report =  when(reportType) {
+            ReportType.TASK -> planReportRepository.getPlanReport(reportId)
+            ReportType.PLAN -> taskReportRepository.getTaskReport(reportId)
+        }?: throw ReportException.ReportNotExist(reportId, reportType)
+
+        val updatedReport = report.update(
+            title = updatedPlanReport.title,
+            description = updatedPlanReport.description,
+            documentId = updatedPlanReport.documentId,
+        )
+
+        val updatedReportId = when(reportType) {
+            ReportType.TASK -> planReportRepository.updatePlanReport(reportId, updatedReport)
+            ReportType.PLAN -> taskReportRepository.updateTaskReport(reportId, updatedReport)
+        }?: throw ReportException.FailedToUpdateReport(reportId, reportType)
+
+        return updatedReportId
     }
 
 
     @LoggingMethod("reportModule")
     override fun deleteReport(reportId: ReportId, reportType: ReportType): ReportId {
-        TODO("Not yet implemented")
+        val deletedReportId = when(reportType) {
+            ReportType.TASK -> planReportRepository.deletePlanReport(reportId)
+            ReportType.PLAN -> taskReportRepository.deleteTaskReport(reportId)
+        }?: throw ReportException.FailedToDeleteReport(reportId,reportType)
+
+        return deletedReportId
     }
 
 
     @LoggingMethod("reportModule")
     override fun createReport(id: ReportId?, title: ReportTitle, description: ReportDescription?,
         employeeId: ReportEmployeeId, referenceId: ReportReferenceId, documentId: ReportDocumentId): ReportId {
-        TODO("Not yet implemented")
+
+        val reportExist = when(referenceId){
+            is ReportReferenceId.ForPlan -> planReportRepository.isPlanReportExist(employeeId,title)
+            is ReportReferenceId.ForTask -> taskReportRepository.isTaskReportExist(employeeId,title)
+        }
+
+        if (reportExist) throw ReportException.ReportAlreadyExist(employeeId,title)
+
+        val reportEntity = Report.create(
+            id = id,
+            title = title,
+            description = description,
+            employeeId = employeeId,
+            referenceId = referenceId,
+            documentId = documentId
+        )
+
+        val reportId = when(reportEntity.type){
+            ReportType.TASK ->  taskReportRepository.saveTaskReport(reportEntity)
+            ReportType.PLAN -> planReportRepository.savePlanReport(reportEntity)
+        }?: throw ReportException.FailedToSaveReport(employeeId,title,reportEntity.type)
+
+        return reportId
+
     }
 
 
     @LoggingMethod("reportModule")
-    override fun getCreatedReports(employeeId: ReportEmployeeId, reportType: ReportType): List<PlanReportDetails> {
-        TODO("Not yet implemented")
+    override fun getCreatedReports(employeeId: ReportEmployeeId, reportType: ReportType): List<ReportDetails> {
+        val reportList = when(reportType) {
+            ReportType.TASK -> taskReportRepository.getTaskReportsByEmployeeId(employeeId)
+            ReportType.PLAN -> planReportRepository.getPlanReportsByEmployeeId(employeeId)
+        }
+
+        return reportList.map { ReportToEntityMapper.toReportDetails(it) }
     }
 
 
     @LoggingMethod("reportModule")
-    override fun filterCreatedReportsByStatus(employeeId: ReportEmployeeId, reportType: ReportType, status: ReportStatus): List<PlanReportDetails> {
-        TODO("Not yet implemented")
+    override fun filterCreatedReportsByStatus(employeeId: ReportEmployeeId, reportType: ReportType, status: ReportStatus): List<ReportDetails> {
+
+        val reportList = when(reportType) {
+            ReportType.TASK -> taskReportRepository.getTaskReportsByEmployeeId(employeeId)
+            ReportType.PLAN -> planReportRepository.getPlanReportsByEmployeeId(employeeId)
+        }
+
+        return reportList.filter { planReport ->
+            planReport.reportStatus == status
+        }.map {
+            ReportToEntityMapper.toReportDetails(it)
+        }
     }
 
 
     @LoggingMethod("reportModule")
     override fun changeReportStatus(reportId: ReportId, reportType: ReportType, status: ReportStatus): ReportId {
-        TODO("Not yet implemented")
+        val report =  when(reportType) {
+            ReportType.TASK -> planReportRepository.getPlanReport(reportId)
+            ReportType.PLAN -> taskReportRepository.getTaskReport(reportId)
+        }?: throw ReportException.ReportNotExist(reportId, reportType)
+
+        val reportWithNewStatus = report.changeReportStatus(status)
+
+        val updatedReportId = when(reportType) {
+            ReportType.TASK -> planReportRepository.updatePlanReport(reportId, reportWithNewStatus)
+            ReportType.PLAN -> taskReportRepository.updateTaskReport(reportId, reportWithNewStatus)
+        }?: throw ReportException.FailedToUpdateReportStatus(status,reportId, reportType)
+
+        return updatedReportId
+
     }
 
 
     @LoggingMethod("reportModule")
-    override fun getSubordinatesTaskReports(subordinatesIds: Set<ReportEmployeeId>): List<TaskReportDetails> {
-        TODO("Not yet implemented")
+    override fun getSubordinatesTaskReports(subordinatesIds: Set<ReportEmployeeId>): List<ReportDetails> {
+        val reportList = taskReportRepository.getTaskReportByEmployeeIds(subordinatesIds)
+
+        return reportList.map { ReportToEntityMapper.toReportDetails(it) }
     }
 
 
     @LoggingMethod("reportModule")
-    override fun filterSubordinatesTaskReportsByStatus(subordinatesIds: Set<ReportEmployeeId>, status: ReportStatus): List<TaskReportDetails> {
-        TODO("Not yet implemented")
+    override fun filterSubordinatesTaskReportsByStatus(subordinatesIds: Set<ReportEmployeeId>,
+                                                       status: ReportStatus): List<ReportDetails> {
+        val reportList = taskReportRepository.getTaskReportByEmployeeIds(subordinatesIds)
+
+        return reportList.filter { taskReport ->
+            taskReport.reportStatus == status
+        }.map {
+            ReportToEntityMapper.toReportDetails(it)
+        }
     }
 
 }
