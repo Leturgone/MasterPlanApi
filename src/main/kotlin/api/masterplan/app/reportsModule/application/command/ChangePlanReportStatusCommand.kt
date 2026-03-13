68c50fcbc44b6de4162713b812440b84fe761062
Leturgone <finekon221@gmail.com>
2026-03-13 21:38:14 +0300

    remove ChangePlanReportStatusCommand and ChangeTaskReportStatusCommand

--- src/main/kotlin/api/masterplan/app/reportsModule/application/command/ChangePlanReportStatusCommand.kt
+++
@@ -1,9 +0,0 @@
-package api.masterplan.app.reportsModule.application.command
-
-import api.masterplan.app.reportsModule.domain.models.value.PlanReportId
-import api.masterplan.app.reportsModule.domain.models.value.ReportStatus
-
-data class ChangePlanReportStatusCommand(
-    val reportId: PlanReportId,
-    val status: ReportStatus
-)
