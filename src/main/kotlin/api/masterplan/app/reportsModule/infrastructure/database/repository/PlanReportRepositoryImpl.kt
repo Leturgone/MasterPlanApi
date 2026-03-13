dd09cc9db3fbdda2e31bf41890fe98f8f320c914
Leturgone <finekon221@gmail.com>
2026-03-13 22:53:27 +0300

    add PlanReportRepositoryImpl

--- src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/repository/PlanReportRepositoryImpl.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/repository/PlanReportRepositoryImpl.kt
@@ -1,4 +1,38 @@
 package api.masterplan.app.reportsModule.infrastructure.database.repository
 
-class PlanReportRepositoryImpl {
+import api.masterplan.app.reportsModule.domain.interfaces.PlanReportRepository
+import api.masterplan.app.reportsModule.domain.models.entity.Report
+import api.masterplan.app.reportsModule.domain.models.value.ReportEmployeeId
+import api.masterplan.app.reportsModule.domain.models.value.ReportId
+import api.masterplan.app.reportsModule.domain.models.value.ReportTitle
+import org.springframework.stereotype.Repository
+
+@Repository
+class PlanReportRepositoryImpl(
+    private val jpaPlanReportRepository: JpaPlanReportRepository,
+    private val jpaPlanStatusRepository: JpaReportStatusRepository
+): PlanReportRepository {
+    override fun getPlanReport(planReportId: ReportId): Report? {
+        TODO("Not yet implemented")
+    }
+
+    override fun deletePlanReport(planReportId: ReportId): ReportId? {
+        TODO("Not yet implemented")
+    }
+
+    override fun savePlanReport(report: Report): ReportId? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPlanReportsByEmployeeId(employeeId: ReportEmployeeId): List<Report> {
+        TODO("Not yet implemented")
+    }
+
+    override fun isPlanReportExist(employeeId: ReportEmployeeId, title: ReportTitle): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun updatePlanReport(planReportId: ReportId, updatedReport: Report): ReportId? {
+        TODO("Not yet implemented")
+    }
 }
