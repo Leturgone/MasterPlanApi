28103d7617b381a2caae681fb5a45a13e39eea52
Leturgone <finekon221@gmail.com>
2026-03-15 21:48:25 +0300

    create ReportStatusEntity

--- src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/entity/ReportStatusEntity.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/entity/ReportStatusEntity.kt
@@ -1,5 +1,27 @@
 package api.masterplan.app.reportsModule.infrastructure.database.entity
 
-data class ReportStatusEntity(
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 
-)
+@Entity
+@Table(name = "report_status")
+data class ReportStatusEntity(
+    @Id
+    @Column(name = "id")
+    val id: Int,
+    @Column(name = "status",unique = true,nullable = false, length = 45)
+    var status: String,
+){
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as ReportStatusEntity
+        return id == other.id
+    }
+
+    override fun hashCode(): Int  = id.hashCode()
+
+    override fun toString(): String = "ReportStatusEntity(id=$id, status=$status)"
+}
