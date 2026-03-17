2284dfa41000e4951c2d1520778494325131d74f
Leturgone <finekon221@gmail.com>
2026-03-17 20:27:53 +0300

    add toEntity to mapper

--- src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/mapper/PlanReportDatabaseMapper.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/mapper/PlanReportDatabaseMapper.kt
@@ -1,5 +1,6 @@
 package api.masterplan.app.reportsModule.infrastructure.database.mapper
 
+import api.masterplan.app.reportsModule.domain.exceptions.ReportException
 import api.masterplan.app.reportsModule.domain.models.entity.Report
 import api.masterplan.app.reportsModule.domain.models.value.ReportDescription
 import api.masterplan.app.reportsModule.domain.models.value.ReportDocumentId
@@ -9,6 +10,7 @@ import api.masterplan.app.reportsModule.domain.models.value.ReportPlanId
 import api.masterplan.app.reportsModule.domain.models.value.ReportReferenceId
 import api.masterplan.app.reportsModule.domain.models.value.ReportTitle
 import api.masterplan.app.reportsModule.infrastructure.database.entity.PlanReportEntity
+import api.masterplan.app.reportsModule.infrastructure.database.entity.ReportStatusEntity
 
 object PlanReportDatabaseMapper {
 
@@ -17,14 +19,29 @@ object PlanReportDatabaseMapper {
         return Report.create(
             id = ReportId(entity.id),
             title = ReportTitle.validate(entity.title),
-            description = ReportDescription.validate(entity.description),
+            description = entity.description?.let { ReportDescription.validate(it)},
             employeeId = ReportEmployeeId(entity.employeeId),
             referenceId = ReportReferenceId.ForPlan(ReportPlanId(entity.planId)),
             documentId = ReportDocumentId(entity.documentId),
         ).changeReportStatus(domainStatus)
     }
 
-
-
-
+    fun toEntity(report: Report,statusSet: Set<ReportStatusEntity>): PlanReportEntity{
+        val statusEntity = ReportStatusDatabaseMapper.toEntity(statusSet,report.reportStatus)
+        val planId = when (report.referenceId) {
+            is ReportReferenceId.ForPlan -> report.referenceId.planId.value
+            is ReportReferenceId.ForTask -> throw ReportException.InvalidReferenceId(report.referenceId)
+        }
+        return PlanReportEntity(
+            id = report.id.value,
+            title = report.title.value,
+            creationDate = report.creationDate.value,
+            editDate = report.editDate?.value,
+            description = report.description?.value,
+            reportStatus = statusEntity,
+            employeeId = report.employeeId.value,
+            planId = planId,
+            documentId = report.documentId.value
+        )
+    }
 }
