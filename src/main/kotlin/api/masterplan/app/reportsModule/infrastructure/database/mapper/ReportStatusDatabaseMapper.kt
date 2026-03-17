3b19f2316b30df0e7fbb7f552d3df53b26430883
Leturgone <finekon221@gmail.com>
2026-03-17 20:07:39 +0300

    add ReportStatusDatabaseMapper

--- src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/mapper/ReportStatusDatabaseMapper.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/mapper/ReportStatusDatabaseMapper.kt
@@ -1,4 +1,21 @@
 package api.masterplan.app.reportsModule.infrastructure.database.mapper
 
-object ReportStatusDatabaseMapper {
+import api.masterplan.app.reportsModule.domain.exceptions.ReportException
+import api.masterplan.app.reportsModule.domain.models.value.ReportStatus
+import api.masterplan.app.reportsModule.infrastructure.database.entity.ReportStatusEntity
+
+internal object ReportStatusDatabaseMapper {
+    fun toDomain(entity: ReportStatusEntity): ReportStatus {
+        return try {
+            ReportStatus.valueOf(entity.status.uppercase())
+        }catch (_: IllegalArgumentException){
+            throw ReportException.InvalidReportStatus(entity.status.uppercase())
+        }
+    }
+
+    fun toEntity(reportStatusList: Set<ReportStatusEntity>, reportStatus: ReportStatus): ReportStatusEntity {
+        val statusByTitle = reportStatusList.associateBy { it.status.uppercase() }
+        val domainStatus = statusByTitle[reportStatus.name]?:throw ReportException.InvalidReportStatus(reportStatus.name)
+        return domainStatus
+    }
 }
