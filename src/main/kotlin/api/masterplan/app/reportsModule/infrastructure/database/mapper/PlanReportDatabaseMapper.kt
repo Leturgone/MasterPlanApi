ba85b658ad5cd097f9d9e9ce5e1539c2aa4ab3c1
Leturgone <finekon221@gmail.com>
2026-03-17 20:13:30 +0300

    transfer to domain to task report mapper

--- src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/mapper/PlanReportDatabaseMapper.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/mapper/PlanReportDatabaseMapper.kt
@@ -1,4 +1,30 @@
 package api.masterplan.app.reportsModule.infrastructure.database.mapper
 
+import api.masterplan.app.reportsModule.domain.models.entity.Report
+import api.masterplan.app.reportsModule.domain.models.value.ReportDescription
+import api.masterplan.app.reportsModule.domain.models.value.ReportDocumentId
+import api.masterplan.app.reportsModule.domain.models.value.ReportEmployeeId
+import api.masterplan.app.reportsModule.domain.models.value.ReportId
+import api.masterplan.app.reportsModule.domain.models.value.ReportPlanId
+import api.masterplan.app.reportsModule.domain.models.value.ReportReferenceId
+import api.masterplan.app.reportsModule.domain.models.value.ReportTitle
+import api.masterplan.app.reportsModule.infrastructure.database.entity.PlanReportEntity
+
 object PlanReportDatabaseMapper {
+
+    fun toDomain(entity: PlanReportEntity): Report {
+        val domainStatus = ReportStatusDatabaseMapper.toDomain(entity.reportStatus)
+        return Report.create(
+            id = ReportId(entity.id),
+            title = ReportTitle.validate(entity.title),
+            description = ReportDescription.validate(entity.description),
+            employeeId = ReportEmployeeId(entity.employeeId),
+            referenceId = ReportReferenceId.ForPlan(ReportPlanId(entity.planId)),
+            documentId = ReportDocumentId(entity.documentId),
+        ).changeReportStatus(domainStatus)
+    }
+
+
+
+
 }
--- src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/mapper/TaskReportDatabaseMapper.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/mapper/TaskReportDatabaseMapper.kt
@@ -1,4 +1,25 @@
 package api.masterplan.app.reportsModule.infrastructure.database.mapper
 
+import api.masterplan.app.reportsModule.domain.models.entity.Report
+import api.masterplan.app.reportsModule.domain.models.value.ReportDescription
+import api.masterplan.app.reportsModule.domain.models.value.ReportDocumentId
+import api.masterplan.app.reportsModule.domain.models.value.ReportEmployeeId
+import api.masterplan.app.reportsModule.domain.models.value.ReportId
+import api.masterplan.app.reportsModule.domain.models.value.ReportReferenceId
+import api.masterplan.app.reportsModule.domain.models.value.ReportTaskId
+import api.masterplan.app.reportsModule.domain.models.value.ReportTitle
+import api.masterplan.app.reportsModule.infrastructure.database.entity.TaskReportEntity
+
 object TaskReportDatabaseMapper {
+    fun toDomain(entity: TaskReportEntity): Report {
+        val domainStatus = ReportStatusDatabaseMapper.toDomain(entity.reportStatus)
+        return Report.create(
+            id = ReportId(entity.id),
+            title = ReportTitle.validate(entity.title),
+            description = ReportDescription.validate(entity.description),
+            employeeId = ReportEmployeeId(entity.employeeId),
+            referenceId = ReportReferenceId.ForTask(ReportTaskId(entity.taskId)),
+            documentId = ReportDocumentId(entity.documentId),
+        ).changeReportStatus(domainStatus)
+    }
 }
