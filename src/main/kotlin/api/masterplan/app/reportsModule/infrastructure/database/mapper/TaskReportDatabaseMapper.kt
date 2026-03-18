9a3f31c5062988b57460d7d6d27022b29393cfdc
Leturgone <finekon221@gmail.com>
2026-03-18 20:01:38 +0300

    add toEntity

--- src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/mapper/TaskReportDatabaseMapper.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/mapper/TaskReportDatabaseMapper.kt
@@ -1,13 +1,9 @@
 package api.masterplan.app.reportsModule.infrastructure.database.mapper
 
+import api.masterplan.app.reportsModule.domain.exceptions.ReportException
 import api.masterplan.app.reportsModule.domain.models.entity.Report
-import api.masterplan.app.reportsModule.domain.models.value.ReportDescription
-import api.masterplan.app.reportsModule.domain.models.value.ReportDocumentId
-import api.masterplan.app.reportsModule.domain.models.value.ReportEmployeeId
-import api.masterplan.app.reportsModule.domain.models.value.ReportId
-import api.masterplan.app.reportsModule.domain.models.value.ReportReferenceId
-import api.masterplan.app.reportsModule.domain.models.value.ReportTaskId
-import api.masterplan.app.reportsModule.domain.models.value.ReportTitle
+import api.masterplan.app.reportsModule.domain.models.value.*
+import api.masterplan.app.reportsModule.infrastructure.database.entity.ReportStatusEntity
 import api.masterplan.app.reportsModule.infrastructure.database.entity.TaskReportEntity
 
 object TaskReportDatabaseMapper {
@@ -16,10 +12,33 @@ object TaskReportDatabaseMapper {
         return Report.create(
             id = ReportId(entity.id),
             title = ReportTitle.validate(entity.title),
-            description = ReportDescription.validate(entity.description),
+            description = entity.description?.let { ReportDescription.validate(it)},
             employeeId = ReportEmployeeId(entity.employeeId),
             referenceId = ReportReferenceId.ForTask(ReportTaskId(entity.taskId)),
             documentId = ReportDocumentId(entity.documentId),
         ).changeReportStatus(domainStatus)
+    }
+
+    fun toDomain(entities: List<TaskReportEntity>): List<Report> {
+        return entities.map { toDomain(it) }
+    }
+
+    fun toEntity(report: Report,statusSet: Set<ReportStatusEntity>): TaskReportEntity {
+        val statusEntity = ReportStatusDatabaseMapper.toEntity(statusSet,report.reportStatus)
+        val taskId = when (report.referenceId) {
+            is ReportReferenceId.ForPlan ->  throw ReportException.InvalidReferenceId(report.referenceId)
+            is ReportReferenceId.ForTask -> report.referenceId.taskId.value
+        }
+        return TaskReportEntity(
+            id = report.id.value,
+            title = report.title.value,
+            creationDate = report.creationDate.value,
+            editDate = report.editDate?.value,
+            description = report.description?.value,
+            reportStatus = statusEntity,
+            employeeId = report.employeeId.value,
+            taskId = taskId,
+            documentId = report.documentId.value
+        )
     }
 }
