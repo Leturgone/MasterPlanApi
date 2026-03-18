6c2feee2f913ec23a3e93672bc99a7d82c0b245b
Leturgone <finekon221@gmail.com>
2026-03-18 20:55:56 +0300

    add methods

--- src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/repository/JpaTaskReportRepository.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/repository/JpaTaskReportRepository.kt
@@ -1,8 +1,18 @@
 package api.masterplan.app.reportsModule.infrastructure.database.repository
 
 import api.masterplan.app.reportsModule.infrastructure.database.entity.TaskReportEntity
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import java.util.*
 
 interface JpaTaskReportRepository: CrudRepository<TaskReportEntity, UUID>  {
+
+    fun findByEmployeeId(employeeId: UUID): List<TaskReportEntity>
+
+    fun existsByTitleAndEmployeeId(title: String, employeeId: UUID): Boolean
+
+    @Query("SELECT t FROM TasReportkEntity t " +
+        "WHERE t.employeeId IN :employeeIds")
+    fun findByEmployeeIds(employeeIds: Set<UUID>): List<TaskReportEntity>
+
 }
