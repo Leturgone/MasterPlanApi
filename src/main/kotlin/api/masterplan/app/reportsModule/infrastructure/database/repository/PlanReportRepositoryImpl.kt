914dd72b13608910e5a30e0ae260341f99c50d45
Leturgone <finekon221@gmail.com>
2026-03-18 19:55:43 +0300

    created PlanReportRepositoryImpl

--- src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/repository/PlanReportRepositoryImpl.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/database/repository/PlanReportRepositoryImpl.kt
@@ -1,38 +1,60 @@
 package api.masterplan.app.reportsModule.infrastructure.database.repository
 
+import api.masterplan.app.logging.LoggingDatabaseMethod
 import api.masterplan.app.reportsModule.domain.interfaces.PlanReportRepository
 import api.masterplan.app.reportsModule.domain.models.entity.Report
 import api.masterplan.app.reportsModule.domain.models.value.ReportEmployeeId
 import api.masterplan.app.reportsModule.domain.models.value.ReportId
 import api.masterplan.app.reportsModule.domain.models.value.ReportTitle
+import api.masterplan.app.reportsModule.infrastructure.database.mapper.PlanReportDatabaseMapper
 import org.springframework.stereotype.Repository
+import kotlin.jvm.optionals.getOrElse
 
 @Repository
 class PlanReportRepositoryImpl(
     private val jpaPlanReportRepository: JpaPlanReportRepository,
     private val jpaPlanStatusRepository: JpaReportStatusRepository
 ): PlanReportRepository {
+
+    @LoggingDatabaseMethod(moduleName = "reportModule")
     override fun getPlanReport(planReportId: ReportId): Report? {
-        TODO("Not yet implemented")
+        val planReport = jpaPlanReportRepository.findById(planReportId.value).getOrElse { return null }
+        return PlanReportDatabaseMapper.toDomain(planReport)
     }
 
+    @LoggingDatabaseMethod(moduleName = "reportModule")
     override fun deletePlanReport(planReportId: ReportId): ReportId? {
-        TODO("Not yet implemented")
+        jpaPlanReportRepository.deleteById(planReportId.value)
+        return planReportId
     }
 
+    @LoggingDatabaseMethod(moduleName = "reportModule")
     override fun savePlanReport(report: Report): ReportId? {
-        TODO("Not yet implemented")
+        val statusSet = jpaPlanStatusRepository.findAll().toSet()
+        val planReportEntity= PlanReportDatabaseMapper.toEntity(report, statusSet)
+        val planReportId = jpaPlanReportRepository.save(planReportEntity).id
+        return ReportId(planReportId)
     }
 
+    @LoggingDatabaseMethod(moduleName = "reportModule")
     override fun getPlanReportsByEmployeeId(employeeId: ReportEmployeeId): List<Report> {
-        TODO("Not yet implemented")
+        val planReports = jpaPlanReportRepository.findByEmployeeId(employeeId.value)
+        return PlanReportDatabaseMapper.toDomain(planReports)
+
     }
 
+    @LoggingDatabaseMethod(moduleName = "reportModule")
     override fun isPlanReportExist(employeeId: ReportEmployeeId, title: ReportTitle): Boolean {
-        TODO("Not yet implemented")
+         return jpaPlanReportRepository.existsByTitleAndeEmployeeId(title.value,employeeId.value)
     }
 
+    @LoggingDatabaseMethod(moduleName = "reportModule")
     override fun updatePlanReport(planReportId: ReportId, updatedReport: Report): ReportId? {
-        TODO("Not yet implemented")
+        jpaPlanReportRepository.findById(planReportId.value).getOrElse { return null }
+        val statusSet = jpaPlanStatusRepository.findAll().toSet()
+        val updatedPlanReportEntity = PlanReportDatabaseMapper.toEntity(updatedReport, statusSet)
+        val planReportId = jpaPlanReportRepository.save(updatedPlanReportEntity).id
+        return ReportId(planReportId)
+
     }
 }
