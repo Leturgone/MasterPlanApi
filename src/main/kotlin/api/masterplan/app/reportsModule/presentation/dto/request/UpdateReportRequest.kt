e1434d1027c7ddacd3086b2c904a9d95f926716d
Leturgone <finekon221@gmail.com>
2026-03-19 19:20:28 +0300

    add desc to UpdateReportRequest

--- src/main/kotlin/api/masterplan/app/reportsModule/presentation/dto/request/UpdateReportRequest.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/presentation/dto/request/UpdateReportRequest.kt
@@ -1,12 +1,16 @@
 package api.masterplan.app.reportsModule.presentation.dto.request
 
 import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
 import java.util.UUID
 
 data class UpdateReportRequest(
+    @NotBlank(message = "Title cant be blank")
+    @Schema(description = "Название Отчета", example = "Отчет 1")
     val title: String,
+    @Schema(description = "Описание отчета", example = "Отчет с заметкой")
     val description: String?,
-
+    @Schema(description = "ID файла UUIDv7", example = "06115aa098-9277-0087-49a8-cb901fc2f7")
     val documentId: UUID,
     @Schema(description = "Название файла", example = "PlanExport23022026172732")
     val documentName: String,
