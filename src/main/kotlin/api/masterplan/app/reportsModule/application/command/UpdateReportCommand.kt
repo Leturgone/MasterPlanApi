b14a86ca0bd3cb670a212ca9b74b5f3749979f62
Leturgone <finekon221@gmail.com>
2026-03-19 19:05:40 +0300

    add ReportUpdateData to UpdateReportCommand

--- src/main/kotlin/api/masterplan/app/reportsModule/application/command/UpdateReportCommand.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/application/command/UpdateReportCommand.kt
@@ -1,13 +1,13 @@
 package api.masterplan.app.reportsModule.application.command
 
 import api.masterplan.app.reportsModule.application.dto.ReportFile
-import api.masterplan.app.reportsModule.domain.models.entity.Report
+import api.masterplan.app.reportsModule.domain.dtos.ReportUpdateData
 import api.masterplan.app.reportsModule.domain.models.value.ReportId
 import api.masterplan.app.reportsModule.domain.models.value.ReportType
 
 data class UpdateReportCommand(
     val reportId: ReportId,
-    val updatedReport: Report,
+    val updatedData: ReportUpdateData,
     val reportType: ReportType,
     val document: ReportFile,
 )
