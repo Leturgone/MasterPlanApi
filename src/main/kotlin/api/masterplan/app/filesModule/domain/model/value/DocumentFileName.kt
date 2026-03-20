b22a47f8096740a6256c46645a9d0ff886276622
Leturgone <finekon221@gmail.com>
2026-03-20 23:26:10 +0300

    add checks to DocumentFileName

--- src/main/kotlin/api/masterplan/app/filesModule/domain/model/value/DocumentFileName.kt
+++ src/main/kotlin/api/masterplan/app/filesModule/domain/model/value/DocumentFileName.kt
@@ -1,5 +1,6 @@
 package api.masterplan.app.filesModule.domain.model.value
 
+import api.masterplan.app.filesModule.domain.exceptions.FilesException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -7,6 +8,12 @@ import java.time.format.DateTimeFormatter
 value class DocumentFileName(val value: String){
     companion object {
         fun generate(fileName: String):DocumentFileName{
+            try {
+                require(fileName.isNotBlank()) { "File name cant be blank" }
+                require(fileName.length <= 125) { "File name too long" }
+            } catch (e: IllegalArgumentException) {
+                throw FilesException.InvalidFileName(e.message)
+            }
             val timeSnap = DateTimeFormatter.ofPattern("yyyMMddHHmmss").format(LocalDateTime.now())
             val newFileName = "$timeSnap$fileName"
             return DocumentFileName(newFileName)
