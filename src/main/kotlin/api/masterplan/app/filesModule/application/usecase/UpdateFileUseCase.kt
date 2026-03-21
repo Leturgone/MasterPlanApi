551dc1ed9005978f9e60a59274f6cc3c3d688128
Leturgone <finekon221@gmail.com>
2026-03-21 23:49:01 +0300

    add UpdateFileUseCase

---
+++ src/main/kotlin/api/masterplan/app/filesModule/application/usecase/UpdateFileUseCase.kt
@@ -0,0 +1,24 @@
+package api.masterplan.app.filesModule.application.usecase
+
+import api.masterplan.app.filesModule.application.command.UpdateFileCommand
+import api.masterplan.app.filesModule.domain.interfaces.DocumentFileService
+import api.masterplan.app.filesModule.domain.model.value.DocumentFileId
+import org.springframework.stereotype.Service
+
+@Service
+class UpdateFileUseCase(
+    private val documentFileService: DocumentFileService
+){
+    operator fun invoke(command:UpdateFileCommand): Result<DocumentFileId>{
+        return try {
+            val updatedFileId = documentFileService.updateFile(
+                fileId = command.fileId,
+                documentFileBaseName = command.documentFileBaseName,
+                documentFileData = command.documentFileData
+            )
+            Result.success(updatedFileId)
+        }catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+}
