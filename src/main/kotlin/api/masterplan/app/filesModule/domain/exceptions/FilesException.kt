79b13b93bcabca03cb6c2ae6097ddddcad2a2f35
Leturgone <finekon221@gmail.com>
2026-03-21 21:05:58 +0300

    add Exceptions

--- src/main/kotlin/api/masterplan/app/filesModule/domain/exceptions/FilesException.kt
+++ src/main/kotlin/api/masterplan/app/filesModule/domain/exceptions/FilesException.kt
@@ -1,9 +1,33 @@
 package api.masterplan.app.filesModule.domain.exceptions
 
+import api.masterplan.app.filesModule.domain.model.value.DocumentFileBaseName
+import api.masterplan.app.filesModule.domain.model.value.DocumentFileId
+import api.masterplan.app.filesModule.domain.model.value.DocumentFileName
+
 sealed class FilesException(message: String) : Exception(message) {
 
     class InvalidFileName(message: String?) : FilesException(
         "Invalid file name: ${message?.let { ": $it" } ?: ""}"
+    )
+
+    class FileNotExist(fileId: DocumentFileId): FilesException(
+        "File with id ${fileId.value} not found"
+    )
+
+    class FileAlreadyExists(fileName: DocumentFileBaseName): FilesException(
+        "File with name: ${fileName.value} already exists"
+    )
+
+    class FailedToCreateFile(fileName: DocumentFileName): FilesException(
+        "Failed to create file with name: ${fileName.value}"
+    )
+
+    class FailedToUpdateFile(fileId: DocumentFileId): FilesException(
+        "Failed to update file with id: ${fileId.value}"
+    )
+
+    class FailedToDeleteFile(fileId: DocumentFileId): FilesException(
+        "Failed to delete file with id: ${fileId.value}"
     )
 
 }
