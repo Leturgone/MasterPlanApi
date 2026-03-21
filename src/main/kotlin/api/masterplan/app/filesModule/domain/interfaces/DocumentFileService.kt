7c561f9d860ebb846435858a73049d25e6fe1e36
Leturgone <finekon221@gmail.com>
2026-03-21 21:06:59 +0300

    add params

--- src/main/kotlin/api/masterplan/app/filesModule/domain/interfaces/DocumentFileService.kt
+++ src/main/kotlin/api/masterplan/app/filesModule/domain/interfaces/DocumentFileService.kt
@@ -1,17 +1,19 @@
 package api.masterplan.app.filesModule.domain.interfaces
 
 import api.masterplan.app.filesModule.domain.dtos.DocumentFileDetails
+import api.masterplan.app.filesModule.domain.model.value.DocumentFileBaseName
 import api.masterplan.app.filesModule.domain.model.value.DocumentFileData
 import api.masterplan.app.filesModule.domain.model.value.DocumentFileId
-import api.masterplan.app.filesModule.domain.model.value.DocumentFileName
 
 interface DocumentFileService {
 
-    fun uploadFile(documentFileName: DocumentFileName,documentFileData: DocumentFileData): DocumentFileId
+    fun uploadFile(documentFileBaseName: DocumentFileBaseName, documentFileData: DocumentFileData): DocumentFileId
 
     fun removeFile(fileId: DocumentFileId):DocumentFileId
 
-    fun uploadOrUpdateFile(fileId: DocumentFileId? = null,documentFileData: DocumentFileData):DocumentFileId
+    fun uploadOrUpdateFile(fileId: DocumentFileId? = null,
+                           documentFileBaseName: DocumentFileBaseName,
+                           documentFileData: DocumentFileData):DocumentFileId
 
     fun downloadFile(fileId: DocumentFileId): DocumentFileDetails
 }
