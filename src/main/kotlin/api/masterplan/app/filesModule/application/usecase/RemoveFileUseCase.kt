8fa5b3a4e1f8ebec77d41023bb5e0727035532c6
Leturgone <finekon221@gmail.com>
2026-03-21 23:43:41 +0300

    add RemoveFileUseCase

---
+++ src/main/kotlin/api/masterplan/app/filesModule/application/usecase/RemoveFileUseCase.kt
@@ -0,0 +1,20 @@
+package api.masterplan.app.filesModule.application.usecase
+
+import api.masterplan.app.filesModule.application.command.RemoveFileCommand
+import api.masterplan.app.filesModule.domain.interfaces.DocumentFileService
+import api.masterplan.app.filesModule.domain.model.value.DocumentFileId
+import org.springframework.stereotype.Service
+
+@Service
+class RemoveFileUseCase(
+    private val documentFileService: DocumentFileService
+) {
+    operator fun invoke(command: RemoveFileCommand): Result<DocumentFileId>{
+        return try {
+            val deletedFileId = documentFileService.removeFile(command.fileId)
+            Result.success(deletedFileId)
+        }catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+}
