b7bc9ba18009baa79118c5c4e2230f3548213602
Leturgone <finekon221@gmail.com>
2026-03-21 23:49:30 +0300

    remove space

--- src/main/kotlin/api/masterplan/app/filesModule/application/command/UpdateFileCommand.kt
+++ src/main/kotlin/api/masterplan/app/filesModule/application/command/UpdateFileCommand.kt
@@ -4,7 +4,6 @@ import api.masterplan.app.filesModule.domain.model.value.DocumentFileBaseName
 import api.masterplan.app.filesModule.domain.model.value.DocumentFileData
 import api.masterplan.app.filesModule.domain.model.value.DocumentFileId
 
-
 data class UpdateFileCommand(
     val fileId: DocumentFileId,
     val documentFileBaseName: DocumentFileBaseName,
