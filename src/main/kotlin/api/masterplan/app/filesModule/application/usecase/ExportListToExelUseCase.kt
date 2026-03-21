57114eedc46721e2ca57775a68f99aa7ad1787b5
Leturgone <finekon221@gmail.com>
2026-03-21 23:40:33 +0300

    add ExportListToExelUseCase

--- src/main/kotlin/api/masterplan/app/filesModule/application/usecase/ExportListToExelUseCase.kt
+++ src/main/kotlin/api/masterplan/app/filesModule/application/usecase/ExportListToExelUseCase.kt
@@ -1,17 +1,18 @@
 package api.masterplan.app.filesModule.application.usecase
 
 import api.masterplan.app.filesModule.application.command.ExportListToExelCommand
+import api.masterplan.app.filesModule.domain.dtos.ExportFileDetails
 import api.masterplan.app.filesModule.domain.interfaces.ExportFileService
-import api.masterplan.app.filesModule.domain.model.entity.ExportFile
 import org.springframework.stereotype.Service
 
 @Service
 class ExportListToExelUseCase(
     private val exportFileService: ExportFileService
 ) {
-    operator fun invoke(command: ExportListToExelCommand): Result<ExportFile> {
+    operator fun <T> invoke(command: ExportListToExelCommand<T> ): Result<ExportFileDetails> {
         return try {
-            val exportedFile = exportFileService.exportListToExel(command)
+            val exportedFile = exportFileService.exportListToExel(command.data)
+            Result.success(exportedFile)
         }catch (e: Exception){
             Result.failure(e)
         }
