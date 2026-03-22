35b74953fd4481e57427ead3076b006bebcde94b
Leturgone <finekon221@gmail.com>
2026-03-22 20:47:59 +0300

    add DocumentDatabaseMapper

---
+++ src/main/kotlin/api/masterplan/app/filesModule/infrastructure/database/mapper/DocumentDatabaseMapper.kt
@@ -0,0 +1,24 @@
+package api.masterplan.app.filesModule.infrastructure.database.mapper
+
+import api.masterplan.app.filesModule.domain.model.value.DocumentFileId
+import api.masterplan.app.filesModule.domain.model.value.DocumentFileName
+import api.masterplan.app.filesModule.infrastructure.database.dto.DocumentEntityDto
+import api.masterplan.app.filesModule.infrastructure.database.entity.DocumentEntity
+
+object DocumentDatabaseMapper {
+    fun toDto(entity: DocumentEntity): DocumentEntityDto {
+        return DocumentEntityDto(
+            id = DocumentFileId(entity.id),
+            name = DocumentFileName(entity.name),
+            path = entity.path
+        )
+    }
+
+    fun toEntity(dto: DocumentEntityDto): DocumentEntity {
+        return DocumentEntity(
+            id = dto.id.value,
+            name = dto.name.value,
+            path = dto.path
+        )
+    }
+}
