9b68f4544a3bf1d1ef3fdeccde5f44390714503d
Leturgone <finekon221@gmail.com>
2026-03-22 21:08:17 +0300

    remove unused method

--- src/main/kotlin/api/masterplan/app/filesModule/domain/interfaces/ExportFileRepository.kt
+++ src/main/kotlin/api/masterplan/app/filesModule/domain/interfaces/ExportFileRepository.kt
@@ -4,5 +4,4 @@ import api.masterplan.app.filesModule.domain.model.entity.ExportFile
 
 interface ExportFileRepository {
     fun <T> exportListToExel(data: List<T>): ExportFile
-    fun <T> exportSingletEntityToExel(data: T): ExportFile
 }
