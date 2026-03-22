30615ec581d86bb420125b0117de4206a70f2906
Leturgone <finekon221@gmail.com>
2026-03-22 21:08:01 +0300

    add JpaDocumentRepository

---
+++ src/main/kotlin/api/masterplan/app/filesModule/infrastructure/database/repository/JpaDocumentRepository.kt
@@ -0,0 +1,10 @@
+package api.masterplan.app.filesModule.infrastructure.database.repository
+
+import api.masterplan.app.filesModule.infrastructure.database.entity.DocumentEntity
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface JpaDocumentRepository: CrudRepository<DocumentEntity, UUID> {
+}
