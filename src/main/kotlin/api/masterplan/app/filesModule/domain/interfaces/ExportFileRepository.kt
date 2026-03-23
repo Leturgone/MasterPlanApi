4076db454622dccfc951a5090444d3a0774638e4
Leturgone <finekon221@gmail.com>
2026-03-23 20:50:53 +0300

    rename

--- src/main/kotlin/api/masterplan/app/filesModule/domain/interfaces/ExportFileRepository.kt
+++ src/main/kotlin/api/masterplan/app/filesModule/domain/interfaces/ExportFileRepository.kt
@@ -1,7 +1,8 @@
 package api.masterplan.app.filesModule.domain.interfaces
 
 import api.masterplan.app.filesModule.domain.model.entity.ExportFile
+import api.masterplan.app.filesModule.domain.model.value.DocumentFileBaseName
 
 interface ExportFileRepository {
-    fun <T> exportListToExel(data: List<T>): ExportFile
+    fun <T: Any> exportListToExcel(fileName: DocumentFileBaseName, data: List<T>): ExportFile
 }
