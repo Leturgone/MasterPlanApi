1db92dd14e35cfafb4f8b070b3a1bbfd355ab916
Leturgone <finekon221@gmail.com>
2026-03-23 14:52:08 +0300

    add existsByBaseName method

--- src/main/kotlin/api/masterplan/app/filesModule/infrastructure/database/repository/JpaDocumentRepository.kt
+++ src/main/kotlin/api/masterplan/app/filesModule/infrastructure/database/repository/JpaDocumentRepository.kt
@@ -1,10 +1,15 @@
 package api.masterplan.app.filesModule.infrastructure.database.repository
 
 import api.masterplan.app.filesModule.infrastructure.database.entity.DocumentEntity
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
 interface JpaDocumentRepository: CrudRepository<DocumentEntity, UUID> {
+
+    @Query("SELECT p FROM Document p WHERE p.name LIKE CONCAT(:baseName, '%')")
+    fun existsByBaseName(baseName: String): Boolean
+
 }
