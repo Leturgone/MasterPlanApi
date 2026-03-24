ba85af7a24e9e2c55aab58cd994b55519c1a97f2
Leturgone <finekon221@gmail.com>
2026-03-24 23:44:38 +0300

    add ReportModuleExceptions

--- src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/adapters/ReportInnerModuleErrorMapper.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/adapters/ReportInnerModuleErrorMapper.kt
@@ -1,13 +1,22 @@
 package api.masterplan.app.reportsModule.infrastructure.adapters
 
 import api.masterplan.app.employeeModule.EmployeeModuleErrorDto
+import api.masterplan.app.filesModule.FilesModuleErrorDto
 import api.masterplan.app.reportsModule.domain.exceptions.ReportException
+import api.masterplan.app.reportsModule.domain.exceptions.ReportException.*
 
 internal object ReportInnerModuleErrorMapper {
     fun exceptionToModuleException(ex: Throwable): ReportException{
         return when(ex){
-            is EmployeeModuleErrorDto.InternalServerError -> ReportException.InternalServerError(ex.message)
-            else -> ReportException.InternalServerError()
+            is EmployeeModuleErrorDto.InternalServerError -> InternalServerError(ex.message)
+            is FilesModuleErrorDto.FailedToCreateFile -> FailedToCreateReportFile(ex.fileName)
+            is FilesModuleErrorDto.FailedToDeleteFile -> FailedToDeleteReportFile(ex.fileId)
+            is FilesModuleErrorDto.FailedToUpdateFile -> FailedToUpdateReportFile(ex.fileId)
+            is FilesModuleErrorDto.FileAlreadyExists -> ReportFileAlreadyExists(ex.fileName)
+            is FilesModuleErrorDto.FileNotExist -> ReportFileNotExist(ex.fileId)
+            is FilesModuleErrorDto.InternalServerError -> InternalServerError(ex.message)
+            is FilesModuleErrorDto.InvalidFileName -> InvalidReportTitle(ex.message)
+            else -> InternalServerError()
         }
     }
 }
