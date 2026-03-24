d729087d3f23a9a9c251289ca164330aba472aa6
Leturgone <finekon221@gmail.com>
2026-03-24 18:21:06 +0300

    add errorMessageParan

--- src/main/kotlin/api/masterplan/app/filesModule/domain/model/value/DocumentFileBaseName.kt
+++ src/main/kotlin/api/masterplan/app/filesModule/domain/model/value/DocumentFileBaseName.kt
@@ -1,18 +1,16 @@
 package api.masterplan.app.filesModule.domain.model.value
 
 import api.masterplan.app.filesModule.domain.exceptions.FilesException
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 @JvmInline
 value class DocumentFileBaseName(val value: String){
     companion object {
-        fun generate(fileName: String):DocumentFileBaseName{
+        fun validate(fileName: String):DocumentFileBaseName{
             try {
                 require(fileName.isNotBlank()) { "File name cant be blank" }
                 require(fileName.length <= 125) { "File name too long" }
             } catch (e: IllegalArgumentException) {
-                throw FilesException.InvalidFileName(e.message)
+                throw FilesException.InvalidFileName(errorMessage = e.message)
             }
             return DocumentFileBaseName(fileName)
         }
