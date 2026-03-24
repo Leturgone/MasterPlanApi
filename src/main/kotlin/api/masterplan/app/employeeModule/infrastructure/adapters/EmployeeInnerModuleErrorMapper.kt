6cc78f8f9d53191178559e88c44e2f6996d2b5aa
Leturgone <finekon221@gmail.com>
2026-03-24 21:48:39 +0300

    add file module exception

--- src/main/kotlin/api/masterplan/app/employeeModule/infrastructure/adapters/EmployeeInnerModuleErrorMapper.kt
+++ src/main/kotlin/api/masterplan/app/employeeModule/infrastructure/adapters/EmployeeInnerModuleErrorMapper.kt
@@ -1,12 +1,14 @@
 package api.masterplan.app.employeeModule.infrastructure.adapters
 
 import api.masterplan.app.employeeModule.domain.exceptions.EmployeeException
+import api.masterplan.app.filesModule.FilesModuleErrorDto
 import api.masterplan.app.plansModule.PlanModuleErrorDto
 
 internal object EmployeeInnerModuleErrorMapper {
     fun exceptionToModuleException(ex: Throwable): EmployeeException{
         return when(ex){
             is PlanModuleErrorDto.InternalServerError -> EmployeeException.InternalServerError("Internal plan module error")
+            is FilesModuleErrorDto.InternalServerError -> EmployeeException.InternalServerError("Internal files module error")
             else -> EmployeeException.InternalServerError("Internal error")
         }
     }
