fbe053f6b13b0748e6c372fccce35a97f0ddcfc3
Leturgone <finekon221@gmail.com>
2026-03-24 23:43:25 +0300

    remove unused

--- src/main/kotlin/api/masterplan/app/filesModule/infrastructure/intermodule/InterModuleFileToDtoSuccessMapper.kt
+++ src/main/kotlin/api/masterplan/app/filesModule/infrastructure/intermodule/InterModuleFileToDtoSuccessMapper.kt
@@ -1,21 +1,12 @@
 package api.masterplan.app.filesModule.infrastructure.intermodule
 
-import api.masterplan.app.filesModule.ExportFileDto
 import api.masterplan.app.filesModule.FileDataDto
 import api.masterplan.app.filesModule.domain.dtos.DocumentFileDetails
 import api.masterplan.app.filesModule.domain.dtos.ExportFileDetails
 
 object InterModuleFileToDtoSuccessMapper {
-    fun toDto(file:ExportFileDetails): ExportFileDto{
-        return ExportFileDto(
-            fileData = file.fileData.value,
-            fileName = file.fileName.value
-        )
-    }
-
-    fun toDto(file: DocumentFileDetails): FileDataDto {
+    fun toDto(file:ExportFileDetails): FileDataDto{
         return FileDataDto(
-            fileId = file.fileId.value,
             fileData = file.fileData.value,
             fileName = file.fileName.value
         )
