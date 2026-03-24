3f4d130a5589791af23a025fb8c38d85ebb9da0b
Leturgone <finekon221@gmail.com>
2026-03-24 18:21:47 +0300

    add vals

--- src/main/kotlin/api/masterplan/app/filesModule/domain/exceptions/FilesException.kt
+++ src/main/kotlin/api/masterplan/app/filesModule/domain/exceptions/FilesException.kt
@@ -6,27 +6,27 @@ import api.masterplan.app.filesModule.domain.model.value.DocumentFileName
 
 sealed class FilesException(message: String) : Exception(message) {
 
-    class InvalidFileName(message: String?) : FilesException(
-        "Invalid file name: ${message?.let { ": $it" } ?: ""}"
+    class InvalidFileName(val errorMessage: String?) : FilesException(
+        "Invalid file name: ${errorMessage?.let { ": $it" } ?: ""}"
     )
 
-    class FileNotExist(fileId: DocumentFileId): FilesException(
+    class FileNotExist(val fileId: DocumentFileId): FilesException(
         "File with id ${fileId.value} not found"
     )
 
-    class FileAlreadyExists(fileName: DocumentFileBaseName): FilesException(
+    class FileAlreadyExists(val fileName: DocumentFileBaseName): FilesException(
         "File with name: ${fileName.value} already exists"
     )
 
-    class FailedToCreateFile(fileName: DocumentFileName): FilesException(
+    class FailedToCreateFile(val fileName: DocumentFileName): FilesException(
         "Failed to create file with name: ${fileName.value}"
     )
 
-    class FailedToUpdateFile(fileId: DocumentFileId): FilesException(
+    class FailedToUpdateFile(val fileId: DocumentFileId): FilesException(
         "Failed to update file with id: ${fileId.value}"
     )
 
-    class FailedToDeleteFile(fileId: DocumentFileId): FilesException(
+    class FailedToDeleteFile(val fileId: DocumentFileId): FilesException(
         "Failed to delete file with id: ${fileId.value}"
     )
 
