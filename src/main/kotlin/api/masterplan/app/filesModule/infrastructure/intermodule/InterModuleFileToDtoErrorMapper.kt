e9a31c0624d63f860d0883c4816531f6c974886d
Leturgone <finekon221@gmail.com>
2026-03-24 23:42:01 +0300

    edit types in InterModuleFileToDtoErrorMapper

--- src/main/kotlin/api/masterplan/app/filesModule/infrastructure/intermodule/InterModuleFileToDtoErrorMapper.kt
+++ src/main/kotlin/api/masterplan/app/filesModule/infrastructure/intermodule/InterModuleFileToDtoErrorMapper.kt
@@ -6,11 +6,11 @@ import api.masterplan.app.filesModule.domain.exceptions.FilesException
 object InterModuleFileToDtoErrorMapper {
     fun toDto(exception: Throwable): FilesModuleErrorDto{
         return when (exception) {
-            is FilesException.FailedToCreateFile -> FilesModuleErrorDto.FailedToCreateFile(exception.fileName)
-            is FilesException.FailedToDeleteFile -> FilesModuleErrorDto.FailedToDeleteFile(exception.fileId)
-            is FilesException.FailedToUpdateFile -> FilesModuleErrorDto.FailedToUpdateFile(exception.fileId)
-            is FilesException.FileAlreadyExists -> FilesModuleErrorDto.FileAlreadyExists(exception.fileName)
-            is FilesException.FileNotExist -> FilesModuleErrorDto.FileNotExist(exception.fileId)
+            is FilesException.FailedToCreateFile -> FilesModuleErrorDto.FailedToCreateFile(exception.fileName.value)
+            is FilesException.FailedToDeleteFile -> FilesModuleErrorDto.FailedToDeleteFile(exception.fileId.value)
+            is FilesException.FailedToUpdateFile -> FilesModuleErrorDto.FailedToUpdateFile(exception.fileId.value)
+            is FilesException.FileAlreadyExists -> FilesModuleErrorDto.FileAlreadyExists(exception.fileName.value)
+            is FilesException.FileNotExist -> FilesModuleErrorDto.FileNotExist(exception.fileId.value)
             is FilesException.InvalidFileName -> FilesModuleErrorDto.InvalidFileName(errorMessage = exception.errorMessage)
             else -> FilesModuleErrorDto.InternalServerError("Internal files module server error")
         }
