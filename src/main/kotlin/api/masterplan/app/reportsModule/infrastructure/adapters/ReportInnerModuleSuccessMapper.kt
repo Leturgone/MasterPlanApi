a0888c7ec58e9b36918edab16dc2e0671015f325
Leturgone <finekon221@gmail.com>
2026-03-24 23:44:12 +0300

    add toReportDocumentId

--- src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/adapters/ReportInnerModuleSuccessMapper.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/infrastructure/adapters/ReportInnerModuleSuccessMapper.kt
@@ -1,10 +1,14 @@
 package api.masterplan.app.reportsModule.infrastructure.adapters
 
+import api.masterplan.app.reportsModule.domain.models.value.ReportDocumentId
 import api.masterplan.app.reportsModule.domain.models.value.ReportEmployeeId
 import java.util.UUID
 
 internal object ReportInnerModuleSuccessMapper {
-    fun toReportEmpIdSet(ids:Set<UUID>):Set<ReportEmployeeId>  {
-        return ids.map{ReportEmployeeId(it)}.toSet()
+
+    fun toReportEmpIdSet(ids: Set<UUID>): Set<ReportEmployeeId> {
+        return ids.map { ReportEmployeeId(it) }.toSet()
     }
+
+    fun toReportDocumentId(id: UUID) = ReportDocumentId(id)
 }
