22e9e79786debe402b8bbc0ce47458ef2e1898d5
Leturgone <finekon221@gmail.com>
2026-03-25 16:35:43 +0300

    add PlansInnerModuleErrorMapper

---
+++ src/main/kotlin/api/masterplan/app/plansModule/infrastructure/adapters/PlansInnerModuleErrorMapper.kt
@@ -0,0 +1,23 @@
+package api.masterplan.app.plansModule.infrastructure.adapters
+
+import api.masterplan.app.employeeModule.EmployeeModuleErrorDto
+import api.masterplan.app.filesModule.FilesModuleErrorDto
+import api.masterplan.app.plansModule.domain.exceptions.PlanException
+
+object PlansInnerModuleErrorMapper {
+    fun exceptionToModuleException(ex: Throwable): PlanException{
+        return when(ex){
+            is EmployeeModuleErrorDto.EmployeeNotExist -> PlanException.ExecutorNotExist(ex.employeeId)
+            is EmployeeModuleErrorDto.InternalServerError -> PlanException.InternalServerError(ex.message)
+            is FilesModuleErrorDto.FailedToCreateFile -> PlanException.FailedToCreatePlanFile(ex.fileName)
+            is FilesModuleErrorDto.FailedToDeleteFile -> PlanException.FailedToDeletePlanFile(ex.fileId)
+            is FilesModuleErrorDto.FailedToUpdateFile -> PlanException.FailedToDeletePlanFile(ex.fileId)
+            is FilesModuleErrorDto.FileAlreadyExists -> PlanException.PlanFileAlreadyExists(ex.fileName)
+            is FilesModuleErrorDto.FileNotExist -> PlanException.PlanFileNotExist(ex.fileId)
+            is FilesModuleErrorDto.InternalServerError -> PlanException.InternalServerError(ex.message)
+            is FilesModuleErrorDto.InvalidFileName -> PlanException.InvalidPlanFileName(ex.errorMessage)
+            else -> PlanException.InternalServerError(ex.message)
+        }
+    }
+
+}
