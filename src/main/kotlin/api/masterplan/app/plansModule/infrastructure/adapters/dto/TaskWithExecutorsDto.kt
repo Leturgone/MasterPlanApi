963a6327fdbf1830e1d09d5f2048152ca2f1874c
Leturgone <finekon221@gmail.com>
2026-03-25 20:26:49 +0300

    add TaskWithExecutorsDto

---
+++ src/main/kotlin/api/masterplan/app/plansModule/infrastructure/adapters/dto/TaskWithExecutorsDto.kt
@@ -0,0 +1,19 @@
+package api.masterplan.app.plansModule.infrastructure.adapters.dto
+
+import api.masterplan.app.export.ExportDisplayName
+import java.time.LocalDate
+
+data class TaskWithExecutorsDto(
+    @ExportDisplayName("ID")
+    val id: String,
+    @ExportDisplayName("Название")
+    val title: String,
+    @ExportDisplayName("Описание")
+    val description: String,
+    @ExportDisplayName("Срок выполнения")
+    val endDate: LocalDate,
+    @ExportDisplayName("Статус выполнения")
+    val status: String,
+    @ExportDisplayName("Исполнители")
+    val executorsIds: List<ExecutorExportData>
+)
