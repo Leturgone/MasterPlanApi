ef975db3539254d4c508ebfc138fdce1c8bece29
Leturgone <finekon221@gmail.com>
2026-03-25 19:26:30 +0300

    add planTitle to exportPlan method

--- src/main/kotlin/api/masterplan/app/plansModule/application/ports/PlanFilesPort.kt
+++ src/main/kotlin/api/masterplan/app/plansModule/application/ports/PlanFilesPort.kt
@@ -1,10 +1,14 @@
 package api.masterplan.app.plansModule.application.ports
 
+import api.masterplan.app.plansModule.application.dto.ExecutorDto
 import api.masterplan.app.plansModule.application.dto.PlanExportFile
 import api.masterplan.app.plansModule.application.dto.PlanFile
 import api.masterplan.app.plansModule.application.dto.TaskFile
 import api.masterplan.app.plansModule.domain.dtos.PlanDetails
+import api.masterplan.app.plansModule.domain.dtos.TaskDetails
+import api.masterplan.app.plansModule.domain.model.entity.Task
 import api.masterplan.app.plansModule.domain.model.value.PlanDocumentId
+import api.masterplan.app.plansModule.domain.model.value.PlanTitle
 import api.masterplan.app.plansModule.domain.model.value.TaskDocumentId
 
 interface PlanFilesPort {
@@ -13,7 +17,7 @@ interface PlanFilesPort {
 
     fun uploadTaskFile(taskFile: TaskFile): TaskDocumentId
 
-    fun exportPlan(plan: PlanDetails): PlanExportFile
+    fun exportPlan(planTitle: PlanTitle, planTaskList: List<TaskDetails>, executors: List<ExecutorDto>): PlanExportFile
 
     fun removePlanFile(planFileId: PlanDocumentId): PlanDocumentId
 
