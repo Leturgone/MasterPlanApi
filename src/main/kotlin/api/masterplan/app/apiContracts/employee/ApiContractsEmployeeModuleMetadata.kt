ce49c205325389525e99f6d19be5c79522e5d7a6
Leturgone <finekon221@gmail.com>
2026-03-26 18:39:54 +0300

    add ApiContractsEmployeeModuleMetadata

---
+++ src/main/kotlin/api/masterplan/app/apiContracts/employee/ApiContractsEmployeeModuleMetadata.kt
@@ -0,0 +1,9 @@
+package api.masterplan.app.apiContracts.employee
+
+import org.springframework.modulith.NamedInterface
+import org.springframework.modulith.PackageInfo
+
+@PackageInfo
+@NamedInterface("employee")
+class ApiContractsEmployeeModuleMetadata {
+}
