b632a5101b11ee0bc69760f5d7aa94253ccb3693
Leturgone <finekon221@gmail.com>
2026-03-27 16:45:19 +0300

    edit with multipart data

--- src/main/kotlin/api/masterplan/app/filesModule/presentation/mapper/FilesDomainToResponseMapper.kt
+++ src/main/kotlin/api/masterplan/app/filesModule/presentation/mapper/FilesDomainToResponseMapper.kt
@@ -2,12 +2,21 @@ package api.masterplan.app.filesModule.presentation.mapper
 
 import api.masterplan.app.filesModule.domain.dtos.DocumentFileDetails
 import api.masterplan.app.filesModule.presentation.dto.response.FileResponse
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 
 object FilesDomainToResponseMapper {
     fun toFileResponse(file: DocumentFileDetails): FileResponse{
+        val contentType = "application/pdf"
+
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.parseMediaType(contentType)
+        headers.set("Content-ID", file.fileId.value.toString())
+        headers.setContentDispositionFormData("file", "application/pdf")
+        headers.contentLength = file.fileData.value.size.toLong()
+
         return FileResponse(
-            fileId = file.fileId.value,
-            fileName = file.fileName.value,
+            fileHeaders = headers,
             fileData = file.fileData.value
         )
     }
