9483b8434c827bc080c4161fa5086a7b037320a3
Leturgone <finekon221@gmail.com>
2026-03-28 00:16:51 +0300

    remove file data from request

--- src/main/kotlin/api/masterplan/app/reportsModule/presentation/dto/request/CreateReportRequest.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/presentation/dto/request/CreateReportRequest.kt
@@ -18,40 +18,5 @@ data class CreateReportRequest(
     val employeeId: UUID,
     @NotBlank(message = "Reference id cant be blank")
     @Schema(description = "ID задачи или плана, на которую ссылается отчет UUIDv7", example = "06115aa098-9277-0087-49a8-cb901fc2f7")
-    val referenceId: UUID,
-    @NotBlank(message = "Document name cant be blank")
-    @Schema(description = "Название файла", example = "PlanExport23022026172732")
-    val documentName: String,
-    @NotBlank(message = "Document cant be blank")
-    @Schema(description = "Файл")
-    val document: ByteArray
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as CreateReportRequest
-
-        if (id != other.id) return false
-        if (title != other.title) return false
-        if (description != other.description) return false
-        if (employeeId != other.employeeId) return false
-        if (referenceId != other.referenceId) return false
-        if (documentName != other.documentName) return false
-        if (!document.contentEquals(other.document)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id?.hashCode() ?: 0
-        result = 31 * result + title.hashCode()
-        result = 31 * result + (description?.hashCode() ?: 0)
-        result = 31 * result + employeeId.hashCode()
-        result = 31 * result + referenceId.hashCode()
-        result = 31 * result + documentName.hashCode()
-        result = 31 * result + document.contentHashCode()
-        return result
-    }
-
-}
+    val referenceId: UUID
+)
