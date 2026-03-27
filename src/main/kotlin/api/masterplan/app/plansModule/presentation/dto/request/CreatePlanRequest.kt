a617a126a8820d503ef17c85f4b7c34d86ef330c
Leturgone <finekon221@gmail.com>
2026-03-27 19:40:04 +0300

    remove file inf from request

--- src/main/kotlin/api/masterplan/app/plansModule/presentation/dto/request/CreatePlanRequest.kt
+++ src/main/kotlin/api/masterplan/app/plansModule/presentation/dto/request/CreatePlanRequest.kt
@@ -15,14 +15,12 @@ data class CreatePlanRequest(
     @NotBlank(message = "Description cant be blank")
     @Schema(description = "Описание плана", example = "Выполнить мероприятия для выполнения цели")
     val description: String,
-    @Schema(description = "Дата начала выполнения плана", example = "01.01.2026")
+    @Schema(description = "Дата начала выполнения плана", example = "2026-05-06")
     val startDate: LocalDate? = null,
     @NotBlank(message = "End date cant be blank")
-    @Schema(description = "Финальный срок выполнения плана", example = "01.01.2026")
+    @Schema(description = "Финальный срок выполнения плана", example = "2026-05-06")
     val endDate: LocalDate,
     @NotBlank(message = "Director id cant be blank")
     @Schema(description = "ID руководителя плана", example = "06115aa098-9277-0087-49a8-cb901fc2f7")
-    val directorId: UUID,
-    @Schema(description = "Название файла", example = "PlanExport23022026172732")
-    val documentName: String? = null,
+    val directorId: UUID
 )
