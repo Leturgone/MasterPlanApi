d946c79a9d1770f639d38038ee8275fc8c5d2b0f
Leturgone <finekon221@gmail.com>
2026-03-27 16:46:29 +0300

    edit response

--- src/main/kotlin/api/masterplan/app/filesModule/presentation/dto/response/FileResponse.kt
+++ src/main/kotlin/api/masterplan/app/filesModule/presentation/dto/response/FileResponse.kt
@@ -1,18 +1,8 @@
 package api.masterplan.app.filesModule.presentation.dto.response
 
-import io.swagger.v3.oas.annotations.media.Schema
-import java.util.*
-
-@Schema(description = "Ответ Успешно скачанный файл")
 data class FileResponse(
-    @Schema(description = "ID файла UUIDv7",
-        example = "06115aa098-9277-0087-49a8-cb901fc2f7"
-    )
-    val fileId: UUID,
-    @Schema(description = "Файл")
+    val fileHeaders: org.springframework.http.HttpHeaders,
     val fileData: ByteArray,
-    @Schema(description = "Название файла", example = "23022026172732Отчет")
-    val fileName: String,
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -20,17 +10,15 @@ data class FileResponse(
 
         other as FileResponse
 
-        if (fileId != other.fileId) return false
+        if (fileHeaders != other.fileHeaders) return false
         if (!fileData.contentEquals(other.fileData)) return false
-        if (fileName != other.fileName) return false
 
         return true
     }
 
     override fun hashCode(): Int {
-        var result = fileId.hashCode()
+        var result = fileHeaders.hashCode()
         result = 31 * result + fileData.contentHashCode()
-        result = 31 * result + fileName.hashCode()
         return result
     }
 }
