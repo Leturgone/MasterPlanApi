7edf6edc91b7aface3edd3be6a144b6e8d803601
Leturgone <finekon221@gmail.com>
2026-03-28 20:03:07 +0300

    add AdminRequestDetails

---
+++ src/main/kotlin/api/masterplan/app/adminRequestsModule/domain/dto/AdminRequestDetails.kt
@@ -0,0 +1,17 @@
+package api.masterplan.app.adminRequestsModule.domain.dto
+
+import api.masterplan.app.adminRequestsModule.domain.model.value.AdminRequestCreationDate
+import api.masterplan.app.adminRequestsModule.domain.model.value.AdminRequestDescription
+import api.masterplan.app.adminRequestsModule.domain.model.value.AdminRequestId
+import api.masterplan.app.adminRequestsModule.domain.model.value.AdminRequestSenderId
+import api.masterplan.app.adminRequestsModule.domain.model.value.AdminRequestStatus
+import api.masterplan.app.adminRequestsModule.domain.model.value.AdminRequestTitle
+
+data class AdminRequestDetails(
+    val id: AdminRequestId,
+    val title: AdminRequestTitle,
+    val description: AdminRequestDescription,
+    val creationDate: AdminRequestCreationDate,
+    val senderId: AdminRequestSenderId,
+    val status: AdminRequestStatus
+)
