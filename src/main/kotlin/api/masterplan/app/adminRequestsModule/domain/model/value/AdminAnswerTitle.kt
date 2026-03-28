d6ba4e1fc62bfcc2c7c3c796f2f5f74a16a0e572
Leturgone <finekon221@gmail.com>
2026-03-28 19:39:59 +0300

    edit return types

--- src/main/kotlin/api/masterplan/app/adminRequestsModule/domain/model/value/AdminAnswerTitle.kt
+++ src/main/kotlin/api/masterplan/app/adminRequestsModule/domain/model/value/AdminAnswerTitle.kt
@@ -4,14 +4,14 @@ import api.masterplan.app.adminRequestsModule.domain.exception.AdminRequestExcep
 
 class AdminAnswerTitle(val value: String) {
     companion object {
-        fun validate(desc: String):AdminAnswerDescription{
+        fun validate(desc: String):AdminAnswerTitle{
             try {
                 require(desc.isNotEmpty()) { "Title cant be blank" }
                 require(desc.length <=100){ "Title too long" }
             }catch(e:IllegalStateException){
                 throw AdminRequestException.InvalidAdminAnswerTitle(e.message)
             }
-            return AdminAnswerDescription(desc)
+            return AdminAnswerTitle(desc)
         }
     }
 }
