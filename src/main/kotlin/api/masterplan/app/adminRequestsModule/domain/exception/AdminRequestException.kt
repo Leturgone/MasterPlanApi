305cbf536bc30578c4982c819e2129c321baad6b
Leturgone <finekon221@gmail.com>
2026-03-28 19:40:08 +0300

    add exceptions

--- src/main/kotlin/api/masterplan/app/adminRequestsModule/domain/exception/AdminRequestException.kt
+++ src/main/kotlin/api/masterplan/app/adminRequestsModule/domain/exception/AdminRequestException.kt
@@ -9,4 +9,12 @@ sealed class AdminRequestException(message: String):Exception(message) {
     class InvalidAdminAnswerDesc(message: String?) : AdminRequestException(
         "Invalid admin answer description: ${message?:""}"
     )
+
+    class InvalidAdminAnswerTitle(title: String?) : AdminRequestException(
+        "Invalid admin answer title: ${title?:""}"
+    )
+
+    class InvalidAdminRequestTitle(title: String?) : AdminRequestException(
+        "Invalid admin request title: ${title?:""}"
+    )
 }
