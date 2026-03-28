1f0a38e42a3843c0cc0adb9bf470f3ef412b56ed
Leturgone <finekon221@gmail.com>
2026-03-28 19:11:09 +0300

    add AdminRequestDescription

---
+++ src/main/kotlin/api/masterplan/app/adminRequestsModule/domain/model/value/AdminRequestDescription.kt
@@ -0,0 +1,17 @@
+package api.masterplan.app.adminRequestsModule.domain.model.value
+
+import api.masterplan.app.adminRequestsModule.domain.exception.AdminRequestException
+
+class AdminRequestDescription(val value: String) {
+    companion object {
+        fun validate(desc: String):AdminRequestDescription{
+            try {
+                require(desc.isNotEmpty()) { "Description cant be blank" }
+                require(desc.length <=255){ "Description too long" }
+            }catch(e:IllegalStateException){
+                throw AdminRequestException.InvalidAdminRequestDesc(e.message)
+            }
+            return AdminRequestDescription(desc)
+        }
+    }
+}
