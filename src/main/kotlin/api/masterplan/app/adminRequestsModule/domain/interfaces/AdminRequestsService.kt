f14b179b37122874ac9e7d335a129cab5ee02eb4
Leturgone <finekon221@gmail.com>
2026-03-28 20:26:57 +0300

    add AdminRequestsService

---
+++ src/main/kotlin/api/masterplan/app/adminRequestsModule/domain/interfaces/AdminRequestsService.kt
@@ -0,0 +1,25 @@
+package api.masterplan.app.adminRequestsModule.domain.interfaces
+
+import api.masterplan.app.adminRequestsModule.domain.dto.AdminAnswerDetails
+import api.masterplan.app.adminRequestsModule.domain.dto.AdminRequestDetails
+import api.masterplan.app.adminRequestsModule.domain.model.value.AdminAnswerId
+import api.masterplan.app.adminRequestsModule.domain.model.value.AdminRequestId
+import api.masterplan.app.adminRequestsModule.domain.model.value.AdminRequestSenderId
+import api.masterplan.app.adminRequestsModule.domain.model.value.AdminRequestStatus
+
+interface AdminRequestsService {
+
+    fun createAdminRequest(): AdminRequestId
+
+    fun createAdminAnswer(): AdminAnswerId
+
+    fun changeAdminRequestStatus(id: AdminRequestId,status: AdminRequestStatus): AdminRequestId
+
+    fun getAdminRequestsList(): List<AdminRequestDetails>
+
+    fun getAdminRequest(id: AdminAnswerId): AdminRequestDetails
+
+    fun getCreatedAdminRequestsBySenderList(senderId: AdminRequestSenderId ): List<AdminRequestDetails>
+
+    fun getAdminAnswerForRequest(id: AdminRequestId): AdminAnswerDetails
+}
