70394f9881c7cefde830a1abcbf5b1f19a5a3787
Leturgone <finekon221@gmail.com>
2026-03-29 19:32:48 +0300

    add AdminRequestsControllerExceptionHandler

---
+++ src/main/kotlin/api/masterplan/app/adminRequestsModule/presentation/api/exceptionHandler/AdminRequestsControllerExceptionHandler.kt
@@ -0,0 +1,5 @@
+package api.masterplan.app.adminRequestsModule.presentation.api.exceptionHandler
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class AdminRequestsControllerExceptionHandler()
