e5ee4c0814eac2c3200cef18915f9245cd8f1dde
Leturgone <finekon221@gmail.com>
2026-03-29 19:33:13 +0300

    add AdminRequestsErrorResponse

---
+++ src/main/kotlin/api/masterplan/app/adminRequestsModule/presentation/dto/response/AdminRequestsErrorResponse.kt
@@ -0,0 +1,17 @@
+package api.masterplan.app.adminRequestsModule.presentation.dto.response
+
+import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDateTime
+
+@Schema(description = "Неудачный ответ")
+data class AdminRequestsErrorResponse(
+
+    @Schema(description = "HTTP код ошибки", example = "500")
+    val status: Int,
+
+    @Schema(description = "Описание HTTP ошибки", example = "Error while login")
+    val message: String? = "",
+
+    @Schema(description = "Дата ошибки ", example = "2026-01-27T18:30:00Z")
+    val timestamp: LocalDateTime,
+)
