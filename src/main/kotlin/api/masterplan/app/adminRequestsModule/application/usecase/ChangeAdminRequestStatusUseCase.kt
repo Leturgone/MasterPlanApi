7febb895461c24a1efa8eca10d3f8b68c242bbc6
Leturgone <finekon221@gmail.com>
2026-03-29 12:24:03 +0300

    add ChangeAdminRequestStatusUseCase

---
+++ src/main/kotlin/api/masterplan/app/adminRequestsModule/application/usecase/ChangeAdminRequestStatusUseCase.kt
@@ -0,0 +1,23 @@
+package api.masterplan.app.adminRequestsModule.application.usecase
+
+import api.masterplan.app.adminRequestsModule.application.command.ChangeAdminRequestStatusCommand
+import api.masterplan.app.adminRequestsModule.domain.interfaces.AdminRequestsService
+import api.masterplan.app.adminRequestsModule.domain.model.value.AdminRequestId
+import org.springframework.stereotype.Service
+
+@Service
+class ChangeAdminRequestStatusUseCase(
+    private val adminRequestsService: AdminRequestsService
+) {
+    operator fun invoke(command: ChangeAdminRequestStatusCommand) : Result<AdminRequestId>{
+        return try {
+            val requestId = adminRequestsService.changeAdminRequestStatus(
+                id = command.id,
+                status = command.status
+            )
+            Result.success(requestId)
+        }catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+}
