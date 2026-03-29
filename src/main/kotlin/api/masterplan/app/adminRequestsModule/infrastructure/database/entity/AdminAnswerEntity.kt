5b6769b27239f81a233b0ee264dac84c57fe1c6f
Leturgone <finekon221@gmail.com>
2026-03-29 14:36:41 +0300

    add AdminAnswerEntity

---
+++ src/main/kotlin/api/masterplan/app/adminRequestsModule/infrastructure/database/entity/AdminAnswerEntity.kt
@@ -0,0 +1,36 @@
+package api.masterplan.app.adminRequestsModule.infrastructure.database.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.util.*
+
+@Entity
+@Table(name = "admin_answer")
+class AdminAnswerEntity(
+
+    @Id
+    @Column(name = "id")
+    val id: Int,
+
+    @Column(name = "title", length = 100, nullable = false)
+    val title: String,
+
+    @Column(name = "description", length = 255, nullable = false)
+    val description : String,
+
+    @Column(name = "admin_request_id",nullable = false)
+    val adminRequestId: UUID,
+){
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as AdminAnswerEntity
+        return id == other.id
+    }
+
+    override fun hashCode(): Int = id.hashCode()
+
+    override fun toString(): String = "AdminAnswerEntity(id=$id, title='$title', description='$description', adminRequestId=$adminRequestId)"
+}
