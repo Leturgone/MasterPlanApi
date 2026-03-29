f30c6ec52db10fa9acd4116d0127fd2a11539469
Leturgone <finekon221@gmail.com>
2026-03-30 00:37:01 +0300

    add docs

--- src/main/kotlin/api/masterplan/app/adminRequestsModule/presentation/api/controller/AdminRequestsController.kt
+++ src/main/kotlin/api/masterplan/app/adminRequestsModule/presentation/api/controller/AdminRequestsController.kt
@@ -1,38 +1,23 @@
 package api.masterplan.app.adminRequestsModule.presentation.api.controller
 
-import api.masterplan.app.adminRequestsModule.application.command.ChangeAdminRequestStatusCommand
-import api.masterplan.app.adminRequestsModule.application.command.CreateAdminAnswerCommand
-import api.masterplan.app.adminRequestsModule.application.command.CreateAdminRequestCommand
-import api.masterplan.app.adminRequestsModule.application.command.GetAdminAnswerForRequestCommand
-import api.masterplan.app.adminRequestsModule.application.command.GetAdminRequestCommand
-import api.masterplan.app.adminRequestsModule.application.command.GetCreatedAdminRequestsBySenderListCommand
-import api.masterplan.app.adminRequestsModule.application.usecase.ChangeAdminRequestStatusUseCase
-import api.masterplan.app.adminRequestsModule.application.usecase.CreateAdminAnswerUseCase
-import api.masterplan.app.adminRequestsModule.application.usecase.CreateAdminRequestUseCase
-import api.masterplan.app.adminRequestsModule.application.usecase.GetAdminAnswerForRequestUseCase
-import api.masterplan.app.adminRequestsModule.application.usecase.GetAdminRequestUseCase
-import api.masterplan.app.adminRequestsModule.application.usecase.GetAdminRequestsListUseCase
-import api.masterplan.app.adminRequestsModule.application.usecase.GetCreatedAdminRequestsBySenderListUseCase
+import api.masterplan.app.adminRequestsModule.application.command.*
+import api.masterplan.app.adminRequestsModule.application.usecase.*
 import api.masterplan.app.adminRequestsModule.presentation.api.exceptionHandler.AdminRequestsControllerExceptionHandler
 import api.masterplan.app.adminRequestsModule.presentation.dto.request.CreateAdminAnswerRequest
 import api.masterplan.app.adminRequestsModule.presentation.dto.request.CreateAdminRequestRequest
 import api.masterplan.app.adminRequestsModule.presentation.dto.request.UpdateRequestStatusRequest
-import api.masterplan.app.adminRequestsModule.presentation.dto.response.AdminAnswerIdResponse
-import api.masterplan.app.adminRequestsModule.presentation.dto.response.AdminAnswerResponse
-import api.masterplan.app.adminRequestsModule.presentation.dto.response.AdminRequestIdResponse
-import api.masterplan.app.adminRequestsModule.presentation.dto.response.AdminRequestResponse
+import api.masterplan.app.adminRequestsModule.presentation.dto.response.*
 import api.masterplan.app.adminRequestsModule.presentation.mapper.AdminRequestsDomainToResponseMapper
 import api.masterplan.app.adminRequestsModule.presentation.mapper.AdminRequestsRequestsToDomainMapper
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-import java.util.UUID
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @AdminRequestsControllerExceptionHandler
@@ -48,6 +33,38 @@ class AdminRequestsController(
     private val getCreatedAdminRequestsBySenderListUseCase: GetCreatedAdminRequestsBySenderListUseCase
 ) {
 
+
+    @Operation(
+        summary = "Изменение статуса заявки",
+        description = "Изменение статуса заявки по id заявки",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Статус заявки изменен",
+                content = [Content(schema = Schema(implementation = AdminRequestIdResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Некорректные данные запроса",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "Заявка с указанным id не найдена",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Внутренняя ошибка сервера: сбой при обновлении статуса",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "Нет роли админа"
+            )
+
+        ]
+    )
     @PatchMapping(("/admin/requests/{requestId}/status"))
     fun  changeAdminRequestStatus(
         @PathVariable(value = "requestId") requestId: UUID,
@@ -66,6 +83,38 @@ class AdminRequestsController(
     }
 
 
+
+    @Operation(
+        summary = "Создание ответа на заявку",
+        description = "Создание ответа на заявку с указанием всех данных",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Ответ на заявку создан",
+                content = [Content(schema = Schema(implementation = AdminAnswerIdResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Некорректные данные запроса",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "Заявка с указанным id не найдена",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Внутренняя ошибка сервера: сбой при создании ответа",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "Нет роли админа"
+            )
+
+        ]
+    )
     @PostMapping(("/admin/requests/{requestId}/answer"))
     fun createAdminAnswer(
         @PathVariable(value = "requestId") requestId: UUID,
@@ -85,6 +134,33 @@ class AdminRequestsController(
         return ResponseEntity.ok(resp)
     }
 
+
+    @Operation(
+        summary = "Создание заявки",
+        description = "Создание новой заявки с указанием всех данных",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Заявка создана",
+                content = [Content(schema = Schema(implementation = AdminAnswerIdResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Некорректные данные запроса",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Внутренняя ошибка сервера: сбой при создании заявки",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "Нет роли руководителя"
+            )
+
+        ]
+    )
     @PostMapping(("/dir/requests/sender/{senderId}"))
     fun createAdminRequest(
         @PathVariable(value = "senderId}") senderId: UUID,
@@ -104,6 +180,38 @@ class AdminRequestsController(
         return ResponseEntity.ok(resp)
     }
 
+
+    @Operation(
+        summary = "Получение ответа на заявку",
+        description = "Получение ответа на заявку по id заявки",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Ответ получен",
+                content = [Content(schema = Schema(implementation = AdminAnswerResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Некорректные данные запроса",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "Ответ на указанную заявку не найден",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Внутренняя ошибка сервера: сбой при получении ответа",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "Нет роли руководителя или админа"
+            )
+
+        ]
+    )
     @GetMapping(("/dir/requests/{requestId}/answer"))
     fun getAdminAnswerForRequest(
         @PathVariable(value = "requestId") requestId: UUID
@@ -119,6 +227,30 @@ class AdminRequestsController(
         return ResponseEntity.ok(resp)
     }
 
+
+    @Operation(
+        summary = "Получение списка всех заявок ",
+        description = "Получение списка всех заявок",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Список заявок получен",
+                content = [Content(
+                    array = ArraySchema(schema = Schema(implementation = AdminRequestResponse::class)))
+                ]
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Внутренняя ошибка сервера: сбой при получении списка заявок",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "Нет роли руководителя или админа"
+            )
+
+        ]
+    )
     @GetMapping(("/dir/requests"))
     fun getAdminRequestsList(): ResponseEntity<List<AdminRequestResponse>>{
         val result = getAdminRequestsListUseCase().getOrThrow()
@@ -126,6 +258,38 @@ class AdminRequestsController(
         return ResponseEntity.ok(resp)
     }
 
+
+    @Operation(
+        summary = "Получение информации о заявке",
+        description = "Получение информациии о заявке по id заявки",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Заявка получена",
+                content = [Content(schema = Schema(implementation = AdminAnswerResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Некорректные данные запроса",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "Заявка не найдена",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Внутренняя ошибка сервера: сбой при получении заявки",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "Нет роли руководителя или админа"
+            )
+
+        ]
+    )
     @GetMapping(("/dir/requests/{requestId}"))
     fun getAdminRequest(
         @PathVariable(value = "requestId") requestId: UUID
@@ -141,6 +305,35 @@ class AdminRequestsController(
         return ResponseEntity.ok(resp)
     }
 
+
+    @Operation(
+        summary = "Получение списка всех заявок созданных отправителем",
+        description = "Получение списка всех заявок созданных отправителем по id отправителя",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Список заявок получен",
+                content = [Content(
+                    array = ArraySchema(schema = Schema(implementation = AdminRequestResponse::class)))
+                ]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Некорректные данные запроса",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Внутренняя ошибка сервера: сбой при получении списка заявок",
+                content = [Content(schema = Schema(implementation = AdminRequestsErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "Нет роли руководителя или админа"
+            )
+
+        ]
+    )
     @GetMapping(("/dir/requests/sender/{senderId}"))
     fun getCreatedAdminRequestsBySenderList(
         @PathVariable(value = "senderId") senderId: UUID
