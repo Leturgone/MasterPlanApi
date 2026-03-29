617b4772e9056c02299f0140d1fd05994170c7fb
Leturgone <finekon221@gmail.com>
2026-03-29 14:48:36 +0300

    add JpaAdminAnswerRepository

---
+++ src/main/kotlin/api/masterplan/app/adminRequestsModule/infrastructure/database/repository/JpaAdminAnswerRepository.kt
@@ -0,0 +1,8 @@
+package api.masterplan.app.adminRequestsModule.infrastructure.database.repository
+
+import api.masterplan.app.adminRequestsModule.infrastructure.database.entity.AdminAnswerEntity
+import org.springframework.data.repository.CrudRepository
+import java.util.*
+
+interface JpaAdminAnswerRepository: CrudRepository<AdminAnswerEntity, UUID>  {
+}
