98e4701c989b3b299a3266e639551766bbda3b30
Leturgone <finekon221@gmail.com>
2026-03-30 23:06:58 +0300

    add WebSocketConfig

---
+++ src/main/kotlin/api/masterplan/app/notification/config/WebSocketConfig.kt
@@ -0,0 +1,25 @@
+package api.masterplan.app.notification.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfig: WebSocketMessageBrokerConfigurer {
+
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        // Создание простого брокера для отправки сообщений (исходящая рассылка)
+        registry.enableSimpleBroker("/topic")
+        // Входящая рассылка
+        registry.setApplicationDestinationPrefixes("/app")
+    }
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        // Создание точки входа для клиентов
+        registry.addEndpoint("/websocket").setAllowedOrigins("*").withSockJS()
+    }
+
+}
