c7006b4e2079ea32b83db939c24badf07f8dcef3
Leturgone <finekon221@gmail.com>
2026-03-31 18:54:04 +0300

    remove title from command

--- src/main/kotlin/api/masterplan/app/notification/application/command/SendNotificationCommand.kt
+++ src/main/kotlin/api/masterplan/app/notification/application/command/SendNotificationCommand.kt
@@ -3,13 +3,11 @@ package api.masterplan.app.notification.application.command
 import api.masterplan.app.notification.domain.model.value.CustomerId
 import api.masterplan.app.notification.domain.model.value.NotificationChannel
 import api.masterplan.app.notification.domain.model.value.NotificationMessage
-import api.masterplan.app.notification.domain.model.value.NotificationTitle
 import api.masterplan.app.notification.domain.model.value.NotificationType
 
 data class SendNotificationCommand(
     val customerId: CustomerId,
     val channel: NotificationChannel,
     val notificationType: NotificationType,
-    val title: NotificationTitle,
     val message: NotificationMessage
 )
