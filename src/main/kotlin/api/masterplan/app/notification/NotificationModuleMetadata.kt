b67a007df5b0d8bb9794ccf0f1590a57975b22c9
Leturgone <finekon221@gmail.com>
2026-03-31 15:26:20 +0300

    add NotificationModuleMetadata

---
+++ src/main/kotlin/api/masterplan/app/notification/NotificationModuleMetadata.kt
@@ -0,0 +1,9 @@
+package api.masterplan.app.notification
+
+import org.springframework.modulith.ApplicationModule
+
+@ApplicationModule(allowedDependencies = [
+    "logging::annotations"
+])
+class NotificationModuleMetadata {
+}
