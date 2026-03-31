dda0b86b7ac134e1779a8a007590565a5bd055ba
Leturgone <finekon221@gmail.com>
2026-03-31 12:54:22 +0300

    add NotificationService

---
+++ src/main/kotlin/api/masterplan/app/notification/domain/interfaces/NotificationService.kt
@@ -0,0 +1,16 @@
+package api.masterplan.app.notification.domain.interfaces
+
+import api.masterplan.app.notification.domain.model.value.CustomerId
+import api.masterplan.app.notification.domain.model.value.NotificationChannel
+import api.masterplan.app.notification.domain.model.value.NotificationMessage
+import api.masterplan.app.notification.domain.model.value.NotificationTitle
+import api.masterplan.app.notification.domain.model.value.NotificationType
+
+interface NotificationService {
+
+    fun sendNotification(customerId: CustomerId,
+                         channel: NotificationChannel,
+                         notificationType: NotificationType,
+                         title: NotificationTitle,
+                         message: NotificationMessage)
+}
