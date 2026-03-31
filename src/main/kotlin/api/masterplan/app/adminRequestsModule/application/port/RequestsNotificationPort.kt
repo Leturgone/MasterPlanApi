2cb3f262641f8b6d4e8a609ec07d8a5be8e53203
Leturgone <finekon221@gmail.com>
2026-04-01 00:46:37 +0300

    add values for notification message

--- src/main/kotlin/api/masterplan/app/adminRequestsModule/application/port/RequestsNotificationPort.kt
+++ src/main/kotlin/api/masterplan/app/adminRequestsModule/application/port/RequestsNotificationPort.kt
@@ -1,10 +1,12 @@
 package api.masterplan.app.adminRequestsModule.application.port
 
+import api.masterplan.app.adminRequestsModule.domain.model.value.AdminRequestStatus
+import api.masterplan.app.adminRequestsModule.domain.model.value.AdminRequestTitle
 import java.util.*
 
 interface RequestsNotificationPort {
 
-    fun sendNewRequestNotification(message: String)
+    fun sendNewRequestNotification(requestTitle: AdminRequestTitle)
 
-    fun sendRequestChangeStatusNotification(consumerId: UUID, message: String)
+    fun sendRequestChangeStatusNotification(consumerId: UUID,requestTitle: AdminRequestTitle,requestStatus: AdminRequestStatus)
 }
