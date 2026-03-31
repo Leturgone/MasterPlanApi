6898b4df66f9a64ce2e7e512898c0342cde391fa
Leturgone <finekon221@gmail.com>
2026-03-31 12:58:42 +0300

    add NotificationServiceImpl

---
+++ src/main/kotlin/api/masterplan/app/notification/application/service/NotificationServiceImpl.kt
@@ -0,0 +1,39 @@
+package api.masterplan.app.notification.application.service
+
+import api.masterplan.app.logging.annotations.LoggingMethod
+import api.masterplan.app.notification.domain.interfaces.NotificationSender
+import api.masterplan.app.notification.domain.interfaces.NotificationService
+import api.masterplan.app.notification.domain.model.entity.GenericNotification
+import api.masterplan.app.notification.domain.model.value.CustomerId
+import api.masterplan.app.notification.domain.model.value.NotificationChannel
+import api.masterplan.app.notification.domain.model.value.NotificationMessage
+import api.masterplan.app.notification.domain.model.value.NotificationTitle
+import api.masterplan.app.notification.domain.model.value.NotificationType
+import org.springframework.stereotype.Service
+
+@Service
+class NotificationServiceImpl(
+    private val notificationSender: NotificationSender
+): NotificationService {
+
+    @LoggingMethod("notificationModule")
+    override fun sendNotification(
+        customerId: CustomerId,
+        channel: NotificationChannel,
+        notificationType: NotificationType,
+        title: NotificationTitle,
+        message: NotificationMessage
+    ) {
+        val notification = GenericNotification.create(
+            title = title,
+            message = message,
+            notificationType = notificationType,
+        )
+
+        when(channel){
+            NotificationChannel.PUSH -> notificationSender.sendPushNotification(customerId, notification)
+        }
+    }
+
+
+}
