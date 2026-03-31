3373321a5da6cedf0cb9e085bc1089966367c210
Leturgone <finekon221@gmail.com>
2026-03-31 18:28:27 +0300

    add validation

--- src/main/kotlin/api/masterplan/app/notification/domain/model/value/NotificationMessage.kt
+++ src/main/kotlin/api/masterplan/app/notification/domain/model/value/NotificationMessage.kt
@@ -4,13 +4,16 @@ import api.masterplan.app.notification.domain.exception.NotificationException
 
 @JvmInline
 value class NotificationMessage(val message:String) {
-    fun validate(message: String): NotificationMessage {
-        try {
-            require(message.isNotBlank()) { "Message cant be blank" }
-            require(message.length <= 100) { "Message too long" }
-        } catch (e: IllegalArgumentException) {
-            throw NotificationException.InvalidNotificationMessage(e.message)
+
+    companion object{
+        fun validate(message: String): NotificationMessage {
+            try {
+                require(message.isNotBlank()) { "Message cant be blank" }
+                require(message.length <= 100) { "Message too long" }
+            } catch (e: IllegalArgumentException) {
+                throw NotificationException.InvalidNotificationMessage(e.message)
+            }
+            return NotificationMessage(message)
         }
-        return NotificationMessage(message)
     }
 }
