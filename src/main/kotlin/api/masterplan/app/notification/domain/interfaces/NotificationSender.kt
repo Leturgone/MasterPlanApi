55723c2f521a3052f35406445f55d15e495a7e2f
Leturgone <finekon221@gmail.com>
2026-03-31 12:54:37 +0300

    add NotificationSender

---
+++ src/main/kotlin/api/masterplan/app/notification/domain/interfaces/NotificationSender.kt
@@ -0,0 +1,9 @@
+package api.masterplan.app.notification.domain.interfaces
+
+import api.masterplan.app.notification.domain.model.entity.GenericNotification
+import api.masterplan.app.notification.domain.model.value.CustomerId
+
+
+interface NotificationSender {
+    fun sendPushNotification(customerId: CustomerId, notification: GenericNotification)
+}
