4cc17f80307599307ef53e73a13ad954b365d9a8
Leturgone <finekon221@gmail.com>
2026-03-31 15:26:36 +0300

    add NotificationSenderImpl

---
+++ src/main/kotlin/api/masterplan/app/notification/infrastructure/websocket/sender/NotificationSenderImpl.kt
@@ -0,0 +1,20 @@
+package api.masterplan.app.notification.infrastructure.websocket.sender
+
+import api.masterplan.app.logging.annotations.LoggingNotificationMethod
+import api.masterplan.app.notification.domain.interfaces.NotificationSender
+import api.masterplan.app.notification.domain.model.entity.GenericNotification
+import api.masterplan.app.notification.domain.model.value.CustomerId
+import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.stereotype.Component
+
+@Component
+class NotificationSenderImpl(
+    private val messagingTemplate: SimpMessagingTemplate
+): NotificationSender {
+
+    @LoggingNotificationMethod
+    override fun sendPushNotification(customerId: CustomerId, notification: GenericNotification) {
+        val destination = "/topic/notifications/${customerId.value}"
+        messagingTemplate.convertAndSend(destination, notification)
+    }
+}
