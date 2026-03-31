d5ee726cf5408c30f094fbc7fb9d91cee70a637a
Leturgone <finekon221@gmail.com>
2026-04-01 00:47:04 +0300

    add creating notification message in adapter

--- src/main/kotlin/api/masterplan/app/adminRequestsModule/infrastructure/adapters/RequestsNotificationAdapter.kt
+++ src/main/kotlin/api/masterplan/app/adminRequestsModule/infrastructure/adapters/RequestsNotificationAdapter.kt
@@ -1,6 +1,8 @@
 package api.masterplan.app.adminRequestsModule.infrastructure.adapters
 
 import api.masterplan.app.adminRequestsModule.application.port.RequestsNotificationPort
+import api.masterplan.app.adminRequestsModule.domain.model.value.AdminRequestStatus
+import api.masterplan.app.adminRequestsModule.domain.model.value.AdminRequestTitle
 import api.masterplan.app.apiContracts.notifications.NotificationModuleService
 import org.springframework.stereotype.Component
 import java.util.UUID
@@ -10,11 +12,13 @@ class RequestsNotificationAdapter(
     private val notificationService: NotificationModuleService
 ): RequestsNotificationPort {
 
-    override fun sendNewRequestNotification(message: String) {
-        notificationService.sendNewRequestNotification(message)
+    override fun sendNewRequestNotification(requestTitle: AdminRequestTitle) {
+        val notificationMessage = "Новая заявка: ${requestTitle.value}"
+        notificationService.sendNewRequestNotification(notificationMessage)
     }
 
-    override fun sendRequestChangeStatusNotification(consumerId: UUID, message: String) {
-        notificationService.sendRequestChangeStatusNotification(consumerId, message)
+    override fun sendRequestChangeStatusNotification(consumerId: UUID,requestTitle: AdminRequestTitle,requestStatus: AdminRequestStatus) {
+        val notificationMessage = "Статус заявки ${requestTitle.value} изменен на ${requestStatus.name}"
+        notificationService.sendRequestChangeStatusNotification(consumerId, notificationMessage)
     }
 }
