4da5580d124e125937f7b63c0cbe475d07b91365
Leturgone <finekon221@gmail.com>
2026-03-31 18:54:39 +0300

    edit without title in command

--- src/main/kotlin/api/masterplan/app/notification/application/usecase/SendAdminNotificationUseCase.kt
+++ src/main/kotlin/api/masterplan/app/notification/application/usecase/SendAdminNotificationUseCase.kt
@@ -2,11 +2,6 @@ package api.masterplan.app.notification.application.usecase
 
 import api.masterplan.app.notification.application.command.SendAdminNotificationCommand
 import api.masterplan.app.notification.domain.interfaces.NotificationService
-import api.masterplan.app.notification.domain.model.value.CustomerId
-import api.masterplan.app.notification.domain.model.value.NotificationChannel
-import api.masterplan.app.notification.domain.model.value.NotificationMessage
-import api.masterplan.app.notification.domain.model.value.NotificationTitle
-import api.masterplan.app.notification.domain.model.value.NotificationType
 import org.springframework.stereotype.Service
 
 @Service
@@ -18,7 +13,6 @@ class SendAdminNotificationUseCase(
             val result = notificationService.sendAdminNotification(
                 channel = command.channel,
                 notificationType = command.notificationType,
-                title = command.title,
                 message = command.message
             )
             Result.success(result)
