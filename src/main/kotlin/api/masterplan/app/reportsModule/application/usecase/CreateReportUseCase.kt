547b4dfcd1fc0771736f5d52cf0e9d5ef1378a88
Leturgone <finekon221@gmail.com>
2026-04-01 14:36:01 +0300

    add CreateReport notification to use case

--- src/main/kotlin/api/masterplan/app/reportsModule/application/usecase/CreateReportUseCase.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/application/usecase/CreateReportUseCase.kt
@@ -1,7 +1,9 @@
 package api.masterplan.app.reportsModule.application.usecase
 
 import api.masterplan.app.reportsModule.application.command.CreateReportCommand
+import api.masterplan.app.reportsModule.application.ports.ReportEmployeesPort
 import api.masterplan.app.reportsModule.application.ports.ReportFilesPort
+import api.masterplan.app.reportsModule.application.ports.ReportNotificationPort
 import api.masterplan.app.reportsModule.domain.interfaces.ReportService
 import api.masterplan.app.reportsModule.domain.models.value.ReportId
 import org.springframework.stereotype.Service
@@ -9,20 +11,30 @@ import org.springframework.stereotype.Service
 @Service
 class CreateReportUseCase(
     private val reportService: ReportService,
-    private val reportFilesPort: ReportFilesPort
+    private val reportEmployeePort: ReportEmployeesPort,
+    private val reportFilesPort: ReportFilesPort,
+    private val notificationPort: ReportNotificationPort
 ) {
     operator fun invoke(command: CreateReportCommand): Result<ReportId> {
         return try {
-            val planReportFileId = reportFilesPort.uploadReportFile(command.document)
-            val planReportId = reportService.createReport(
+            val reportFileId = reportFilesPort.uploadReportFile(command.document)
+            val reportId = reportService.createReport(
                 id = command.id,
                 title = command.title,
                 description = command.description,
                 employeeId = command.employeeId,
                 referenceId = command.referenceId,
-                documentId = planReportFileId,
+                documentId = reportFileId,
             )
-            Result.success(planReportId)
+
+            val directorId = reportEmployeePort.getDirectorId(command.employeeId)
+            directorId?.let {
+                notificationPort.sendNewReportNotification(
+                    directorId = directorId,
+                    reportTitle = command.title,
+                )
+            }
+            Result.success(reportId)
         }catch (e: Exception){
             Result.failure(e)
         }
