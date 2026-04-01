4c2f3e0ee738470fff0813e4d90ef7d3fbaaf20a
Leturgone <finekon221@gmail.com>
2026-04-01 14:35:32 +0300

    add ChangeReport notification to use case

--- src/main/kotlin/api/masterplan/app/reportsModule/application/usecase/ChangeReportStatusUseCase.kt
+++ src/main/kotlin/api/masterplan/app/reportsModule/application/usecase/ChangeReportStatusUseCase.kt
@@ -1,22 +1,29 @@
 package api.masterplan.app.reportsModule.application.usecase
 
 import api.masterplan.app.reportsModule.application.command.ChangeReportStatusCommand
+import api.masterplan.app.reportsModule.application.ports.ReportNotificationPort
 import api.masterplan.app.reportsModule.domain.interfaces.ReportService
 import api.masterplan.app.reportsModule.domain.models.value.ReportId
 import org.springframework.stereotype.Service
 
 @Service
 class ChangeReportStatusUseCase(
-    private val reportService: ReportService
+    private val reportService: ReportService,
+    private val reportNotificationPort: ReportNotificationPort
 ) {
     operator fun invoke(command: ChangeReportStatusCommand): Result<ReportId> {
         return try {
-            val planReportId = reportService.changeReportStatus(
+            val report = reportService.changeReportStatus(
                 reportId = command.reportId,
                 reportType = command.reportType,
                 status = command.status
             )
-            Result.success(planReportId)
+            reportNotificationPort.sendReportChangeStatusNotification(
+                employeeId = report.employeeId,
+                reportTitle = report.title,
+                reportStatus = report.reportStatus,
+            )
+            Result.success(report.id)
         }catch (e: Exception){
             Result.failure(e)
         }
