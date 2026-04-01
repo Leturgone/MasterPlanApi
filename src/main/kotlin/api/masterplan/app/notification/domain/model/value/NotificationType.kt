59dabb288d85457da173e4dea31fae96430dd8b9
Leturgone <finekon221@gmail.com>
2026-04-01 13:34:57 +0300

    add NotificationTypes

--- src/main/kotlin/api/masterplan/app/notification/domain/model/value/NotificationType.kt
+++ src/main/kotlin/api/masterplan/app/notification/domain/model/value/NotificationType.kt
@@ -3,8 +3,10 @@ package api.masterplan.app.notification.domain.model.value
 enum class NotificationType {
     TASK_ASSIGNMENT,
     TASK_CHANGE_STATUS,
+    TASK_UPDATED,
     NEW_REQUEST,
     REQUEST_CHANGE_STATUS,
     NEW_REPORT,
     REPORT_CHANGE_STATUS,
+    REPORT_UPDATED
 }
