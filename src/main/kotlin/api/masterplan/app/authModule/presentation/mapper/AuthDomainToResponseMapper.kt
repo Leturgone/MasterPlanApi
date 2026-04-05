11a80cbeb71c804ad17279f3adc81930391bf88a
Leturgone <finekon221@gmail.com>
2026-04-05 19:58:53 +0300

    add AuthDomainToResponseMapper

---
+++ src/main/kotlin/api/masterplan/app/authModule/presentation/mapper/AuthDomainToResponseMapper.kt
@@ -0,0 +1,13 @@
+package api.masterplan.app.authModule.presentation.mapper
+
+import api.masterplan.app.authModule.domain.dto.JwtToken
+import api.masterplan.app.authModule.presentation.dto.LoginResponse
+
+object AuthDomainToResponseMapper {
+    fun toLoginResponse(token: JwtToken): LoginResponse {
+        return LoginResponse(
+            token = token.token,
+            roles = token.roles.map { it.name }.toList()
+        )
+    }
+}
