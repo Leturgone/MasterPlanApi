0e6660ef4bc44339b4a91de96cdcacd83460d389
Leturgone <finekon221@gmail.com>
2026-04-07 19:17:30 +0300

    add AuthUserId param

--- src/main/kotlin/api/masterplan/app/authModule/domain/interfaces/TokenGeneratorService.kt
+++ src/main/kotlin/api/masterplan/app/authModule/domain/interfaces/TokenGeneratorService.kt
@@ -1,10 +1,11 @@
 package api.masterplan.app.authModule.domain.interfaces
 
 import api.masterplan.app.authModule.domain.dto.JwtToken
+import api.masterplan.app.authModule.domain.model.value.AuthUserId
 import api.masterplan.app.authModule.domain.model.value.AuthUserLogin
 import api.masterplan.app.authModule.domain.model.value.AuthUserRole
 
 interface TokenGeneratorService {
 
-    fun generateToken(authUserName: AuthUserLogin, authUserRoles: Set<AuthUserRole>): JwtToken
+    fun generateToken(authUserName: AuthUserLogin, authUserRoles: Set<AuthUserRole>,authUserId: AuthUserId): JwtToken
 }
