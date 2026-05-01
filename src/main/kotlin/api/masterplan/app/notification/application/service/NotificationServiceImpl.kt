3aab3f8809cb00fc94023eb7841b48a73a5798d7
Leturgone <finekon221@gmail.com>
2026-05-01 20:19:31 +0300

    add missing annotation

--- src/main/kotlin/api/masterplan/app/notification/application/service/NotificationServiceImpl.kt
+++ src/main/kotlin/api/masterplan/app/notification/application/service/NotificationServiceImpl.kt
@@ -38,6 +38,7 @@ class NotificationServiceImpl(
         }
     }
 
+    @LoggingMethod("notificationModule")
     override fun sendAdminNotification(
         channel: NotificationChannel,
         notificationType: NotificationType,
